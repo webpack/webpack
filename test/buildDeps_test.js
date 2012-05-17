e48e2a2c12c7c4e4983567ac6ac0db4167f73510
Tobias Koppers <tobias.koppers@googlemail.com>
2012-05-17 12:45:48 +0200

    added named chunks

--- test/buildDeps_test.js
+++ test/buildDeps_test.js
@@ -21,9 +21,9 @@ vows.describe("buildDeps").addBatch({
 		},
 
 		"one chunk": function(depTree) {
-			assert.deepEqual(Object.keys(depTree.chunks), ["0"]);
+			assert.deepEqual(Object.keys(depTree.chunks), ["main"]);
 			for(var i in depTree.modulesById) {
-				assert.deepEqual(depTree.modulesById[i].chunks, [0]);
+				assert.deepEqual(depTree.modulesById[i].chunks, ["main"]);
 			}
 		}
 	},
@@ -42,12 +42,12 @@ vows.describe("buildDeps").addBatch({
 		},
 
 		"two chunks": function(depTree) {
-			assert.deepEqual(Object.keys(depTree.chunks), ["0", "1"]);
-			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "main2.js")].chunks, [0]);
-			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "a.js")].chunks, [0]);
-			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "b.js")].chunks, [0]);
-			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "node_modules", "m1", "a.js")].chunks, [1]);
-			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "node_modules", "m1", "b.js")].chunks, [1]);
+			assert.deepEqual(Object.keys(depTree.chunks), ["1", "main"]);
+			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "main2.js")].chunks, ["main"]);
+			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "a.js")].chunks, ["main"]);
+			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "b.js")].chunks, ["main"]);
+			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "node_modules", "m1", "a.js")].chunks, ["1"]);
+			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "node_modules", "m1", "b.js")].chunks, ["1"]);
 		}
 	},
 
@@ -63,19 +63,19 @@ vows.describe("buildDeps").addBatch({
 		},
 
 		"two chunks": function(depTree) {
-			assert.deepEqual(Object.keys(depTree.chunks), ["0", "1"]);
-			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "main3.js")].chunks, [0]);
-			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "a.js")].chunks, [0, 1]);
-			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "c.js")].chunks, [1]);
+			assert.deepEqual(Object.keys(depTree.chunks), ["1", "main"]);
+			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "main3.js")].chunks, ["main"]);
+			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "a.js")].chunks, ["main", "1"]);
+			assert.deepEqual(depTree.modulesByFile[path.join(__dirname, "fixtures", "c.js")].chunks, ["1"]);
 			var main3id = ""+depTree.modulesByFile[path.join(__dirname, "fixtures", "main3.js")].id;
 			var aid = ""+depTree.modulesByFile[path.join(__dirname, "fixtures", "a.js")].id;
 			var cid = ""+depTree.modulesByFile[path.join(__dirname, "fixtures", "c.js")].id;
-			assert.deepEqual(Object.keys(depTree.chunks[0].modules), [main3id, aid]);
-			assert.deepEqual(Object.keys(depTree.chunks[1].modules), [cid, aid]);
-			assert.deepEqual(depTree.chunks[0].modules[main3id], "include");
-			assert.deepEqual(depTree.chunks[0].modules[aid], "include");
-			assert.deepEqual(depTree.chunks[1].modules[aid], "in-parent");
-			assert.deepEqual(depTree.chunks[1].modules[cid], "include");
+			assert.deepEqual(Object.keys(depTree.chunks.main.modules), [main3id, aid]);
+			assert.deepEqual(Object.keys(depTree.chunks["1"].modules), [cid, aid]);
+			assert.deepEqual(depTree.chunks.main.modules[main3id], "include");
+			assert.deepEqual(depTree.chunks.main.modules[aid], "include");
+			assert.deepEqual(depTree.chunks["1"].modules[aid], "in-parent");
+			assert.deepEqual(depTree.chunks["1"].modules[cid], "include");
 		}
 
 	}
