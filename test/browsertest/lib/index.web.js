9bc3759a3161e122f416986cc647cb748f83782e
Tobias Koppers <tobias.koppers@googlemail.com>
2013-02-04 12:49:01 +0100

    one more test

--- test/browsertest/lib/index.web.js
+++ test/browsertest/lib/index.web.js
@@ -161,11 +161,16 @@ describe("main", function() {
 		});
 
 		it("should resolve loaders relative to require", function() {
-			var index = "index";
-			require("../loaders/queryloader?query!../node_modules/subcontent/" + index + ".js").should.be.eql({
+			var index = "index", test = "test";
+			require("../loaders/queryloader?query!!!!../node_modules/subcontent/" + index + ".js").should.be.eql({
 				resourceQuery: null,
 				query: "?query",
 				prev: "module.exports = \"error\";"
+			});
+			require("!../loaders/queryloader?query!../node_modules/subcontent/" + test + ".jade").should.be.eql({
+				resourceQuery: null,
+				query: "?query",
+				prev: "xyz: abc"
 			});
 		});
 	});
