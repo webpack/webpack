a3cd3baf164e34a3e84caeed8073bbfab5b16f81
Tobias Koppers <tobias.koppers@googlemail.com>
2013-11-03 15:28:50 +0100

    fixes #114
    allow bound function expressions in require.ensure
    allow unparsed expressions in require.ensure

--- test/browsertest/lib/index.web.js
+++ test/browsertest/lib/index.web.js
@@ -272,6 +272,25 @@ describe("main", function() {
 			}
 		});
 
+		it("should handle bound function expressions", function(done) {
+			require.ensure([], function(require) {
+				this.should.be.eql({ test: true })
+				require("./empty?test");
+				process.nextTick.should.have.type("function"); // check if injection still works
+				require.ensure([], function(require) {
+					this.should.be.eql({ test: true })
+					done();
+				}.bind(this));
+			}.bind({test: true}));
+		});
+
+		it("should handle require.ensure without function expression", function(done) {
+			function f() {
+				done();
+			}
+			require.ensure([], f);
+		});
+
 		it("should accept a require.include call", function() {
 			require.include("./require.include");
 			var value = null;
