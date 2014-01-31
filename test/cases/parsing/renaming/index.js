5e7dfc52ccaffe894676fdaa92055430d522410a
Tobias Koppers <tobias.koppers@googlemail.com>
2014-01-31 14:09:08 +0100

    experimental renaming support

--- test/cases/parsing/renaming/index.js
+++ test/cases/parsing/renaming/index.js
@@ -24,3 +24,48 @@ it("should be able to rename require by IIFE call", function() {
 		somethingElse.should.be.eql(123);
 	}.call(this, 123, typeof require === "function" ? require : "error"));
 });
+
+it("should be able to rename stuff by IIFE call", function() {
+	(function(_exports, _exports2, _module, _module2, _define, _define2, _require, _require2) {
+		_define(function(R, E, M) {
+			R("./file").should.be.eql("ok");
+			_require("./file").should.be.eql("ok");
+			_require2("./file").should.be.eql("ok");
+			E.should.be.eql(exports);
+			_exports.should.be.eql(exports);
+			_exports2.should.be.eql(exports);
+			M.should.be.eql(module);
+			_module.should.be.eql(module);
+			_module2.should.be.eql(module);
+		});
+		_define2(["./file"], function(file) {
+			file.should.be.eql("ok");
+		});
+	}).call(this,
+			typeof exports !== 'undefined' ? exports : null,
+			exports,
+			typeof module !== 'undefined' ? module : null,
+			module,
+			typeof define !== 'undefined' ? define : null,
+			define,
+			typeof require !== 'undefined' ? require : null,
+			require);
+});
+
+it("should be able to rename stuff by IIFE call", function() {
+	(function(_exports, _module, _define, _require) {
+		_define(function(R, E, M) {
+			R("./file").should.be.eql("ok");
+			_require("./file").should.be.eql("ok");
+			E.should.be.eql(exports);
+			_exports.should.be.eql(exports);
+			M.should.be.eql(module);
+			_module.should.be.eql(module);
+		});
+	}).call(this,
+			typeof exports !== 'undefined' ? exports : null,
+			typeof module !== 'undefined' ? module : null,
+			typeof define !== 'undefined' ? define : null,
+			typeof require !== 'undefined' ? require : null);
+});
+
