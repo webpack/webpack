6086d8cd5584f5a1292ed7f4b0175df40695c4c1
Tobias Koppers <tobias.koppers@googlemail.com>
2014-02-12 14:53:33 +0100

    insert free var injection code to correct place
    webpack/webpack#167

---
+++ test/cases/parsing/inject-free-vars/index.js
@@ -0,0 +1,15 @@
+it("should inject the module object into a chunk (AMD1)", function() {
+	require([], function() {
+		module.webpackPolyfill.should.be.eql(1);
+	});
+});
+
+it("should inject the module object into a chunk (AMD2)", function() {
+	require([module.webpackPolyfill ? "./x1" : "./fail"]);
+});
+
+it("should inject the module object into a chunk (ensure)", function() {
+	require.ensure([], function(require) {
+		module.webpackPolyfill.should.be.eql(1);
+	});
+});
