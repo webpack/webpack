0f25ccfbe0027ee1e5584a82ddb2caf68007a9bf
Tobias Koppers <tobias.koppers@googlemail.com>
2014-03-12 22:30:21 +0100

    fixes #202

--- test/cases/parsing/local-modules/index.js
+++ test/cases/parsing/local-modules/index.js
@@ -24,3 +24,20 @@ it("should not create a chunk for a AMD require to a local module", function() {
 	});
 	sync.should.be.eql(true);
 });
+
+it("should define and require a local module with deps", function() {
+	module.exports = "not set";
+	define("my-module3", ["./dep"], function(dep) {
+		dep.should.be.eql("dep");
+		return 1234;
+	});
+	module.exports.should.be.eql("not set");
+	define("my-module4", ["my-module3", "./dep"], function(myModule, dep) {
+		dep.should.be.eql("dep");
+		myModule.should.be.eql(1234);
+		return 2345;
+	});
+	module.exports.should.be.eql("not set");
+	require("my-module3").should.be.eql(1234);
+	require("my-module4").should.be.eql(2345);
+});
