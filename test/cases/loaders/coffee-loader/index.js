9cbabcd6f27d40e69b272ab443dba2229130b206
Tobias Koppers <tobias.koppers@googlemail.com>
2014-03-21 16:11:43 +0100

    added test case of literate coffee-script

--- test/cases/loaders/coffee-loader/index.js
+++ test/cases/loaders/coffee-loader/index.js
@@ -2,3 +2,7 @@ it("should handle the coffee loader correctly", function() {
 	require("!coffee!../_resources/script.coffee").should.be.eql("coffee test");
 	require("../_resources/script.coffee").should.be.eql("coffee test");
 });
+
+it("should handle literate coffee script correctly", function() {
+	require("!coffee?literate!./script.coffee.md").should.be.eql("literate coffee test");
+});
