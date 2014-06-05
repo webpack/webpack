ff36642818de60525f0614e4152feba466382134
Tobias Koppers <tobias.koppers@googlemail.com>
2014-06-05 19:52:45 +0200

    added support and a special message for browserify pre-built javascripts.
    fixed #299

---
+++ test/cases/parsing/browserify/index.js
@@ -0,0 +1,5 @@
+it("should be able to parse browserified modules", function(done) {
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+		done();
+	},{}]},{},[1]);
+});
