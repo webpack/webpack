9218b4c414d967943a80109bda4e957429ae03f0
Tobias Koppers <tobias.koppers@googlemail.com>
2014-06-11 23:01:05 +0200

    fixed tests

--- test/Examples.test.js
+++ test/Examples.test.js
@@ -17,17 +17,23 @@ describe("Examples", function() {
 			webpackConfigPath = webpackConfigPath.substr(0, 1).toUpperCase() + webpackConfigPath.substr(1);
 			if(fs.existsSync(webpackConfigPath))
 				options = require(webpackConfigPath);
-			options.context = examplePath;
-			options.optimize = options.optimize || {};
-			options.output = options.output || {};
-			options.optimize.occurenceOrder = true;
-			options.output.pathInfo = true;
-			options.output.path = path.join(examplePath, "js");
-			options.output.publicPath = "js/";
-			if(!options.output.filename)
-				options.output.filename = "output.js";
-			if(!options.entry)
-				options.entry = "./example.js";
+			if(Array.isArray(options))
+				options.forEach(processOptions);
+			else
+				processOptions(options);
+			function processOptions(options) {
+				options.context = examplePath;
+				options.optimize = options.optimize || {};
+				options.output = options.output || {};
+				options.optimize.occurenceOrder = true;
+				options.output.pathInfo = true;
+				options.output.path = path.join(examplePath, "js");
+				options.output.publicPath = "js/";
+				if(!options.output.filename)
+					options.output.filename = "output.js";
+				if(!options.entry)
+					options.entry = "./example.js";
+			}
 			webpack(options, function(err, stats) {
 				if(err) return done(err);
 				stats = stats.toJson();
