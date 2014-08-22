f371945a29646a78d6aa614f13e706f277f43765
Jason Anderson <diurnalist@gmail.com>
2014-08-22 13:51:24 +0200

    Add TemplatedPathPlugin

---
+++ test/configCases/hash-length/output-filename/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = [{
+	output: {
+		filename: 'bundle.[hash:6].js'
+	}
+}, {
+	output: {
+		filename: 'bundle.[hash].js'
+	}
+}, {
+	output: {
+		filename: 'bundle.[chunkhash:8].js'
+	}
+}, {
+	output: {
+		filename: 'bundle.[chunkhash].js'
+	}
+}];
