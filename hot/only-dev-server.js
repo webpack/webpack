ec7ebe0ba8257695821140817d9fd521d470d371
Nick Dima <nick.dima@gmail.com>
2014-10-07 18:20:02 +0200

    Replace window.onmessage with window.addEventListener

--- hot/only-dev-server.js
+++ hot/only-dev-server.js
@@ -60,7 +60,7 @@ if(module.hot) {
 			});
 		});
 	}
-	window.onmessage = function(event) {
+	window.addEventListener("message", function(event) {
 		if(typeof event.data === "string" && event.data.indexOf("webpackHotUpdate") === 0) {
 			lastData = event.data;
 			if(!upToDate() && module.hot.status() === "idle") {
@@ -68,7 +68,7 @@ if(module.hot) {
 				check();
 			}
 		}
-	};
+	});
 	console.log("[HMR] Waiting for update signal from WDS...");
 } else {
 	throw new Error("[HMR] Hot Module Replacement is disabled");
