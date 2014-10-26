41e42e85895f5d75b30f00eeb326b38e27ba04ea
Tobias Koppers <tobias.koppers@googlemail.com>
2014-10-26 11:17:47 +0100

    fixed renaming in IIFEs and Assignments fixed #551, fixed #494

--- test/cases/parsing/issue-551/index.js
+++ test/cases/parsing/issue-551/index.js
@@ -1,16 +1,20 @@
 var window = {};
 
 it("should be able to set the public path", function() {
+	global.xyz = "xyz";
+	__webpack_public_path__ = global.xyz;
+	__webpack_require__.p.should.be.eql("xyz");
+
 	window.something = "something";
 	__webpack_public_path__ = window.something;
-	__webpack_public_path__.should.be.eql("something");
-	
+	__webpack_require__.p.should.be.eql("something");
+
 	__webpack_public_path__ = "abc";
-	__webpack_public_path__.should.be.eql("abc");
-	
+	__webpack_require__.p.should.be.eql("abc");
+
 	__webpack_public_path__ = func();
-	__webpack_public_path__.should.be.eql("func");
-	
+	__webpack_require__.p.should.be.eql("func");
+
 	function func() {
 		return "func";
 	}
