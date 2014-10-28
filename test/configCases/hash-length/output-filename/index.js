97780e1c1621176e4883adfb6c448a652571497f
Jason Anderson <diurnalist@gmail.com>
2014-10-28 12:24:26 +0100

    Updates hash length tests to catch async chunk loading

--- test/configCases/hash-length/output-filename/index.js
+++ test/configCases/hash-length/output-filename/index.js
@@ -1,3 +1,6 @@
-it("should compile and run the test", function () {
-
+it("should compile and run the test", function (done) {
+  require(['./chunk'], function () {
+    it("should load additional chunks", function () {});
+    done();
+  });
 });
