6a8caf5cd149f1c2dc0cf428673b78ff3bbf50a7
Tobias Koppers <tobias.koppers@googlemail.com>
2014-11-02 12:16:55 +0100

    fixes #524 again

--- test/cases/context/issue-524/index.js
+++ test/cases/context/issue-524/index.js
@@ -9,3 +9,17 @@ it("should support an empty context", function() {
 	}).should.throw();
 	c.keys().should.be.eql([]);
 });
+
+// This would be a useful testcase, but it requires an (really) empty directory.
+// **but** you cannot commit empty directories into git
+/*it("should support an empty context (empty dir)", function() {
+	var c = require.context("./empty", true, /^nothing$/);
+	c.id.should.be.type("number");
+	(function() {
+		c.resolve("");
+	}).should.throw();
+	(function() {
+		c("");
+	}).should.throw();
+	c.keys().should.be.eql([]);
+});*/
