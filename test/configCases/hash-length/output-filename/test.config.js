d5dc904a15d1eaa029f107e21924405acb0323b1
Tobias Koppers <tobias.koppers@googlemail.com>
2014-11-03 08:02:02 +0100

    fixed incomplete hash bug

--- test/configCases/hash-length/output-filename/test.config.js
+++ test/configCases/hash-length/output-filename/test.config.js
@@ -5,7 +5,7 @@ module.exports = {
 		var files = fs.readdirSync(options.output.path);
 		var hashParamMatches = options.output.filename.match(/:(\d+)/);
 		var hashLength = hashParamMatches && hashParamMatches[1];
-		var bundleDetect = new RegExp("^bundle\.[a-z0-9]" + (hashLength ? "{" + hashLength + "}" : "{16,}") + "\.js$", "i");
+		var bundleDetect = new RegExp("^bundle" + i, "i");
 		for (var i = 0, file; i < files.length; i++) {
 			file = files[i];
 			if (bundleDetect.test(file)) {
--- test/configCases/hash-length/output-filename/webpack.config.js
+++ test/configCases/hash-length/output-filename/webpack.config.js
@@ -1,29 +1,29 @@
 module.exports = [{
 	output: {
-		filename: 'bundle.[hash:6].js'
+		filename: 'bundle0.[hash:6].js'
 	}
 }, {
 	output: {
-		filename: 'bundle.[hash].js'
+		filename: 'bundle1.[hash].js'
 	}
 }, {
 	output: {
-		filename: 'bundle.[chunkhash:8].js',
+		filename: 'bundle2.[chunkhash:8].js',
 		chunkFilename: '[id].bundle.[chunkhash:8].js'
 	}
 }, {
 	output: {
-		filename: 'bundle.[chunkhash].js',
+		filename: 'bundle3.[chunkhash].js',
 		chunkFilename: '[id].bundle.[chunkhash].js'
 	}
 }, {
 	output: {
-		filename: 'bundle.[hash].js',
+		filename: 'bundle4.[hash].js',
 		chunkFilename: '[id].bundle.[hash:8].js'
 	}
 }, {
 	output: {
-		filename: 'bundle.[hash:6].js',
+		filename: 'bundle5.[hash:6].js',
 		chunkFilename: '[id].bundle.[hash:8].js'
 	}
 }];
