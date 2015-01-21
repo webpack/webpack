7da31b29902539fc48e2f4d36a13d3a8d850ad32
Tobias Koppers <tobias.koppers@googlemail.com>
2015-01-21 20:25:25 +0100

    change `node.Buffer` in a way that is backward-compatible #709

---
+++ test/configCases/target/buffer-backward/index.js
@@ -0,0 +1,13 @@
+require("should");
+
+it("should provide a global Buffer shim", function () {
+	(typeof Buffer).should.be.eql("undefined");
+});
+
+it("should fail on the buffer module", function () {
+	(function(argument) {
+		try {
+			require("buffer");
+		} catch(e) { throw e; }
+	}).should.throw();
+});
---
+++ test/configCases/target/buffer-backward/webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	target: "web",
+	node: {
+		buffer: false
+	}
+};
