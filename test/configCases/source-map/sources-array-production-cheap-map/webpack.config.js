c3f950506a6f59aa5f9aa0e735e5ac23171417ef
Tobias Koppers <tobias.koppers@googlemail.com>
2015-02-27 22:42:37 +0100

    added cheap mode test cases

---
+++ test/configCases/source-map/sources-array-production-cheap-map/webpack.config.js
@@ -0,0 +1,14 @@
+var webpack = require("../../../../");
+module.exports = {
+	node: {
+		__dirname: false,
+		__filename: false
+	},
+	plugins: [
+		new webpack.SourceMapDevToolPlugin({
+			filename: "[file].map",
+			cheap: true
+		}),
+		new webpack.optimize.UglifyJsPlugin()
+	]
+};
