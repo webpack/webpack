3105d31d7948cc74e7d7830091505437e85f7218
Tobias Koppers <tobias.koppers@googlemail.com>
2015-03-23 00:27:55 +0100

    spelling

--- test/configCases/target/buffer-default/index.js
+++ test/configCases/target/buffer-default/index.js
@@ -1,7 +1,7 @@
 require("should");
 
 it("should provide a global Buffer shim", function () {
-	Buffer.should.be.an.Function;
+	Buffer.should.be.a.Function;
 });
 
 it("should provide the buffer module", function () {
