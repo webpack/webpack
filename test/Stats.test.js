fc9b941c62b6d27c6de11c5f62da6be2df835a60
Tobias Koppers <tobias.koppers@googlemail.com>
2015-04-26 22:56:55 +0200

    tests should not include version number

--- test/Stats.test.js
+++ test/Stats.test.js
@@ -47,7 +47,7 @@ describe("Stats", function() {
 					colors: false
 				});
 				(typeof actual).should.be.eql("string");
-				actual = actual.replace(/[0-9]+(\s?ms)/g, "X$1").replace(/\r/g, "");
+				actual = actual.replace(/Version:.+\n/, "").replace(/[0-9]+(\s?ms)/g, "X$1").replace(/\r/g, "");
 				var expected = fs.readFileSync(path.join(base, testName, "expected.txt"), "utf-8").replace(/\r/g, "");
 				if(actual !== expected) {
 					fs.writeFileSync(path.join(base, testName, "actual.txt"), actual, "utf-8");
