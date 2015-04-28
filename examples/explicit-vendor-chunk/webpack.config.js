300ee8db48da55ce20e4a06629330f97d33005e4
Tobias Koppers <tobias.koppers@googlemail.com>
2015-04-28 16:58:01 +0200

    added explicit-vendor-chunk example

---
+++ examples/explicit-vendor-chunk/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require("path");
+var CommonsChunkPlugin = require("../../lib/optimize/CommonsChunkPlugin");
+module.exports = {
+	entry: {
+		vendor: "./vendor",
+		pageA: "./pageA",
+		pageB: "./pageB",
+		pageC: "./pageC"
+	},
+	output: {
+		path: path.join(__dirname, "js"),
+		filename: "[name].js"
+	},
+	plugins: [
+		new CommonsChunkPlugin({
+			name: "vendor",
+			minChunks: Infinity
+		})
+	]
+}
