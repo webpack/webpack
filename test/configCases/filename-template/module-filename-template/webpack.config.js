79c90fe616b9d01c039d897cd1df6166501165d5
Avi Haiat <thaiat@ipelia.fr>
2015-07-07 17:07:26 +0300

    Add unit test

---
+++ test/configCases/filename-template/module-filename-template/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    output: {
+        lineToLine: true,
+        devtoolModuleFilenameTemplate: function(info) {
+            return "dummy:///" + info.resourcePath;
+        }
+    },
+    node: {
+        __dirname: false,
+        __filename: false
+    },
+    devtool: "cheap-source-map"
+
+};
+
