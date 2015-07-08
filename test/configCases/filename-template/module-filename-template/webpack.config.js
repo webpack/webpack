fb489deb62cfcc325edb522b6d238d3d3451a422
Avi Haiat <thaiat@ipelia.fr>
2015-07-08 12:30:43 +0300

    Fix indentation

--- test/configCases/filename-template/module-filename-template/webpack.config.js
+++ test/configCases/filename-template/module-filename-template/webpack.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    output: {
-        lineToLine: true,
-        devtoolModuleFilenameTemplate: function(info) {
-            return "dummy:///" + info.resourcePath;
-        }
-    },
-    node: {
-        __dirname: false,
-        __filename: false
-    },
-    devtool: "cheap-source-map"
+	output: {
+		lineToLine: true,
+		devtoolModuleFilenameTemplate: function(info) {
+			return "dummy:///" + info.resourcePath;
+		}
+	},
+	node: {
+		__dirname: false,
+		__filename: false
+	},
+	devtool: "cheap-source-map"
 
 };
-
