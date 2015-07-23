f19712f1bfbf0e8b1b14a919f79fe14c623ef04e
Kamil Szot <kamil.szot@gmail.com>
2015-07-23 12:31:56 +0200

    Adjust to use of interpret module version 0.6

--- bin/convert-argv.js
+++ bin/convert-argv.js
@@ -56,15 +56,27 @@ module.exports = function(optimist, argv, convertOptions) {
 	}
 
 	if(configPath) {
-		var moduleName = interpret.extensions[ext];
-		if (moduleName) {
-			var compiler = require(moduleName);
-			var register = interpret.register[moduleName];
-			var config = interpret.configurations[moduleName];
-			if (register) {
-				register(compiler, config);
+
+		function registerCompiler(moduleDescriptor) {
+			if (moduleDescriptor) {
+				if(typeof(moduleDescriptor) == "string") {
+					require(moduleDescriptor);
+				} else if(!Array.isArray(moduleDescriptor)) {
+					moduleDescriptor.register(require(moduleDescriptor.module));
+				} else {
+					for(var i = 0; i < moduleDescriptor.length; i++) {
+						try {
+							registerCompiler(moduleDescriptor[i]);
+							break;
+						} catch(e) {
+							// do nothing
+						}
+					}
+				}
 			}
 		}
+
+		registerCompiler(interpret.extensions[ext]);
 		options = require(configPath);
 	}
 
