9aaf42b65967034a8a981a0377054bcc6ee63089
Tobias Koppers <tobias.koppers@googlemail.com>
2016-02-22 19:39:33 +0100

    added another test case for #2084

--- test/cases/parsing/issue-2084/index.js
+++ test/cases/parsing/issue-2084/index.js
@@ -12,7 +12,7 @@ it("should bind this context on require callback", function(done) {
 				done();
 			} catch(e) { done(e); }
 		}.bind(this));
-	})
+	});
 });
 
 it("should bind this context on require callback (loaded)", function(done) {
@@ -25,7 +25,42 @@ it("should bind this context on require callback (loaded)", function(done) {
 				done();
 			} catch(e) { done(e); }
 		}.bind(this));
-	})
+	});
+});
+
+it("should bind this context on require callback (foo)", function(done) {
+	var foo = {ok: true};
+	require([], function(load) {
+		try {
+			require("./file").should.be.eql("file");
+			this.should.be.eql({ok: true});
+			done();
+		} catch(e) { done(e); }
+	}.bind(foo));
+});
+
+it("should bind this context on require callback (foo, loaded)", function(done) {
+	var foo = {ok: true};
+	require(["./load.js"], function(load) {
+		try {
+			require("./file").should.be.eql("file");
+			load.should.be.eql("load");
+			this.should.be.eql({ok: true});
+			done();
+		} catch(e) { done(e); }
+	}.bind(foo));
+});
+
+it("should bind this context on require callback (foo)", function(done) {
+	runWithThis({ok: true}, function() {
+		require([], function(load) {
+			try {
+				require("./file").should.be.eql("file");
+				this.should.be.eql({ok: {ok: true}});
+				done();
+			} catch(e) { done(e); }
+		}.bind({ok: this}));
+	});
 });
 
 it("should bind this context on require.ensure callback", function(done) {
@@ -37,7 +72,7 @@ it("should bind this context on require.ensure callback", function(done) {
 				done();
 			} catch(e) { done(e); }
 		}.bind(this));
-	})
+	});
 });
 
 it("should bind this context on require.ensure callback (loaded)", function(done) {
@@ -49,5 +84,5 @@ it("should bind this context on require.ensure callback (loaded)", function(done
 				done();
 			} catch(e) { done(e); }
 		}.bind(this));
-	})
+	});
 });
