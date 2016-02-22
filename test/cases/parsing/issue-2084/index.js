2125e2411e3fc3fdd98380aada3ce7963105cc78
Tobias Koppers <tobias.koppers@googlemail.com>
2016-02-22 18:47:16 +0100

    fixes #2084

---
+++ test/cases/parsing/issue-2084/index.js
@@ -0,0 +1,53 @@
+function runWithThis(obj, fn) {
+	fn.call(obj);
+}
+
+it("should bind this context on require callback", function(done) {
+	require("./file");
+	runWithThis({ok: true}, function() {
+		require([], function() {
+			try {
+				require("./file").should.be.eql("file");
+				this.should.be.eql({ok: true});
+				done();
+			} catch(e) { done(e); }
+		}.bind(this));
+	})
+});
+
+it("should bind this context on require callback (loaded)", function(done) {
+	runWithThis({ok: true}, function() {
+		require(["./load.js"], function(load) {
+			try {
+				require("./file").should.be.eql("file");
+				load.should.be.eql("load");
+				this.should.be.eql({ok: true});
+				done();
+			} catch(e) { done(e); }
+		}.bind(this));
+	})
+});
+
+it("should bind this context on require.ensure callback", function(done) {
+	runWithThis({ok: true}, function() {
+		require.ensure([], function(require) {
+			try {
+				require("./file").should.be.eql("file");
+				this.should.be.eql({ok: true});
+				done();
+			} catch(e) { done(e); }
+		}.bind(this));
+	})
+});
+
+it("should bind this context on require.ensure callback (loaded)", function(done) {
+	runWithThis({ok: true}, function() {
+		require.ensure(["./load.js"], function(require) {
+			try {
+				require("./file").should.be.eql("file");
+				this.should.be.eql({ok: true});
+				done();
+			} catch(e) { done(e); }
+		}.bind(this));
+	})
+});
