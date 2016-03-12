b7d087a7d25fc5aec1ccdf06838109f0cee6c89a
Larkin Family <larkinfamily@Larkins-iMac.local>
2016-03-12 16:23:53 -0600

    Updated test cases for better coverage

--- test/configCases/target/umd-auxiliary-comments-object/index.js
+++ test/configCases/target/umd-auxiliary-comments-object/index.js
@@ -6,8 +6,8 @@ it("should have auxiliary comments", function() {
 	var fs = require("fs");
 	var source = fs.readFileSync(__filename, "utf-8");
 
-	source.should.containEql("//test comment commonjs");
-	source.should.containEql("//test comment commonjs2");
-	source.should.containEql("//test comment amd");
-	source.should.containEql("//test comment root");
+	source.should.containEql("//test " + "comment " + "commonjs");
+	source.should.containEql("//test " + "comment " + "commonjs2");
+	source.should.containEql("//test " + "comment " + "amd");
+	source.should.containEql("//test " + "comment " + "root");
 });
