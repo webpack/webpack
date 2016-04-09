1124ac9785e9f8e6fb498b63d9098636acd63864
Beau Roberts <john.mirabeau.roberts@gmail.com>
2016-04-09 14:33:26 +0100

    Fixes issue #2299 (#2300)
    Fixes an issue where multiple calls to loadModule from within a loader
    would resolve modules using the context of the *last loaded module*
    rather than the module *doing the loading*, which is problematic if the
    modules being loaded are in different directories.

---
+++ test/cases/loaders/issue-2299/loader/index.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var async = require('async');
+var assign = require('object-assign');
+module.exports = function(content) {
+	var cb = this.async();
+	var json = JSON.parse(content);
+	async.mapSeries(
+		json.imports,
+		function(url, callback) {
+			this.loadModule(url, function(err, source, map, module) {
+				if (err) {
+					return callback(err);
+				}
+				callback(null, this.exec(source, url));
+			}.bind(this))
+		}.bind(this),
+		function(err, results) {
+			if (err) {
+				return cb(err);
+			}
+			// Combine all the results into one object and return it
+			cb(null, 'module.exports = ' + JSON.stringify(results.reduce(function(prev, result) {
+				return assign({}, prev, result);
+			}, json)));
+		}
+	);
+}
