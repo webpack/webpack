f57c6caf5b530380f78fe62daa8af309a91c7432
Tobias Koppers <tobias.koppers@googlemail.com>
2016-04-21 23:50:28 +0200

    reverted module.e to module.exports
    because it causes issues with modules that define
    exports on the module object

--- test/cases/parsing/polyfill-module/index.js
+++
@@ -1,8 +0,0 @@
-it("should polyfill module", function() {
-	var m = module;
-
-	(typeof m.id).should.be.not.eql("undefined");
-	m.children.should.be.eql([]);
-	m.exports.should.be.eql({});
-	m.loaded.should.be.eql(true);
-});
