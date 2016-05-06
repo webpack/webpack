a421f69207e70b428c29348eb2eefef909687871
Tobias Koppers <tobias.koppers@googlemail.com>
2016-05-06 12:03:23 +0200

    fixed test, according to behavior of this

--- test/cases/parsing/harmony-this/index.js
+++ test/cases/parsing/harmony-this/index.js
@@ -1,29 +1,17 @@
 "use strict";
 
-import sd, {sa, sb as SB} from "./strict-abc";
-
-import * as sabc from "./strict-abc";
-
 import d, {a, b as B} from "./abc";
 
 import * as abc from "./abc";
 
 function x() { throw new Error("should not be executed"); }
-it("should have this = undefined on imported strict functions", function() {
-	(typeof sd()).should.be.eql("undefined");
-	(typeof sa()).should.be.eql("undefined");
-	(typeof SB()).should.be.eql("undefined");
+it("should have this = undefined on imported non-strict functions", function() {
 	x
-	sabc.sa().should.be.eql(sabc);
-});
-
-it("should have this = global on imported non-strict functions", function() {
+	d().should.be.eql("undefined");
 	x
-	d().should.be.eql(global);
+	a().should.be.eql("undefined");
 	x
-	a().should.be.eql(global);
-	x
-	B().should.be.eql(global);
+	B().should.be.eql("undefined");
 	x
 	abc.a().should.be.eql(abc);
 	x
