ebeb1c88b41fe5ca5a90ff763df1f1bff0387ff8
Tobias Koppers <tobias.koppers@googlemail.com>
2016-06-04 12:16:52 +0200

    added note for browsertest

--- test/cases/runtime/issue-2391/index.js
+++ test/cases/runtime/issue-2391/index.js
@@ -1,3 +1,3 @@
 it("should not have a require.onError function by default", function() {
-	(typeof require.onError).should.be.eql("undefined");
+	(typeof require.onError).should.be.eql("undefined"); // expected to fail in browsertests
 });
