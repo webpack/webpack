e15c8b05fe1a2e29d34d32ed0dba1f51884efa54
Tobias Koppers <tobias.koppers@googlemail.com>
2016-06-05 10:20:36 +0200

    test case for hot accepting chunks

---
+++ test/hotCases/chunks/accept-system-import/index.js
@@ -0,0 +1,19 @@
+it("should import a changed chunk", function(done) {
+	System.import("./chunk").then(function(chunk) {
+		chunk.value.should.be.eql(1);
+		System.import("./chunk2").then(function(chunk2) {
+			chunk2.value.should.be.eql(1);
+			console.log(module);
+			NEXT(require("../../update")(done));
+			module.hot.accept(["./chunk", "./chunk2"], function() {
+				System.import("./chunk").then(function(chunk) {
+					chunk.value.should.be.eql(2);
+					System.import("./chunk2").then(function(chunk2) {
+						chunk2.value.should.be.eql(2);
+						done();
+					}).catch(done);
+				}).catch(done);
+			});
+		}).catch(done);
+	}).catch(done);
+});
