bb27b2e9fcec0121249b2eb952761b709fafa517
Tobias Koppers <tobias.koppers@googlemail.com>
2016-06-07 23:48:25 +0200

    remove console.log

--- test/hotCases/chunks/accept-system-import/index.js
+++ test/hotCases/chunks/accept-system-import/index.js
@@ -3,7 +3,6 @@ it("should import a changed chunk", function(done) {
 		chunk.value.should.be.eql(1);
 		System.import("./chunk2").then(function(chunk2) {
 			chunk2.value.should.be.eql(1);
-			console.log(module);
 			NEXT(require("../../update")(done));
 			module.hot.accept(["./chunk", "./chunk2"], function() {
 				System.import("./chunk").then(function(chunk) {
