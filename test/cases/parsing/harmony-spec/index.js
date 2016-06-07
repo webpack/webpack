d9e8ac07c8117df0fdd90f0f017660468e3b301d
Tobias Koppers <tobias.koppers@googlemail.com>
2016-06-07 23:50:15 +0200

    added more harmony test cases

--- test/cases/parsing/harmony-spec/index.js
+++ test/cases/parsing/harmony-spec/index.js
@@ -1,4 +1,6 @@
 import { value, add } from "./live";
+import { value as value2, add as add2 } from "./live-es5";
+import { value as valueEval, evalInModule } from "./eval";
 import { getLog } from "./order-tracker";
 import "./order-c";
 import cycleValue from "./export-cycle-a";
@@ -7,6 +9,18 @@ it("should establish live binding of values", function() {
 	value.should.be.eql(0);
 	add(2);
 	value.should.be.eql(2);
+});
+
+it("should establish live binding of values with transpiled es5 module", function() {
+	value2.should.be.eql(0);
+	add2(5);
+	value2.should.be.eql(5);
+});
+
+it("should allow to use eval with exports", function() {
+	valueEval.should.be.eql(0);
+	evalInModule("value = 5");
+	valueEval.should.be.eql(5);
 });
 
 it("should execute modules in the correct order", function() {
