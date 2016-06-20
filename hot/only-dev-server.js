7e5d85d62133f49620db48a00dff95943f9bf5a2
Simen Bekkhus <sbekkhus91@gmail.com>
2016-06-20 08:21:00 +0200

    Use eslint's indent rule

--- hot/only-dev-server.js
+++ hot/only-dev-server.js
@@ -30,9 +30,9 @@ if(module.hot) {
 				}
 			}).catch(function(err) {
 				if(module.hot.status() in {
-						abort: 1,
-						fail: 1
-					}) {
+					abort: 1,
+					fail: 1
+				}) {
 					console.warn("[HMR] Cannot apply update. Need to do a full reload!");
 					console.warn("[HMR] " + err.stack || err.message);
 				} else {
@@ -41,9 +41,9 @@ if(module.hot) {
 			});
 		}).catch(function(err) {
 			if(module.hot.status() in {
-					abort: 1,
-					fail: 1
-				}) {
+				abort: 1,
+				fail: 1
+			}) {
 				console.warn("[HMR] Cannot check for update. Need to do a full reload!");
 				console.warn("[HMR] " + err.stack || err.message);
 			} else {
