5102bf132cf19821ce88365e2b1f609d945f5739
Tobias Koppers <tobias.koppers@googlemail.com>
2016-06-24 01:52:28 +0200

    warn when name is imported that is not exported

---
+++ test/cases/parsing/harmony-info/warnings.js
@@ -0,0 +1,5 @@
+module.exports = [
+	[/export 'default' \(imported as 'def'\) was not found in '\.\/module'/],
+	[/export 'e' was not found in '\.\/module'/],
+	[/export 'a' \(imported as 'aa'\) was not found in '\.\/module'/]
+];
