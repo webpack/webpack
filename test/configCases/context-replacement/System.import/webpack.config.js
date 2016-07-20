f8e3bdf5e752c3d4d3a3def67cad97ddfc0f5efa
Tobias Koppers <tobias.koppers@googlemail.com>
2016-07-20 12:52:01 +0200

    added test for System.import and ContextReplacementPlugin

---
+++ test/configCases/context-replacement/System.import/webpack.config.js
@@ -0,0 +1,10 @@
+var path = require("path");
+var webpack = require("../../../../");
+
+module.exports = {
+	plugins: [
+		new webpack.ContextReplacementPlugin(/context-replacement/, path.resolve(__dirname, "modules"), {
+			"a": "./module-b"
+		})
+	]
+};
