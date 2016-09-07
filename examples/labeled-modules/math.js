a14e563f67292cf2bdfe28b01abd8cb5fee1e20e
Tobias Koppers <tobias.koppers@googlemail.com>
2016-09-07 12:25:02 +0200

    removed LabeledModules

--- examples/labeled-modules/math.js
+++
@@ -1,7 +0,0 @@
-exports: function add() {
-    var sum = 0, i = 0, args = arguments, l = args.length;
-    while (i < l) {
-        sum += args[i++];
-    }
-    return sum;
-};
--- examples/mixed/amd.js
+++ examples/mixed/amd.js
@@ -1,11 +1,10 @@
 // AMD Module Format
 define(
 	"app/amd", // anonym is also supported
-	["./commonjs", "./labeled", "./harmony"],
-	function(commonjs1, labeled1, harmony1) {
+	["./commonjs", "./harmony"],
+	function(commonjs1, harmony1) {
 		// but you can use CommonJs-style requires:
 		var commonjs2 = require("./commonjs");
-		var labeled2 = require("./labeled");
 		var harmony2 = require("./harmony");
 		// Do something...
 		return 456;
