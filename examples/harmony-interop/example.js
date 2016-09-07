ac6b9541b3abbd9efdcdfb029ff1b507dfd7904e
Tobias Koppers <tobias.koppers@googlemail.com>
2016-09-07 11:47:52 +0200

    fix examples add harmony interop example remove one dedupe example

---
+++ examples/harmony-interop/example.js
@@ -0,0 +1,16 @@
+// harmony module
+
+// import from CommonJs module
+import fs from "./fs";
+import { readFile } from "./fs";
+import * as fs2 from "./fs";
+fs.readFile("file");
+readFile("file");
+fs2.readFile("file");
+
+// import from harmony module
+import { readFile as readFile2 } from "./reexport-commonjs";
+readFile2("file");
+
+// import a CommonJs module for sideeffects
+import "./example2";
