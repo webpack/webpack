d53c420cb5684268d0bbcbdc15784ccaba280f61
Sean Larkin <sean.larkin@cuw.edu>
2016-10-27 11:26:28 -0500

    fix(travis): fix full version of node v0.12

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [ "$TRAVIS_NODE_VERSIONNODEV":0:1 == "0.12" ];
+if [ "$TRAVIS_NODE_VERSIONNODEV":0:1 == "v0.12.17" ];
 then npm install && npm link && npm link webpack;
 else npm install yarn -g && yarn install && yarn link || true && yarn link webpack;
 fi
