6e530076c1daae19ff9349511fe8053f9f228775
Sean Larkin <sean.larkin@cuw.edu>
2016-10-27 12:13:46 -0500

    fix(travis): brute force install for INVALIDPEERs

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -2,7 +2,7 @@
 set -ev
 
 if [ "$TRAVIS_NODE_VERSION" == "v0.12.17" ]; then 
-    npm install && npm link && npm link webpack;
+    npm install --force && npm link && npm link webpack;
 else 
     npm install yarn -g && yarn install && yarn link || true && yarn link webpack;
 fi
