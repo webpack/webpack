2961bf7694caaa9fe337cf37c879208f38551b5d
Sean Larkin <sean.larkin@cuw.edu>
2016-10-27 11:06:08 -0500

    fix(travis): correct bash interpolation for unary statements

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -2,7 +2,7 @@
 set -ev
 set NODEV = $(node -v)
 
-if [ "${$NODEV:0:1}" == "0.12" ];
+if [ "$NODEV":0:1 == "0.12" ];
 then npm install && npm link && npm link webpack;
 else npm install yarn -g && yarn install && yarn link || true && yarn link webpack;
 fi
