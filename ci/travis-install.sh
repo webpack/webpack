cc8bd9a7ce1ad29c05bcf0ddcad59b1ffcb5291c
Sean Larkin <sean.larkin@cuw.edu>
2016-10-27 12:02:34 -0500

    fix(travis): use bash instead of sh

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
 
-if [ "$TRAVIS_NODE_VERSION" == "v0.12.17" ];
-then npm install && npm link && npm link webpack;
-else npm install yarn -g && yarn install && yarn link || true && yarn link webpack;
+if [ "$TRAVIS_NODE_VERSION" == "v0.12.17" ]; then 
+    npm install && npm link && npm link webpack;
+else 
+    npm install yarn -g && yarn install && yarn link || true && yarn link webpack;
 fi
