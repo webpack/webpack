67b2f59ce5864919c6270255b1c3799bf67d5137
zhaoshengjun <zhao.shengjun@gmail.com>
2016-11-09 22:20:12 +1000

    chore(): Refactor code and fix test filter

--- test/helpers/supportDefaultAssignment.js
+++ test/helpers/supportDefaultAssignment.js
@@ -1,6 +1,8 @@
 module.exports = function supportDefaultAssignment() {
 	try {
-		return true;
+		var E = eval("class E { toString() { return 'default' } }")
+		var f1 = eval("(function f1({a, b = E}) {return new b().toString();})")
+		return f1({a: "test"}) === "default" ;
 	} catch(e) {
 		return false;
 	}
