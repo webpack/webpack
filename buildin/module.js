82f8197996f672fff59db056b901324029baafe7
webpatch <webpatch@sina.com>
2016-11-13 19:35:58 +0800

    add ie8 support

--- buildin/module.js
+++ buildin/module.js
@@ -4,16 +4,21 @@ module.exports = function(module) {
 		module.paths = [];
 		// module.parent = undefined by default
 		if(!module.children) module.children = [];
-		Object.defineProperty(module, "loaded", {
-			enumerable: true,
-			configurable: false,
-			get: function() { return module.l; }
-		});
-		Object.defineProperty(module, "id", {
-			enumerable: true,
-			configurable: false,
-			get: function() { return module.i; }
-		});
+		try{
+      Object.defineProperty(module, "loaded", {
+        enumerable: true,
+        configurable: false,
+        get: function() { return module.l; }
+      });
+      Object.defineProperty(module, "id", {
+        enumerable: true,
+        configurable: false,
+        get: function() { return module.i; }
+      });
+		}catch (e){
+      module["loaded"] = function() { return module.l; };
+      module["id"] = function() { return module.i; };
+		}
 		module.webpackPolyfill = 1;
 	}
 	return module;
