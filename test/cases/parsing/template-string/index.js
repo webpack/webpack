f39d007e0283164d0fd9ee8614d3824fbf37f982
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2016-11-17 01:22:38 +0900

    Improve tests, check AMD and CommonJS too

--- test/cases/parsing/template-string/index.js
+++ test/cases/parsing/template-string/index.js
@@ -2,9 +2,11 @@ var should = require('should')
 
 it("should parse template strings in System.import", function(done) {
 	var name = "abc".split("");
+	var suffix = "Test";
 	Promise.all([
 		System.import(`./abc/${name[0]}${name[1]}${name[2]}Test`),
-		System.import(String.raw`./${name.join("")}/${name.join("")}Test`)
+		System.import(String.raw`./${name.join("")}/${name.join("")}Test`),
+		System.import(String.raw`./abc/${name.join("")}${suffix}`)
 	])
 	.then(function (imports) {
 		for (var i = 0; i < imports.length; i++) {
@@ -13,3 +15,6 @@ it("should parse template strings in System.import", function(done) {
 	})
 	.then(function () { done(); }, done)
 });
+
+require("./cjs")
+require("./amd")
