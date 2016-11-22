d31d9cbb9078c976649b7779958ccd44b4f233c2
Tobias Koppers <tobias.koppers@googlemail.com>
2016-11-22 17:58:24 +0100

    added resourceQuery condition added function use to schema

---
+++ test/configCases/rule-set/query/index.js
@@ -0,0 +1,11 @@
+it("should match rule with resource query", function() {
+	var a1 = require("./a");
+	a1.should.be.eql([
+		"a"
+	]);
+	var a2 = require("./a?loader");
+	a2.should.be.eql([
+		"a",
+		"?query"
+	]);
+});
