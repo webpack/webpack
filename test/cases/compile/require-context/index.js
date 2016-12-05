132272b862fa1fd819e212022b2256e933139512
chuckd <chuckd@us.ibm.com>
2016-12-05 15:34:48 -0500

    Update unit test to use CommonsChunkPlugin

--- test/cases/compile/require-context/index.js
+++
@@ -1,8 +0,0 @@
-it("should maintain require context", function() {
-	var context = {foo: "bar"};
-	(function(){
-		require(["./a", "./b"], function(a, b) {
-			this.foo.should.eql("bar");
-		}.bind(this));
-	}).call(context);
-});
