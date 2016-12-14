980c2a9c8537245639f0076a8938efd364403f26
Tobias Koppers <tobias.koppers@googlemail.com>
2016-12-14 12:03:24 +0100

    add test cases and fixes overwrite

--- test/configCases/loaders/issue-3320/index.js
+++ test/configCases/loaders/issue-3320/index.js
@@ -1,11 +1,23 @@
-it("should resolve aliased loader module with query (issue-3320)", function() {
+it("should resolve aliased loader module with query", function() {
 	var foo = require('./a');
 
 	foo.should.be.eql("someMessage");
 });
 
-it("should favor explicit loader query over aliased query (issue-3320)", function() {
+it("should favor explicit loader query over aliased query (options in rule)", function() {
 	var foo = require('./b');
+
+	foo.should.be.eql("someOtherMessage");
+});
+
+it("should favor explicit loader query over aliased query (inline query in rule)", function() {
+	var foo = require('./b2');
+
+	foo.should.be.eql("someOtherMessage");
+});
+
+it("should favor explicit loader query over aliased query (inline query in rule.use)", function() {
+	var foo = require('./b3');
 
 	foo.should.be.eql("someOtherMessage");
 });
