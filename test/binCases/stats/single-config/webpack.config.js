35e27fac78fa23a0560e7c34c05d0a030dc09a09
Sean Larkin <sean.larkin@cuw.edu>
2017-01-02 15:02:49 -0600

    reworked the binCases infra. Now ARGUMENTS.json can hold commands

--- test/binCases/stats/single-config/webpack.config.js
+++ test/binCases/stats/single-config/webpack.config.js
@@ -1,12 +1,10 @@
 var path = require("path");
 
-module.exports = [
-	{
-		entry: path.resolve(__dirname,"./index"),
-		stats: {
-			assets: true,
-			colors: true,
-			chunks: true
-		}
+module.exports = {
+	entry: path.resolve(__dirname,"./index"),
+	stats: {
+		assets: true,
+		colors: true,
+		chunks: true
 	}
-];
+}
