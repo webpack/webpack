7034daea7daf1bc573cea7096ec98a4ffe575e5a
Sean Larkin <sean.larkin@cuw.edu>
2017-01-02 15:02:49 -0600

    reworked the binCases infra. Now ARGUMENTS.json can hold commands

--- test/BinTestCases.test.js
+++ test/BinTestCases.test.js
@@ -4,46 +4,54 @@
 const should = require("should");
 const path = require("path");
 const fs = require("fs");
-const childProcess = require("child_process");
+const defaultArgs = require("./binCases/ARGUMENTS.json");
 
-describe("BinTestCases", () => {
+describe("BinTestCases", function() {
 	const casesPath = path.join(__dirname, "binCases");
-	let categories = fs.readdirSync(casesPath);
+	let categories = fs.readdirSync(casesPath).filter((folder) => {
+		return fs.statSync(path.join(casesPath, folder)).isDirectory()
+	});
 	categories = categories.map(cat => {
 		return {
 			name: cat,
 			tests: fs.readdirSync(path.join(casesPath, cat)).filter(function(folder) {
-				return folder.indexOf("_") < 0;
+				return folder.indexOf("_") < 0 || folder.indexOf("ARGUMENTS") < 0;
 			})
 		};
 	});
 
 	categories.forEach(category => {
-		describe(category.name, () => {
+		describe(category.name, function() {
+
 			category.tests.forEach(testName => {
-				let suite = describe(testName, () => {});
-				it(`${testName} should compile from bin`, function(done) {
-					this.timeout(30000);
-					const testDirectory = path.join(casesPath, category.name, testName);
-					const outputDirectory = path.join(__dirname, "js", "bin", category.name, testName);
-					const configPath = path.join(testDirectory, "webpack.config.js");
-					const outputFilename = "[name].chunk.js";
+				let suite = describe(testName, function() {});
+				let testArgs = defaultArgs;
+				let outputPath = path.join(path.resolve("../../js/bin"), category.name, testName);
+				const testDirectory = path.join(casesPath, category.name, testName);
+				const execHandler = require(path.join(testDirectory, "test.js"));
+				const execOptions = {
+					cwd: `${path.resolve("./", testDirectory)}`
+				};
 
-					function execHandler(error, stdout, stderr) {
-						if(stderr)
-							throw new Error(error);
-						if(error !== null)
-							throw new Error(error);
+				try { // check if test specfic args exist
+					testArgs = Object.assign(
+						testArgs, require(path.join(testDirectory, "ARGUMENTS.json"))
+					);
+				} catch(e) {}
 
-						stdout.should.be.ok;
-						stderr.should.be.empty;
-						done();
-					}
-
-					childProcess.exec(`node ./bin/webpack.js --config ${configPath} --output-filename ${outputFilename} --output-path ${outputDirectory}`, execHandler);
-				});
+				execHandler(`${path.resolve(process.cwd(), "bin/webpack.js")}`, argsToArray(testArgs).concat(["--output-path", `${outputPath}`]), execOptions);
 			});
 		});
 	});
-
 });
+
+function argsToArray(argsObject) {
+	const argsArray = [];
+
+	for(let argKey in argsObject) {
+		argsArray.push(argKey);
+		argsArray.push(argsObject[argKey]);
+	}
+
+	return argsArray;
+}
---
+++ test/binCases/stats/single-config/test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/*globals it*/
+module.exports = function validateBinExec(cmd, args, opts) {
+	const spawn = require("child_process").spawn;
+
+	it("should compile successfully", function(done) {
+		this.timeout(20000);
+		let child = spawn(cmd, args, opts);
+
+		child.on("close", function(code) {
+			code.should.be.oneOf(0, 1);
+			done();
+		});
+
+		child.on("error", function(error) {
+			throw new Error(error);
+		});
+
+		child.stdout.on("data", (data) => {
+			data.toString().should.be.empty;
+			data.toString().should.be.ok;
+		});
+
+		child.stderr.on("data", (data) => {
+			data.toString().should.be.ok;
+			throw new Error(data.toString());
+		});
+	});
+}
+
+
