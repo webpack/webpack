082e633b4b361b0f5704cfa7705329846a81f600
Jack Moore <jtmthf@mst.edu>
2017-01-03 10:52:55 -0800

    Improved test coverage for WatchIgnorePlugin Now tests ignoring of dirs

--- test/watchCases/plugins/watch-ignore-plugin/0/index.js
+++ test/watchCases/plugins/watch-ignore-plugin/0/index.js
@@ -1,6 +1,8 @@
 import value from "./file"
 import a from "./a"
-it("should ignore change to file", function() {
+const req = require.context("./foo", false, /^.*\.js$/);
+it("should ignore change to file and directory", function() {
 	a.should.be.eql(+WATCH_STEP);
+	req.keys().should.be.deepEqual(["./0.js"])
 	value.should.be.eql(1);
 });
