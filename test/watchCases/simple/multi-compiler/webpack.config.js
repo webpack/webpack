9a688a44fe56b4552772673684f48fd3ab74f360
Yuriy Ostapenko <yuo@scalepoint.com>
2017-01-04 21:41:32 +0100

    Fix whitespace

--- test/watchCases/simple/multi-compiler/webpack.config.js
+++ test/watchCases/simple/multi-compiler/webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = [{
-    name: "changing",
-    entry: "./index.js",
-    output: {
-        filename: "./bundle.js"
-    }
+	name: "changing",
+	entry: "./index.js",
+	output: {
+		filename: "./bundle.js"
+	}
 }, {
-    name: "static",
-    entry: "./static-file.js",
-    output: {
-        filename: "./static.js"
-    }
+	name: "static",
+	entry: "./static-file.js",
+	output: {
+		filename: "./static.js"
+	}
 }];
