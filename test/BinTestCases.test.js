dca14d6a3670688d1efbb09006dd372b448c6a2b
Alistair Brown <github@alistairjcbrown.com>
2017-01-08 01:36:11 +0000

    Simplify test files and move to opts parser
    Move command execution into root test runner to remove boilerplate code
    from specific test files. Move from arguments JSON hash to opts file
    using mochas opts parser.

--- test/BinTestCases.test.js
+++ test/BinTestCases.test.js
@@ -4,54 +4,96 @@
 const should = require("should");
 const path = require("path");
 const fs = require("fs");
-const defaultArgs = require("./binCases/ARGUMENTS.json");
+const spawn = require("child_process").spawn;
+
+function loadOptsFile(optsPath) {
+	// Options file parser from Mocha
+	// https://github.com/mochajs/mocha/blob/2bb2b9fa35818db7a02e5068364b0c417436b1af/bin/options.js#L25-L31
+	return fs.readFileSync(optsPath, 'utf8')
+		.replace(/\\\s/g, '%20')
+		.split(/\s/)
+		.filter(Boolean)
+		.map(function(value) {
+			return value.replace(/%20/g, ' ');
+		});
+}
+
+function getTestSpecificArguments(testDirectory) {
+	try {
+		return loadOptsFile(path.join(testDirectory, "test.opts"));
+	} catch(e) {
+		return [];
+	}
+}
+
+const casesPath = path.join(__dirname, "binCases");
+const defaultArgs = loadOptsFile(path.join(casesPath, "test.opts"));
 
 describe("BinTestCases", function() {
-	const casesPath = path.join(__dirname, "binCases");
-	let categories = fs.readdirSync(casesPath).filter((folder) => {
+	const categoryDirectories = fs.readdirSync(casesPath).filter((folder) => {
 		return fs.statSync(path.join(casesPath, folder)).isDirectory()
 	});
-	categories = categories.map(cat => {
+
+	const categories = categoryDirectories.map(function(categoryDirectory) {
 		return {
-			name: cat,
-			tests: fs.readdirSync(path.join(casesPath, cat)).filter(function(folder) {
-				return folder.indexOf("_") < 0 || folder.indexOf("ARGUMENTS") < 0;
-			})
+			name: categoryDirectory,
+			tests: fs.readdirSync(path.join(casesPath, categoryDirectory))
 		};
 	});
 
-	categories.forEach(category => {
+	categories.forEach(function(category) {
 		describe(category.name, function() {
 
-			category.tests.forEach(testName => {
-				let suite = describe(testName, function() {});
-				let testArgs = defaultArgs;
-				let outputPath = path.join(path.resolve("../../js/bin"), category.name, testName);
+			category.tests.forEach(function(testName) {
 				const testDirectory = path.join(casesPath, category.name, testName);
-				const execHandler = require(path.join(testDirectory, "test.js"));
-				const execOptions = {
-					cwd: `${path.resolve("./", testDirectory)}`
+				const testArgs = defaultArgs.concat(getTestSpecificArguments(testDirectory));
+				const testAssertions = require(path.join(testDirectory, "test.js"));
+				const outputPath = path.join(path.resolve(casesPath, "../js/bin"), category.name, testName);
+
+				const cmd = `${path.resolve(process.cwd(), "bin/webpack.js")}`;
+				const args = testArgs.concat(["--output-path", `${outputPath}`]);
+				const opts = {
+					cwd: path.resolve("./", testDirectory)
 				};
 
-				try { // check if test specfic args exist
-					testArgs = Object.assign(
-						testArgs, require(path.join(testDirectory, "ARGUMENTS.json"))
-					);
-				} catch(e) {}
+				const env = {
+					stdout: [],
+					stderr: [],
+					error: []
+				};
 
-				execHandler(`${path.resolve(process.cwd(), "bin/webpack.js")}`, argsToArray(testArgs).concat(["--output-path", `${outputPath}`]), execOptions);
+				describe(testName, function() {
+					before(function(done) {
+						this.timeout(20000);
+						const child = spawn(cmd, args, opts);
+
+						child.on("close", function(code) {
+							env.code = code;
+							done();
+						});
+
+						child.on("error", function(error) {
+							env.error.push(error);
+						});
+
+						child.stdout.on("data", (data) => {
+							env.stdout.push(data);
+						});
+
+						child.stderr.on("data", (data) => {
+							env.stderr.push(data);
+						});
+					});
+
+					it("should not cause any errors", function() {
+						should(env.error).be.empty();
+					});
+
+					it("should run successfully", function() {
+						testAssertions(env.code, env.stdout, env.stderr);
+					});
+				});
 			});
 		});
 	});
 });
-
-function argsToArray(argsObject) {
-	const argsArray = [];
-
-	for(let argKey in argsObject) {
-		argsArray.push(argKey);
-		argsArray.push(argsObject[argKey]);
-	}
-
-	return argsArray;
-}
