cbd36a89888b424a29c5971e1ce105fd732c0955
Alistair Brown <github@alistairjcbrown.com>
2017-01-08 02:11:53 +0000

    Add tests for CachePlugin (#3840)

--- test/helpers/applyPluginWithOptions.js
+++ test/helpers/applyPluginWithOptions.js
@@ -4,5 +4,10 @@ module.exports = function applyPluginWithOptions(Plugin) {
 	var plugin = new (Function.prototype.bind.apply(Plugin, arguments));
 	var pluginEnvironment = new PluginEnvironment();
 	plugin.apply(pluginEnvironment.getEnvironmentStub());
+
+	var env = (this === global) ? {} : this;
+	env.plugin = plugin;
+	env.pluginEnvironment = pluginEnvironment;
+
 	return pluginEnvironment.getEventBindings();
 };
