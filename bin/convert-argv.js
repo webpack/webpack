cfbbc1b06dfab4921acabe133ebccd1c0e92fb62
Clayton Watts <cletusw@gmail.com>
2017-01-10 10:38:35 -0700

    Support UglifyJsPlugin with no arguments in CLI --plugin
    UglifyJsPlugin checks `typeof options !== "object"`, which doesn't work as expected with `null` since its type is `"object"`. https://github.com/webpack/webpack/blob/a1dca894d9a04f55529e4b7c44dcabe2c1b439a1/lib/optimize/UglifyJsPlugin.js#L16
    
    See https://github.com/webpack/webpack/commit/48e17ab308a996928c965c7f29a07735af1b9391#commitcomment-20424582

--- bin/convert-argv.js
+++ bin/convert-argv.js
@@ -216,7 +216,7 @@ module.exports = function(optimist, argv, convertOptions) {
 
 		function loadPlugin(name) {
 			var loadUtils = require("loader-utils");
-			var args = null;
+			var args;
 			try {
 				var p = name && name.indexOf("?");
 				if(p > -1) {
