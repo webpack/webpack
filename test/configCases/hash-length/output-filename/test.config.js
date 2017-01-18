8f31fd18cca28286dbb8c1b3718abfe9cab5aa13
Jure Triglav <juretriglav@gmail.com>
2017-01-18 14:16:11 +0100

    Add more tests for hash and hashWithLength generation

--- test/configCases/hash-length/output-filename/test.config.js
+++ test/configCases/hash-length/output-filename/test.config.js
@@ -1,17 +1,37 @@
 var fs = require("fs");
 require("should");
 
+var verifyFilenameLength = function (files, regex, expectedNameLength) {
+	for(var j = 0, file; j < files.length; j++) {
+		file = files[j];
+		if (regex.test(file)) {
+			file.should.match(new RegExp("^.{" + expectedNameLength + "}$"));
+			return "./" + file;
+		}
+	}
+}
+
 module.exports = {
 	findBundle: function(i, options) {
 		var files = fs.readdirSync(options.output.path);
+
 		var expectedNameLength = options.amd.expectedFilenameLength;
-		var bundleDetect = new RegExp("^bundle" + i, "i");
-		for(var j = 0, file; j < files.length; j++) {
-			file = files[j];
-			if (bundleDetect.test(file)) {
-				file.should.match(new RegExp("^.{" + expectedNameLength + "}$"));
-				return "./" + file;
-			}
+		var bundleDetects = [{
+			regex: new RegExp("^bundle" + i, "i"),
+			expectedNameLength: options.amd.expectedFilenameLength
+		}, {
+			regex: new RegExp("^0.bundle" + i, "i"),
+			expectedNameLenght: options.amd.expectedChunkFilenameLenght
+		}
+		]
+
+		var bundleDetect;
+		for (bundleDetect of bundleDetects) {
+			return verifyFilenameLength(
+				files,
+				bundleDetect.regex,
+				bundleDetect.expectedNameLength
+			)
 		}
 	}
 };
