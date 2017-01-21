34e61563d941a469081fb18cd03b92e49d252438
Jure Triglav <juretriglav@gmail.com>
2017-01-21 16:02:34 +0100

    Split up searching, verification and returning the bundle. Style fixes.

--- test/configCases/hash-length/output-filename/test.config.js
+++ test/configCases/hash-length/output-filename/test.config.js
@@ -1,37 +1,41 @@
 var fs = require("fs");
 require("should");
 
-var verifyFilenameLength = function (files, regex, expectedNameLength) {
-	for(var j = 0, file; j < files.length; j++) {
-		file = files[j];
-		if (regex.test(file)) {
-			file.should.match(new RegExp("^.{" + expectedNameLength + "}$"));
-			return "./" + file;
+var findFile = function(files, regex) {
+	return files.find(function(file) {
+		if(regex.test(file)) {
+			return true;
 		}
-	}
-}
+	});
+};
+
+var verifyFilenameLength = function(filename, expectedNameLength) {
+	filename.should.match(new RegExp("^.{" + expectedNameLength + "}$"));
+};
 
 module.exports = {
 	findBundle: function(i, options) {
 		var files = fs.readdirSync(options.output.path);
 
-		var expectedNameLength = options.amd.expectedFilenameLength;
 		var bundleDetects = [{
+			regex: new RegExp("^0.bundle" + i, "i"),
+			expectedNameLength: options.amd.expectedChunkFilenameLength
+		}, {
 			regex: new RegExp("^bundle" + i, "i"),
 			expectedNameLength: options.amd.expectedFilenameLength
-		}, {
-			regex: new RegExp("^0.bundle" + i, "i"),
-			expectedNameLenght: options.amd.expectedChunkFilenameLenght
-		}
-		]
+		}];
 
 		var bundleDetect;
-		for (bundleDetect of bundleDetects) {
-			return verifyFilenameLength(
-				files,
-				bundleDetect.regex,
+		var filename;
+
+		for(bundleDetect of bundleDetects) {
+			filename = findFile(files, bundleDetect.regex);
+			verifyFilenameLength(
+				filename,
 				bundleDetect.expectedNameLength
-			)
+			);
 		}
+
+		return './' + filename;
 	}
 };
