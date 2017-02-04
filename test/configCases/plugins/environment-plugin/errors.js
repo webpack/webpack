d4b21513f886a2c37421290f5c7fbd8d45ffed3a
Federico Bertolini <fb@hellofresh.com>
2017-02-04 15:29:58 +0100

    EnvironmentPlugin tests: refactored the regular expressions of the expected errors to allow adding more tests easily

--- test/configCases/plugins/environment-plugin/errors.js
+++ test/configCases/plugins/environment-plugin/errors.js
@@ -1,11 +1,27 @@
-module.exports = [
-	[/(aaa)/, /resolve 'bbb'/],
-	[/(aaa)/, /resolve 'ccc'/],
-	[/(aaa)/, /resolve 'ddd'/],
-	[/(bbbccc)/, /resolve 'aaa'/],
-	[/(bbbccc)/, /resolve 'ddd'/],
-	[/(ddd)/, /resolve 'aaa'/],
-	[/(ddd)/, /resolve 'bbb'/],
-	[/(ddd)/, /resolve 'ccc'/],
-	[/(ddd)/, /resolve 'ddd'/],
-];
+const variables = ['aaa', 'bbb', 'ccc', 'ddd'];
+const modules = [{
+	name: 'aaa',
+	variables: ['aaa']
+}, {
+	name: 'bbbccc',
+	variables: ['bbb', 'ccc']
+}, {
+	name: 'ddd',
+	variables: []
+}];
+
+// build an array of regular expressions of expected errors
+const regex = [];
+modules.forEach(module => {
+	variables.forEach(variable => {
+		if (module.variables.indexOf(variable) === -1) {
+			// the module doesn't include the env variable, an error is expected when requiring the variable
+			regex.push([
+				new RegExp(`(${module.name})`),
+				new RegExp(`Can't resolve '${variable}'`),
+			]);
+		}
+	});
+});
+
+module.exports = regex;
