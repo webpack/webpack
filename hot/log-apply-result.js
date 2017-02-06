8609e07c4e8b263566f8eda8dee789137bf84c33
Shubheksha Jalan <jshubheksha@gmail.com>
2017-02-06 17:09:14 +0530

    refactor(ES6): log-apply-result.js

--- hot/log-apply-result.js
+++ hot/log-apply-result.js
@@ -2,28 +2,24 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+"use strict";
+
 module.exports = function(updatedModules, renewedModules) {
-	var unacceptedModules = updatedModules.filter(function(moduleId) {
+	const unacceptedModules = updatedModules.filter((moduleId) => {
 		return renewedModules && renewedModules.indexOf(moduleId) < 0;
 	});
 
 	if(unacceptedModules.length > 0) {
 		console.warn("[HMR] The following modules couldn't be hot updated: (They would need a full reload!)");
-		unacceptedModules.forEach(function(moduleId) {
-			console.warn("[HMR]  - " + moduleId);
-		});
+		unacceptedModules.forEach((moduleId) => console.warn("[HMR]  - " + moduleId));
 	}
 
 	if(!renewedModules || renewedModules.length === 0) {
 		console.log("[HMR] Nothing hot updated.");
 	} else {
 		console.log("[HMR] Updated modules:");
-		renewedModules.forEach(function(moduleId) {
-			console.log("[HMR]  - " + moduleId);
-		});
-		var numberIds = renewedModules.every(function(moduleId) {
-			return typeof moduleId === "number";
-		});
+		renewedModules.forEach((moduleId) => console.log("[HMR]  - " + moduleId));
+		const numberIds = renewedModules.every((moduleId) => typeof moduleId === "number");
 		if(numberIds)
 			console.log("[HMR] Consider using the NamedModulesPlugin for module names.");
 	}
