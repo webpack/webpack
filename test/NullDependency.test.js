b98640ade124937ffe1ba9aa97268a5cf51660c6
Sergio Rojas <serg.rojas@tutamail.com>
2017-02-17 10:02:19 -0800

    refactor(ES6):NullDependency.test.js

--- test/NullDependency.test.js
+++ test/NullDependency.test.js
@@ -1,36 +1,24 @@
-var should = require("should");
-var sinon = require("sinon");
-var NullDependency = require("../lib/dependencies/NullDependency");
+const should = require("should");
+const sinon = require("sinon");
+const NullDependency = require("../lib/dependencies/NullDependency");
 
-describe("NullDependency", function() {
-	var env;
+describe("NullDependency", () => {
+	let env;
 
-	beforeEach(function() {
-		env = {};
-	});
+	beforeEach(() => env = {});
 
-	it("is a function", function() {
-		NullDependency.should.be.a.Function();
-	});
+	it("is a function", () => NullDependency.should.be.a.Function());
 
-	describe("when created", function() {
-		beforeEach(function() {
-			env.nullDependency = new NullDependency();
-		});
+	describe("when created", () => {
+		beforeEach(() => env.nullDependency = new NullDependency());
 
-		it("has a null type", function() {
-			env.nullDependency.type.should.be.exactly("null");
-		});
+		it("has a null type", () => env.nullDependency.type.should.be.exactly("null"));
 
-		it("is not an equal resource", function() {
-			env.nullDependency.isEqualResource().should.be.False();
-		});
+		it("is not an equal resource", () => env.nullDependency.isEqualResource().should.be.False());
 
-		it("has update hash function", function() {
-			env.nullDependency.updateHash.should.be.Function();
-		});
+		it("has update hash function", () => env.nullDependency.updateHash.should.be.Function());
 
-		it("does not update hash", function() {
+		it("does not update hash", () => {
 			const hash = {
 				update: sinon.stub()
 			};
@@ -39,19 +27,13 @@ describe("NullDependency", function() {
 		});
 	});
 
-	describe("Template", function() {
-		it("is a function", function() {
-			NullDependency.Template.should.be.a.Function();
-		});
+	describe("Template", () => {
+		it("is a function", () => NullDependency.Template.should.be.a.Function());
 
-		describe("when created", function() {
-			beforeEach(function() {
-				env.nullDependencyTemplate = new NullDependency.Template();
-			});
+		describe("when created", () => {
+			beforeEach(() => env.nullDependencyTemplate = new NullDependency.Template());
 
-			it("has apply function", function() {
-				env.nullDependencyTemplate.apply.should.be.Function();
-			});
+			it("has apply function", () => env.nullDependencyTemplate.apply.should.be.Function());
 		});
 	});
 });
