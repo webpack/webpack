dd2a6a9787768c152c5d4f7e0a350714fac6514c
Tobias Koppers <tobias.koppers@googlemail.com>
2017-04-03 16:53:26 +0200

    space to tabs

--- test/cases/resolving/commomjs-local-module/index.js
+++ test/cases/resolving/commomjs-local-module/index.js
@@ -1,23 +1,23 @@
 var should = require("should");
 
 define("regular", function(require, exports, module) {
-  module.exports = "regular-module";
+	module.exports = "regular-module";
 });
 
 define("override-exports", function(require, exports, module) {
-  exports = "this one overrides exports reference";
+	exports = "this one overrides exports reference";
 });
 
 define("return-module", function(require, exports, module) {
-  return "module is returned";
+	return "module is returned";
 });
 
 
 it("should make different modules for query", function() {
-  should.strictEqual(require("regular"), "regular-module");
-  should.strictEqual(require("return-module"), "module is returned");
+	should.strictEqual(require("regular"), "regular-module");
+	should.strictEqual(require("return-module"), "module is returned");
 
-  const overrideExports = require("override-exports");
-  should(overrideExports).be.a.Object();
-  should(Object.keys(overrideExports).length).be.exactly(0);
+	const overrideExports = require("override-exports");
+	should(overrideExports).be.a.Object();
+	should(Object.keys(overrideExports).length).be.exactly(0);
 });
