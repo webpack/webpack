93001a0542c233f09af5df530fbc7666840a1eb5
Stefan Schneider <stefan.schneider@connectiv.de>
2017-04-03 12:42:15 +0200

    Removed unnecessary eslint comments

--- test/configCases/dll-plugin/1-use-dll/webpack.config.js
+++ test/configCases/dll-plugin/1-use-dll/webpack.config.js
@@ -3,7 +3,6 @@ var webpack = require("../../../../");
 module.exports = {
 	plugins: [
 		new webpack.DllReferencePlugin({
-			//eslint-disable-next-line
 			manifest: require("../../../js/config/dll-plugin/manifest0.json"),
 			name: "../0-create-dll/dll.js",
 			scope: "dll",
