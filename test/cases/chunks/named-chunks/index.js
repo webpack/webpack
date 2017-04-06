7986f4047a3e43fb9b84d65f3fe24cf5772233d1
jharris4 <harris.jb@gmail.com>
2017-04-06 10:06:21 -0400

    Update error callback named chunk tests to have unique/distinct query params and chunk names from other tests to ensure there is no contamination of other tests

--- test/cases/chunks/named-chunks/index.js
+++ test/cases/chunks/named-chunks/index.js
@@ -35,37 +35,37 @@ it("should handle empty named chunks", function(done) {
 });
 
 it("should handle named chunks when there is an error callback", function(done) {
-  var sync = false;
-  require.ensure([], function(require) {
-    require("./empty?a");
-    require("./empty?b");
-    testLoad();
-    sync = true;
-    process.nextTick(function() {
-      sync = false;
-    });
-  }, function(error) {}, "named-chunk");
-  function testLoad() {
-    require.ensure([], function(require) {
-      require("./empty?c");
-      require("./empty?d");
-      sync.should.be.ok();
-      done();
-    }, function(error) {}, "named-chunk");
-  }
+	var sync = false;
+	require.ensure([], function(require) {
+		require("./empty?e");
+		require("./empty?f");
+		testLoad();
+		sync = true;
+		process.nextTick(function() {
+			sync = false;
+		});
+	}, function(error) {}, "named-chunk-for-error-callback");
+	function testLoad() {
+		require.ensure([], function(require) {
+			require("./empty?g");
+			require("./empty?h");
+			sync.should.be.ok();
+			done();
+		}, function(error) {}, "named-chunk-for-error-callback");
+	}
 });
 
 it("should handle empty named chunks when there is an error callback", function(done) {
-  var sync = false;
-  require.ensure([], function(require) {
-    sync.should.be.ok();
-  }, function(error) {}, "empty-named-chunk");
-  require.ensure([], function(require) {
-    sync.should.be.ok();
-    done();
-  }, function(error) {}, "empty-named-chunk");
-  sync = true;
-  setImmediate(function() {
-    sync = false;
-  });
+	var sync = false;
+	require.ensure([], function(require) {
+		sync.should.be.ok();
+	}, function(error) {}, "empty-named-chunk-for-error-callback");
+	require.ensure([], function(require) {
+		sync.should.be.ok();
+		done();
+	}, function(error) {}, "empty-named-chunk-for-error-callback");
+	sync = true;
+	setImmediate(function() {
+		sync = false;
+	});
 });
