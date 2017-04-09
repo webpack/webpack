b07e0911d893af2ef40511bba67b7a930a590871
Tim Sebastian <tim.sebastian@gmail.com>
2017-04-09 13:51:46 +1000

    fixup eslint issues

--- test/cases/loaders/issue-2299/loader/index.js
+++ test/cases/loaders/issue-2299/loader/index.js
@@ -1,5 +1,4 @@
-var path = require('path');
-var async = require('async');
+var async = require("async");
 module.exports = function(content) {
 	var cb = this.async();
 	var json = JSON.parse(content);
@@ -7,20 +6,20 @@ module.exports = function(content) {
 		json.imports,
 		function(url, callback) {
 			this.loadModule(url, function(err, source, map, module) {
-				if (err) {
+				if(err) {
 					return callback(err);
 				}
 				callback(null, this.exec(source, url));
-			}.bind(this))
+			}.bind(this));
 		}.bind(this),
 		function(err, results) {
-			if (err) {
+			if(err) {
 				return cb(err);
 			}
 			// Combine all the results into one object and return it
-			cb(null, 'module.exports = ' + JSON.stringify(results.reduce(function(prev, result) {
+			cb(null, "module.exports = " + JSON.stringify(results.reduce(function(prev, result) {
 				return Object.assign({}, prev, result);
 			}, json)));
 		}
 	);
-}
+};
