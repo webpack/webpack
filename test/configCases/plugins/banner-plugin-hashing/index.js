e8ab3989003174a10b19cb5002cdd1f28b504ed9
Travis Matthews <trav.matth@gmail.com>
2017-04-11 15:02:22 -0400

    adding rest of interpolation func, tests

--- test/configCases/plugins/banner-plugin-hashing/index.js
+++ test/configCases/plugins/banner-plugin-hashing/index.js
@@ -1,13 +1,27 @@
-it("should interpolate file hash in bundle0 chunk", function(done) {
-	var fs = require("fs");
-	var source = fs
-		.readFileSync(__filename, "utf-8")
-		.split("\n")
-		.slice(0,1)[0];
+const parseBanner = (banner) => {
+	return banner
+		.slice(4,-3) // remove comment syntax
+		.split(", ") // into key:val pairs
+		.map(n => n.split(":")) //[key, val]
+		.reduce((acc, val) => {
+			acc[val[0]] = val[1];
+			return acc;
+		}, {}); // { key: val }
+};
 
-	//debugger
-	source.should.not.containEql("hash");
-	done()
+var source = require("fs")
+	.readFileSync(__filename, "utf-8")
+	.split("\n")
+	.slice(0,1)[0];
+
+const banner = parseBanner(source)
+
+it("should interpolate file hash in bundle0 chunk", function() {
+	banner["hash"].should.not.containEql("[hash]");
+});
+
+it("should interpolate chunkHash in bundle0 chunk", function() {
+	banner["chunkhash"].should.not.containEql("[chunkhash]");
 });
 
 require.include("./test.js");
