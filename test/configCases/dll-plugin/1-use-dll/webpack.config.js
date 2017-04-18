aea0e641ab6d4ceb6a062b8a4c4eff50092d719b
Martin Veith <ndresx@gmail.com>
2017-04-18 22:35:52 +0200

    Added eslint-disable-line for in dll-plugin test generated file

--- test/configCases/dll-plugin/1-use-dll/webpack.config.js
+++ test/configCases/dll-plugin/1-use-dll/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("../../../../");
 module.exports = {
 	plugins: [
 		new webpack.DllReferencePlugin({
-			manifest: require("../../../js/config/dll-plugin/manifest0.json"),
+			manifest: require("../../../js/config/dll-plugin/manifest0.json"), // eslint-disable-line node/no-missing-require
 			name: "../0-create-dll/dll.js",
 			scope: "dll",
 			sourceType: "commonjs2",
