19d55f7c7453b793c377de2fb2bcd52e4bdab7b4
Arete Code <aretecode@gmail.com>
2017-04-19 04:13:00 -0700

    🆙 updated dll example descriptions on dll-user

--- examples/dll-user/webpack.config.js
+++ examples/dll-user/webpack.config.js
@@ -4,11 +4,11 @@ module.exports = {
 	plugins: [
 		new webpack.DllReferencePlugin({
 			context: path.join(__dirname, "..", "dll"),
-			manifest: require("../dll/js/alpha-manifest.json") // eslint-disable-line
+			manifest: require("../dll/js/alpha-manifest.json")
 		}),
 		new webpack.DllReferencePlugin({
 			scope: "beta",
-			manifest: require("../dll/js/beta-manifest.json"), // eslint-disable-line
+			manifest: require("../dll/js/beta-manifest.json"),
 			extensions: [".js", ".jsx"]
 		})
 	]
