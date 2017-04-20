e1b71a2e1c4dc56a1c3b6d8f22eedf578e3da91d
Evan Scott <probablyup@gmail.com>
2017-04-20 04:38:37 -0400

    test: fix bad test assertion translation

--- test/binCases/watch/multi-config-watch-opt/test.js
+++ test/binCases/watch/multi-config-watch-opt/test.js
@@ -2,8 +2,8 @@
 
 module.exports = function testAssertions(stdout, stderr, done) {
 	expect(stdout).toBeTruthy();
-	expect(stdout[0]).toBe("");
-	expect(stdout[1]).toBe("Webpack is watching the files…");
+	expect(stdout[0]).toBeFalsy();
+	expect(stdout[1]).toContain("Webpack is watching the files…");
 
 	expect(stderr).toHaveLength(0);
 	done();
