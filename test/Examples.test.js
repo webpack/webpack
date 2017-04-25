a3e83710b4abd87139555746c9c48d2fd8090b90
Arete Code <aretecode@gmail.com>
2017-04-24 19:47:06 -0700

    🔬ℹ️️ example test runner updates and comments
    ℹ️️ comments to example test runner
    🔬 update example test runner to prefer example/test.js over webpack.config

--- test/Examples.test.js
+++ test/Examples.test.js
@@ -7,42 +7,83 @@ const fs = require("fs");
 const webpack = require("../");
 
 describe("Examples", () => {
-	const examples = fs.readdirSync(path.join(__dirname, "..", "examples")).map((name) =>
-		path.join(__dirname, "..", "examples", name)).filter((p) =>
-		fs.statSync(p).isDirectory() && fs.existsSync(path.join(p, "build.js")));
+	/**
+	 * @desc find all examples with a build.js
+	 * @type {Array<string>} example tests
+	 */
+	const examples = fs
+		.readdirSync(path.join(__dirname, "..", "examples"))
+		.map(name => path.join(__dirname, "..", "examples", name))
+		.filter(p =>
+			fs.statSync(p).isDirectory() && fs.existsSync(path.join(p, "build.js"))
+		);
 
-	examples.forEach((examplePath) => {
+	/**
+	 * @desc default to empty object
+	 *
+	 * when we have a `test.js`
+	 * 	call it with the test callback, return/break
+	 *
+	 * when the example has a `webpack.config`,
+	 * 	load it
+	 *  when array, compile each config
+	 *
+	 * otherwise,
+	 * 	fallback to default empty object
+	 */
+	examples.forEach(examplePath => {
 		it("should compile " + path.basename(examplePath), function(done) {
 			this.timeout(20000);
+
 			let options = {};
 			let webpackConfigPath = path.join(examplePath, "webpack.config.js");
-			webpackConfigPath = webpackConfigPath.substr(0, 1).toUpperCase() + webpackConfigPath.substr(1);
-			if(fs.existsSync(webpackConfigPath))
-				options = require(webpackConfigPath);
-			if(Array.isArray(options))
-				options.forEach(processOptions);
-			else
-				processOptions(options);
+			let testFilePath = path.join(examplePath, "test.js");
+			webpackConfigPath =
+				webpackConfigPath.substr(0, 1).toUpperCase() +
+				webpackConfigPath.substr(1);
 
+			if(fs.existsSync(testFilePath)) {
+				const test = require(testFilePath);
+				return test(done, webpack);
+			}
+
+			if(fs.existsSync(webpackConfigPath)) {
+				options = require(webpackConfigPath);
+			}
+			if(Array.isArray(options)) {
+				options.forEach(processOptions);
+			} else {
+				processOptions(options);
+			}
+
+			/**
+			 * @desc when there is no webpack.config,
+			 *       set defaults on the config
+			 * @param  {Object} options webpack options
+			 * @return {void}
+			 */
 			function processOptions(options) {
 				options.context = examplePath;
 				options.output = options.output || {};
 				options.output.pathinfo = true;
 				options.output.path = path.join(examplePath, "js");
 				options.output.publicPath = "js/";
-				if(!options.output.filename)
-					options.output.filename = "output.js";
-				if(!options.entry)
-					options.entry = "./example.js";
+				if(!options.output.filename) options.output.filename = "output.js";
+				if(!options.entry) options.entry = "./example.js";
 			}
+
+			/**
+			 * @desc compile:
+			 *       done(errors) when failing,
+			 *       done() for passing
+			 */
 			webpack(options, (err, stats) => {
 				if(err) return done(err);
 				stats = stats.toJson({
-					errorDetails: true
+					errorDetails: true,
 				});
-				if(stats.errors.length > 0) {
-					return done(new Error(stats.errors[0]));
-				}
+				if(stats.errors.length > 0) return done(new Error(stats.errors[0]));
+
 				done();
 			});
 		});
