aca7e04f62c9be81189a196e4742ae93b7dc926e
Arete Code <aretecode@gmail.com>
2017-04-30 14:38:36 -0700

    📜 update examples build all script

--- examples/buildAll.js
+++ examples/buildAll.js
@@ -1,27 +1,46 @@
-var cp = require("child_process");
-var path = require("path");
-var fs = require("fs");
+/* eslint node/no-unsupported-features: "off" */
+const { join } = require("path");
+const { statSync, readdirSync } = require("fs");
+const { execa, log } = require("../../fluents/chain/fluent-cli");
 
-var cmds = fs.readdirSync(__dirname).filter(function(dirname) {
-	return fs.statSync(path.join(__dirname, dirname)).isDirectory() && dirname !== "node_modules";
-}).sort().map(function(dirname) {
-	return "cd " + dirname + " && node build.js";
+log.registerCatch();
+const debug = false;
+
+/**
+ * @TODO could also just `require` and call `cwd`
+ *
+ * @desc
+ *   read dir
+ *   resolve to absolute path
+ *   ensure directory & not node_modules
+ *   add cwd to process.env, dereferences
+ *   call child process for each cwd
+ * @type {Array<Promise>}
+ */
+const builds = readdirSync(__dirname)
+.map(dirname => join(__dirname, dirname))
+.filter(dirname =>
+	statSync(dirname).isDirectory() &&
+	!dirname.includes("node_modules")
+)
+.sort()
+.map(dirname => Object.assign(Object.assign({}, process.env), { cwd: dirname }))
+.map(cmd => execa.shell("node build.js", cmd)
+	.then(result =>
+		log.blue(cmd.cwd).echo() &&
+		log.yellow("result stdout").data(result.stdout).echo(debug))
+);
+
+/**
+ * catch errors, when error, log stdout
+ * otherwise log done
+ * @type {Promise}
+ */
+Promise.all(builds)
+.then(() => {
+	log.green("done building all");
+})
+.catch(e => {
+	const er = e.stdout.split("\n🚨").shift().trim();
+	console.log(er);
 });
-
-var stack = function() {
-	console.log("done");
-};
-for(var i = cmds.length - 1; i >= 0; i--) {
-	var cmd = cmds[i];
-	stack = (function(next, cmd) {
-		return function() {
-			console.log(cmd);
-			cp.exec(cmd, function(error, stdout, stderr) {
-				if(error) console.error(error);
-				else if(stderr) console.error(stderr), next();
-				else next();
-			});
-		};
-	}(stack, cmd));
-}
-stack();
