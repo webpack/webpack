65f64eb8b7dfa1120986158f7fa65c81f1eb6dc5
Joe Bottigliero <joe@bottigliero.com>
2017-05-19 19:27:33 -0500

    [4886] Handles 'verbose' and 'normal' stats cases

--- test/binCases/stats/env/test.js
+++ test/binCases/stats/env/test.js
@@ -4,7 +4,7 @@ module.exports = function testAssertions(code, stdout, stderr) {
 	code.should.be.eql(0);
 
 	stdout.should.be.ok();
-	stdout[2].should.containEql("Environment: \u001b[1mfoobar\u001b[39m\u001b[22m");
+	stdout[2].should.containEql("Environment (--env): \u001b[1mfoobar\u001b[39m\u001b[22m");
 
 	stderr.should.be.empty();
 };
