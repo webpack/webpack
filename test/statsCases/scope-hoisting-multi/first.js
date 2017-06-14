5d4ba56c5f1e89c0b742d1b9d51777d0491b2fb3
Tobias Koppers <tobias.koppers@googlemail.com>
2017-06-14 22:46:26 +0200

    Allow scope hoisting to process modules in multiple chunks

---
+++ test/statsCases/scope-hoisting-multi/first.js
@@ -0,0 +1,6 @@
+import v from "./vendor";
+import c from "./common";
+import x from "./module_first";
+
+import(/* webpackChunkName: "lazy_first" */"./lazy_first");
+import(/* webpackChunkName: "lazy_shared" */"./lazy_shared");
