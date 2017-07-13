6c5dc1b4ee521fa3fd43299e4a6dd9a16a1a2270
Ryan Harvey <rrharvey@users.noreply.github.com>
2017-07-13 12:41:55 -0500

    respect log level in HMR client log groups

--- hot/log.js
+++ hot/log.js
@@ -2,20 +2,42 @@ var logLevel = "info";
 
 function dummy() {}
 
+function shouldLog(level) {
+	var shouldLog = (logLevel === "info" && level === "info") ||
+		(["info", "warning"].indexOf(logLevel) >= 0 && level === "warning") ||
+		(["info", "warning", "error"].indexOf(logLevel) >= 0 && level === "error");
+	return shouldLog;
+}
+
+function logGroup(logFn) {
+	return function(level, msg) {
+		if(shouldLog(level)) {
+			logFn(msg);
+		}
+	};
+}
+
 module.exports = function(level, msg) {
-	if(logLevel === "info" && level === "info")
-		return console.log(msg);
-	if(["info", "warning"].indexOf(logLevel) >= 0 && level === "warning")
-		return console.warn(msg);
-	if(["info", "warning", "error"].indexOf(logLevel) >= 0 && level === "error")
-		return console.error(msg);
+	if(shouldLog(level)) {
+		if(level === "info") {
+			console.log(msg);
+		} else if(level === "warning") {
+			console.warn(msg);
+		} else if(level === "error") {
+			console.error(msg);
+		}
+	}
 };
 
-module.exports.group = console.group || dummy;
+var group = console.group || dummy;
+var groupCollapsed = console.groupCollapsed || dummy;
+var groupEnd = console.groupEnd || dummy;
 
-module.exports.groupCollapsed = console.groupCollapsed || dummy;
+module.exports.group = logGroup(group);
 
-module.exports.groupEnd = console.groupEnd || dummy;
+module.exports.groupCollapsed = logGroup(groupCollapsed);
+
+module.exports.groupEnd = logGroup(groupEnd);
 
 module.exports.setLogLevel = function(level) {
 	logLevel = level;
