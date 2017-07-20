658867e7149fbfce42b8030f915423b050f409b1
Ryan Harvey <rrharvey@users.noreply.github.com>
2017-07-20 14:38:25 -0500

    make test work on all platforms

--- test/binCases/config-name/not-found/test.js
+++ test/binCases/config-name/not-found/test.js
@@ -1,8 +1,7 @@
 "use strict";
 
 module.exports = function testAssertions(code, stdout, stderr) {
-	code.should.be.exactly(255);
-
+	code.should.not.eql(0);
 	stdout.should.be.empty();
 	stderr[0].should.containEql("Configuration with name \'foo\' was not found.");
 };
