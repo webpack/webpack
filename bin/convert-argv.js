0c5725423d1190f983a2544d0e80e9ec8d2e86ec
Esben Petersen <esbenspetersen@gmail.com>
2017-07-24 21:00:55 +0200

    stop yargs from exiting process and to fix failing help-output test
    closes #5345

--- bin/convert-argv.js
+++ bin/convert-argv.js
@@ -5,6 +5,9 @@ var interpret = require("interpret");
 var prepareOptions = require("../lib/prepareOptions");
 
 module.exports = function(yargs, argv, convertOptions) {
+	if (argv["help"] === true || argv["version"] === true) {
+		return {exitByHelpOrVersion: true};
+	}
 
 	var options = [];
 
