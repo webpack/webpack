03798e21c53ac54c6e100cd64d07d19449b196a0
Tobias Koppers <tobias.koppers@googlemail.com>
2017-07-24 11:54:06 +0200

    fix useless-escape linting issues

--- test/binCases/config-name/not-found/test.js
+++ test/binCases/config-name/not-found/test.js
@@ -3,6 +3,6 @@
 module.exports = function testAssertions(code, stdout, stderr) {
 	code.should.not.eql(0);
 	stdout.should.be.empty();
-	stderr[0].should.containEql("Configuration with name \'foo\' was not found.");
+	stderr[0].should.containEql("Configuration with name 'foo' was not found.");
 };
 
