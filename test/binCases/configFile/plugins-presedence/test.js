170cc08eaae47afb72cec220b22fcff16cb6489d
Esben Petersen <esbenspetersen@gmail.com>
2017-07-24 21:46:37 +0200

    fix linting

--- test/binCases/configFile/plugins-presedence/test.js
+++ test/binCases/configFile/plugins-presedence/test.js
@@ -4,6 +4,6 @@ module.exports = function testAssertions(code, stdout, stderr) {
 	code.should.be.exactly(0);
 
 	stdout.should.be.ok();
-	stdout[6].should.containEql('ok.js')
+	stdout[6].should.containEql("ok.js");
 	stderr.should.be.empty();
 };
