83c87fc4db4aac132d47c9943f2459ee2ffa40c8
Gary Borton <gary.borton@airbnb.com>
2017-07-24 19:12:00 -0700

    [ModuleReasons] Remove ModuleReason chunk list.
    I didn't see a need for ModuleReason's to track their chunks separate from the
    module's list of chunks. Having a single source of truth seems beneficial. This
    has a double benefit of speeding up the RemoveParentModulesPlugin.
    
    Before making this change, but including the previous commit: 3160ms
    After making this change: 340ms

--- test/ModuleReason.test.js
+++ test/ModuleReason.test.js
@@ -30,48 +30,4 @@ describe("ModuleReason", () => {
 			should(myModuleReason.hasChunk(myChunk)).be.true();
 		});
 	});
-
-	describe("rewriteChunks", () => {
-		it("if old chunk is present, it is replaced with new chunks", () => {
-			myModuleReason.module.addChunk(myChunk);
-			myModuleReason.rewriteChunks(myChunk, [myChunk2]);
-
-			should(myModuleReason.hasChunk(myChunk)).be.false();
-			should(myModuleReason.hasChunk(myChunk2)).be.true();
-		});
-
-		it("if old chunk is not present, new chunks are not added", () => {
-			myModuleReason.rewriteChunks(myChunk, [myChunk2]);
-
-			should(myModuleReason.hasChunk(myChunk)).be.false();
-			should(myModuleReason.hasChunk(myChunk2)).be.false();
-		});
-
-		it("if already rewritten chunk is present, it is replaced with new chunks", () => {
-			myModuleReason.module.addChunk(myChunk);
-			myModuleReason.rewriteChunks(myChunk, [myChunk2]);
-			myModuleReason.rewriteChunks(myChunk2, [myChunk]);
-
-			should(myModuleReason.hasChunk(myChunk)).be.true();
-			should(myModuleReason.hasChunk(myChunk2)).be.false();
-		});
-	});
-
-	describe(".chunks", () => {
-		it("is null if no rewrites happen first", () => {
-			should(myModuleReason.chunks).be.Null();
-		});
-
-		it("is null if only invalid rewrites happen first", () => {
-			myModuleReason.rewriteChunks(myChunk, [myChunk2]);
-			should(myModuleReason.chunks).be.Null();
-		});
-
-		it("is an array of chunks if a valid rewrite happens", () => {
-			myModuleReason.module.addChunk(myChunk);
-			myModuleReason.rewriteChunks(myChunk, [myChunk2]);
-
-			should(myModuleReason.chunks).be.eql([myChunk2]);
-		});
-	});
 });
