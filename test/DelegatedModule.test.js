5cb7bfa81f100953e520d195ee54eac644b1bc3c
Thai Pangsakulyanont <dtinth@spacet.me>
2017-09-16 03:16:12 +0700

    add test case for DelegatedModule#updateHash
    this test case verifies that #5684 is fixed

---
+++ test/DelegatedModule.test.js
@@ -0,0 +1,30 @@
+/* globals describe, it, beforeEach */
+"use strict";
+require("should");
+const DelegatedModule = require("../lib/DelegatedModule");
+
+describe("DelegatedModule", function() {
+	describe("#updateHash", function() {
+		const sourceRequest = "dll-reference dll_e54c0fb67f8152792ad2";
+		const data = {
+			id: "/xg9"
+		};
+		const type = "require";
+		const userRequest = "./library.js";
+		let hashedText;
+		let hash;
+		beforeEach(function() {
+			hashedText = "";
+			hash = {
+				update: (text) => {
+					hashedText += text;
+				}
+			};
+			const delegatedModule = new DelegatedModule(sourceRequest, data, type, userRequest);
+			delegatedModule.updateHash(hash);
+		});
+		it("calls hash function with delegated module ID", function() {
+			hashedText.should.containEql("/xg9");
+		});
+	});
+});
