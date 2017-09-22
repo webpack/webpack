45173cafad2e4dfdef2c940837caab989236ba93
Rogelio Guzman <rogelioguzmanh@gmail.com>
2017-09-21 19:32:14 -0700

    Update to jest-runner-mocha@0.3.0

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   runner: "jest-runner-mocha",
   testMatch: ["<rootDir>/test/*.test.js"],
   coveragePathIgnorePatterns: ["**/*.runtime.js"],
-  watchPathIgnorePatterns: [".*\\.runtime.js"],
+  watchPathIgnorePatterns: [".*\\c.runtime.js", "\\/test\\/js\\/.*"],
 };
