51be6cdaf1b43683b4815e9a9b75616368d4a040
chuckd <chuckd@us.ibm.com>
2017-10-09 13:01:54 -0400

    Changes per review comments

--- test/cases/amd/namedModulesConstArrayDep/index.js
+++ test/cases/amd/namedModulesConstArrayDep/index.js
@@ -16,14 +16,14 @@ define("named4", [], function() {
 
 define("named1,named2".split(","), function(named1, named2) {
 	it("should load the named modules in const array defined dependencies", function() {
-		"named1".should.be.eql(named1);
-		"named2".should.be.eql(named2);
+		named1.should.be.eql("named1");
+		named2.should.be.eql("named2");
 	});
 
 	it("should load the named modules in const array require dependencies", function(done) {
 		require("named3,named4".split(","), function (named3, named4) {
-			"named3".should.be.eql(named3);
-			"named4".should.be.eql(named4);
+			named3.should.be.eql("named3");
+			named4.should.be.eql("named4");
 			done();
 		});
 	});
