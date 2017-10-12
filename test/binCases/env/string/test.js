9a52a162cf205ead12aa461f1da233dd11d482c7
Joe Bottigliero <joe@albert.io>
2017-10-11 21:08:45 -0500

    [tests] updates test.opts and test files for --env

--- test/binCases/env/string/test.js
+++ test/binCases/env/string/test.js
@@ -1,16 +1,14 @@
 "use strict";
 
 module.exports = function testAssertions(code, stdout, stderr) {
-	code.should.be.eql(0);
+	code.should.be.exactly(0);
 
-	stdout.should.be.ok();
+	stdout.should.be.ok();	
 	stdout[0].should.containEql("Hash: ");
 	stdout[1].should.containEql("Version: ");
 	stdout[2].should.containEql("Time: ");
 	stdout[3].should.containEql("Environment (--env): ");
 	stdout[5].should.containEql("null.js");
-	stdout[6].should.containEql("./index.js");
-	stdout[6].should.containEql("[built]");
 
 	stderr.should.be.empty();
 };
