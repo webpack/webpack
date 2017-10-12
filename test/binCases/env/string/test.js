fe0d090ee88434d13e42854bffa887043453a20b
Joe Bottigliero <joe@albert.io>
2017-10-12 08:52:47 -0500

    [tests] adds more verbose stdout equality check (includes the --env value)

--- test/binCases/env/string/test.js
+++ test/binCases/env/string/test.js
@@ -7,7 +7,7 @@ module.exports = function testAssertions(code, stdout, stderr) {
 	stdout[0].should.containEql("Hash: ");
 	stdout[1].should.containEql("Version: ");
 	stdout[2].should.containEql("Time: ");
-	stdout[3].should.containEql("Environment (--env): ");
+	stdout[3].should.containEql("Environment (--env): \"foo\"");
 	stdout[5].should.containEql("null.js");
 
 	stderr.should.be.empty();
