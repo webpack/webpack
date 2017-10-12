8171f94497a7a7d4297d2fc6c2e2cfdfd01981e5
Joe Bottigliero <joe@albert.io>
2017-10-11 21:08:45 -0500

    [tests] updates test.opts and test files for --env

--- test/binCases/env/complex/test.js
+++ test/binCases/env/complex/test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function testAssertions(code, stdout, stderr) {
-	code.should.be.eql(0);
+	code.should.be.exactly(0);
 
 	stdout.should.be.ok();
 	stdout[0].should.containEql("Hash: ");
