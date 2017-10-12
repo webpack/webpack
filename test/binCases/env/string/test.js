f85595d0d0b64c98b9f6f8cdf17e0895f519b0b7
Joe Bottigliero <joe@albert.io>
2017-10-11 21:37:01 -0500

    [lint] remove trailing space

--- test/binCases/env/string/test.js
+++ test/binCases/env/string/test.js
@@ -3,7 +3,7 @@
 module.exports = function testAssertions(code, stdout, stderr) {
 	code.should.be.exactly(0);
 
-	stdout.should.be.ok();	
+	stdout.should.be.ok();
 	stdout[0].should.containEql("Hash: ");
 	stdout[1].should.containEql("Version: ");
 	stdout[2].should.containEql("Time: ");
