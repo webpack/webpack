00daab25077628fb970e1a8516f5a073a769350d
Antoine Caron <antoine.caron@zenika.com>
2017-10-30 08:43:56 +0100

    fixup! [fix] error on HMR with es6 bundles

--- test/WebWorkerMainTemplatePlugin.unittest.js
+++ test/WebWorkerMainTemplatePlugin.unittest.js
@@ -188,8 +188,8 @@ installedChunks[chunkIds.pop()] = 1;
 				it("returns the original source with hot update callback", () => {
 					env.source.should.be.exactly(`
 moduleSource()
-var parentHotUpdateCallback = window["webpackHotUpdate"];
-window["webpackHotUpdate"] = function webpackHotUpdateCallback(chunkId, moreModules) { // eslint-disable-line no-unused-vars
+var parentHotUpdateCallback = self["webpackHotUpdate"];
+self["webpackHotUpdate"] = function webpackHotUpdateCallback(chunkId, moreModules) { // eslint-disable-line no-unused-vars
 	hotAddUpdateChunk(chunkId, moreModules);
 	if(parentHotUpdateCallback) parentHotUpdateCallback(chunkId, moreModules);
 } ;
