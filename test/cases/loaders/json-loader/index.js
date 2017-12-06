da5c8d87aab15b44d431b115196da169919df23e
Tobias Koppers <tobias.koppers@googlemail.com>
2017-12-06 20:37:58 +0100

    add tree-shaking for json modules

--- test/cases/loaders/json-loader/index.js
+++ test/cases/loaders/json-loader/index.js
@@ -11,15 +11,3 @@ it("should also work when the json extension is omitted", function() {
 	someJson.should.have.property("it", "works");
 	someJson.should.have.property("number", 42);
 });
-
-it("should still be possible to manually apply the json-loader for compatibility reasons", function() {
-	var someJson = require("json-loader!./some.json");
-	someJson.should.have.property("it", "works");
-	someJson.should.have.property("number", 42);
-});
-
-it("should still be possible to use a custom loader", function() {
-	var someJson = JSON.parse(require("raw-loader!./some.json"));
-	someJson.should.have.property("it", "works");
-	someJson.should.have.property("number", 42);
-});
