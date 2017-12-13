079b5f6275d3d5dc14768097a9545baa5be708f1
Tobias Koppers <tobias.koppers@googlemail.com>
2017-12-13 11:30:15 +0100

    remove dependencies, ask for installing webpack-cli when using the CLI

--- bin/webpack.js
+++ bin/webpack.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-require("webpack-cli");
+let webpackCliInstalled = false;
+try {
+	require.resolve("webpack-cli");
+	webpackCliInstalled = true;
+} catch(e) {
+	webpackCliInstalled = false;
+}
+
+if(webpackCliInstalled) {
+	require("webpack-cli"); // eslint-disable-line node/no-unpublished-require
+} else {
+	console.error("The CLI moved into a separate package: webpack-cli.");
+	console.error("Please install 'webpack-cli' in addition to webpack itself to use the CLI.");
+	console.error("-> When using npm: npm install webpack-cli");
+	console.error("-> When using yarn: yarn add webpack-cli");
+	process.exitCode = 1;
+}
