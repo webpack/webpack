97a95ee83ca7860959ba0566de0cda4de7078711
Joe Bottigliero <joe@albert.io>
2018-01-07 15:44:55 -0600

    [tests] fix failing status code test(s) on Windows

--- test/binCases/config-type/invalid-array/test.js
+++ test/binCases/config-type/invalid-array/test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function testAssertions(code, stdout, stderr) {
-	code.should.be.exactly(255);
+	code.should.not.eql(0);
 
 	stderr.should.not.be.empty();
 	stderr[0].should.containEql("Invalid configuration object. Webpack has been initialised using a configuration object that does not match the API schema.");
