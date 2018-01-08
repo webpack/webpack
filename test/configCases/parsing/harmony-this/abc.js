a550a8cf628fd4c5cca83370edbc1007dbcfcf56
Florent Cailhol <florent.cailhol@gmail.com>
2018-01-08 15:31:54 +0100

    Rewrite top level this to undefined in Harmony modules

--- test/configCases/parsing/harmony-this/abc.js
+++ test/configCases/parsing/harmony-this/abc.js
@@ -11,4 +11,21 @@ export {
 	b
 }
 
+export const that = this;
+export const returnThisArrow = () => this;
+export const returnThisMember = () => this.a;
+
+export class C {
+	constructor() {
+		this.x = "bar";
+	}
+	foo() {
+		return this.x;
+	}
+}
+
+export function D() {
+	this.prop = () => "ok";
+}
+
 export default returnThis;
