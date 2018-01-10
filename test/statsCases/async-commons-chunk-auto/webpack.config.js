c784a96876635d5a83501b2a237e296ae0ba7eb9
Tobias Koppers <tobias.koppers@googlemail.com>
2018-01-10 22:38:16 +0100

    expand test case to cover more cases

--- test/statsCases/async-commons-chunk-auto/webpack.config.js
+++ test/statsCases/async-commons-chunk-auto/webpack.config.js
@@ -1,31 +1,79 @@
 const path = require("path");
-module.exports = {
-	mode: "production",
-	entry: {
-		main: "./",
-		a: "./a",
-		b: "./b",
-		c: "./c"
-	},
-	output: {
-		chunkFilename: "[name].chunk.js"
-	},
-	optimization: {
-		asyncCommonsChunks: {
-			minSize: 1 // enforce all
-		},
-		initialVendorsChunk: {
-			"libs": /[\\/](xyz|x)/,
-			vendors: path.resolve(__dirname, "node_modules")
-		}
-	},
-	stats: {
-		hash: false,
-		timings: false,
-		assets: false,
-		chunks: true,
-		chunkOrigins: true,
-		entrypoints: true,
-		modules: false
-	}
+const stats = {
+	hash: false,
+	timings: false,
+	assets: false,
+	chunks: true,
+	chunkOrigins: true,
+	entrypoints: true,
+	modules: false
 };
+module.exports = [
+	{
+		name: "async-only",
+		mode: "production",
+		entry: {
+			main: "./",
+			a: "./a",
+			b: "./b",
+			c: "./c"
+		},
+		output: {
+			filename: "async-only/[name].js"
+		},
+		optimization: {
+			asyncCommonsChunks: {
+				minSize: 1 // enforce all
+			}
+		},
+		stats
+	},
+	{
+		name: "vendors1",
+		mode: "production",
+		entry: {
+			main: "./",
+			a: "./a",
+			b: "./b",
+			c: "./c"
+		},
+		output: {
+			filename: "vendors1/[name].js"
+		},
+		optimization: {
+			asyncCommonsChunks: false,
+			initialCommonsChunks: {
+				minSize: 1 // enforce all
+			},
+			initialVendorsChunk: true
+		},
+		stats
+	},
+	{
+		name: "async-and-vendor",
+		mode: "production",
+		entry: {
+			main: "./",
+			a: "./a",
+			b: "./b",
+			c: "./c",
+			vendors: "xy"
+		},
+		output: {
+			filename: "async-and-vendor/[name].js"
+		},
+		optimization: {
+			asyncCommonsChunks: {
+				minSize: 1 // enforce all
+			},
+			initialVendorsChunk: {
+				"libs": name => {
+					const match = /[\\/](xyz|x)\.js/.exec(name);
+					if(match) return "libs-" + match[1];
+				},
+				vendors: path.resolve(__dirname, "node_modules")
+			}
+		},
+		stats
+	}
+];
