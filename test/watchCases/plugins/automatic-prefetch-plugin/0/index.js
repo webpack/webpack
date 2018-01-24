7a700209742962ec487dad148c9667eb2e8d7a12
Florent Cailhol <florent.cailhol@gmail.com>
2018-01-24 23:26:49 +0100

    wip

--- test/watchCases/plugins/automatic-prefetch-plugin/0/index.js
+++ test/watchCases/plugins/automatic-prefetch-plugin/0/index.js
@@ -1,7 +1,7 @@
 it("should watch for changes", function() {
-	require("./foo/" + WATCH_STEP).should.be.eql('This is only a test.' + WATCH_STEP);
+	expect(require("./foo/" + WATCH_STEP)).toBe('This is only a test.' + WATCH_STEP);
 	if(+WATCH_STEP > 0) {
 		for(var m of STATS_JSON.modules.filter(m => /(a|b|c)\.js$/.test(m.identifier)))
-			m.prefetched.should.be.true();
+			expect(m.prefetched).toBe(true);
 	}
 });
