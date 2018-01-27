5f4b4be0a2ce290e938c106c8f74ff2e6d830bf6
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2018-01-27 16:34:20 +0100

    migrate TestCases

--- test/TestCases.test.js
+++ test/TestCases.test.js
@@ -1,13 +1,12 @@
-/* global describe, it*/
+/* global describe it fit expect */
 "use strict";
 
-require("should");
 const path = require("path");
 const fs = require("fs");
 const vm = require("vm");
 const mkdirp = require("mkdirp");
-const Test = require("mocha/lib/test");
 const checkArrayExpectation = require("./checkArrayExpectation");
+const async = require("async");
 
 const Stats = require("../lib/Stats");
 const webpack = require("../lib/webpack");
@@ -116,7 +115,6 @@ describe("TestCases", () => {
 		describe(config.name, () => {
 			categories.forEach((category) => {
 				describe(category.name, function() {
-					this.timeout(30000);
 					category.tests.filter((test) => {
 						const testDirectory = path.join(casesPath, category.name, test);
 						const filterPath = path.join(testDirectory, "test.filter.js");
@@ -126,95 +124,96 @@ describe("TestCases", () => {
 						}
 						return true;
 					}).forEach((testName) => {
-						const suite = describe(testName, () => {});
-						it(testName + " should compile", (done) => {
-							const testDirectory = path.join(casesPath, category.name, testName);
-							const outputDirectory = path.join(__dirname, "js", config.name, category.name, testName);
-							const options = {
-								context: casesPath,
-								entry: "./" + category.name + "/" + testName + "/index",
-								target: "async-node",
-								devtool: config.devtool,
-								mode: config.mode || "none",
-								optimization: config.mode ? NO_EMIT_ON_ERRORS_OPTIMIZATIONS : Object.assign({}, config.optimization, DEFAULT_OPTIMIZATIONS),
-								performance: {
-									hints: false
-								},
-								output: {
-									pathinfo: true,
-									path: outputDirectory,
-									filename: "bundle.js"
-								},
-								resolve: {
-									modules: ["web_modules", "node_modules"],
-									mainFields: ["webpack", "browser", "web", "browserify", ["jam", "main"], "main"],
-									aliasFields: ["browser"],
-									extensions: [".mjs", ".webpack.js", ".web.js", ".js", ".json"],
-									concord: true
-								},
-								resolveLoader: {
-									modules: ["web_loaders", "web_modules", "node_loaders", "node_modules"],
-									mainFields: ["webpackLoader", "webLoader", "loader", "main"],
-									extensions: [".webpack-loader.js", ".web-loader.js", ".loader.js", ".js"]
-								},
-								module: {
-									rules: [{
-										test: /\.coffee$/,
-										loader: "coffee-loader"
-									}, {
-										test: /\.jade$/,
-										loader: "jade-loader"
-									}]
-								},
-								plugins: (config.plugins || []).concat(function() {
-									this.hooks.compilation.tap("TestCasesTest", (compilation) => {
-										["optimize", "optimizeModulesBasic", "optimizeChunksBasic", "afterOptimizeTree", "afterOptimizeAssets"].forEach((hook) => {
-											compilation.hooks[hook].tap("TestCasesTest", () => compilation.checkConstraints());
+						describe(testName, () => {
+							it(testName + " should compile", (done) => {
+								const testDirectory = path.join(casesPath, category.name, testName);
+								const outputDirectory = path.join(__dirname, "js", config.name, category.name, testName);
+								const options = {
+									context: casesPath,
+									entry: "./" + category.name + "/" + testName + "/index",
+									target: "async-node",
+									devtool: config.devtool,
+									mode: config.mode || "none",
+									optimization: config.mode ? NO_EMIT_ON_ERRORS_OPTIMIZATIONS : Object.assign({}, config.optimization, DEFAULT_OPTIMIZATIONS),
+									performance: {
+										hints: false
+									},
+									output: {
+										pathinfo: true,
+										path: outputDirectory,
+										filename: "bundle.js"
+									},
+									resolve: {
+										modules: ["web_modules", "node_modules"],
+										mainFields: ["webpack", "browser", "web", "browserify", ["jam", "main"], "main"],
+										aliasFields: ["browser"],
+										extensions: [".mjs", ".webpack.js", ".web.js", ".js", ".json"],
+										concord: true
+									},
+									resolveLoader: {
+										modules: ["web_loaders", "web_modules", "node_loaders", "node_modules"],
+										mainFields: ["webpackLoader", "webLoader", "loader", "main"],
+										extensions: [".webpack-loader.js", ".web-loader.js", ".loader.js", ".js"]
+									},
+									module: {
+										rules: [{
+											test: /\.coffee$/,
+											loader: "coffee-loader"
+										}, {
+											test: /\.jade$/,
+											loader: "jade-loader"
+										}]
+									},
+									plugins: (config.plugins || []).concat(function() {
+										this.hooks.compilation.tap("TestCasesTest", (compilation) => {
+											["optimize", "optimizeModulesBasic", "optimizeChunksBasic", "afterOptimizeTree", "afterOptimizeAssets"].forEach((hook) => {
+												compilation.hooks[hook].tap("TestCasesTest", () => compilation.checkConstraints());
+											});
 										});
+									})
+								};
+								webpack(options, (err, stats) => {
+									if(err) return done(err);
+									const statOptions = Stats.presetToOptions("verbose");
+									statOptions.colors = false;
+									mkdirp.sync(outputDirectory);
+									fs.writeFileSync(path.join(outputDirectory, "stats.txt"), stats.toString(statOptions), "utf-8");
+									const jsonStats = stats.toJson({
+										errorDetails: true
 									});
-								})
-							};
-							webpack(options, (err, stats) => {
-								if(err) return done(err);
-								const statOptions = Stats.presetToOptions("verbose");
-								statOptions.colors = false;
-								mkdirp.sync(outputDirectory);
-								fs.writeFileSync(path.join(outputDirectory, "stats.txt"), stats.toString(statOptions), "utf-8");
-								const jsonStats = stats.toJson({
-									errorDetails: true
+									if(checkArrayExpectation(testDirectory, jsonStats, "error", "Error", done)) return;
+									if(checkArrayExpectation(testDirectory, jsonStats, "warning", "Warning", done)) return;
+									let exportedTests = [];
+
+									function _it(title, fn) {
+										exportedTests.push(fit(title, fn));
+										// TODO: is this necessary in 'jest'?
+										// WORKAROUND for a v8 bug
+										// Error objects retrain all scopes in the stacktrace
+										// test._trace = test._trace.message;
+									}
+
+									function _require(module) {
+										if(module.substr(0, 2) === "./") {
+											const p = path.join(outputDirectory, module);
+											const fn = vm.runInThisContext("(function(require, module, exports, __dirname, it, expect) {" + fs.readFileSync(p, "utf-8") + "\n})", p);
+											const m = {
+												exports: {},
+												webpackTestSuiteModule: true
+											};
+											fn.call(m.exports, _require, m, m.exports, outputDirectory, _it, expect);
+											return m.exports;
+										} else return require(module);
+									}
+									_require.webpackTestSuiteRequire = true;
+									_require("./bundle.js");
+									if(exportedTests.length === 0) return done(new Error("No tests exported by test case"));
+									async.waterfall(
+										exportedTests.map(test => (callback) => test.execute(callback, true)),
+										done
+									);
 								});
-								if(checkArrayExpectation(testDirectory, jsonStats, "error", "Error", done)) return;
-								if(checkArrayExpectation(testDirectory, jsonStats, "warning", "Warning", done)) return;
-								let exportedTest = 0;
-
-								function _it(title, fn) {
-									const test = new Test(title, fn);
-									suite.addTest(test);
-									exportedTest++;
-									// WORKAROUND for a v8 bug
-									// Error objects retrain all scopes in the stacktrace
-									test._trace = test._trace.message;
-
-									return test;
-								}
-
-								function _require(module) {
-									if(module.substr(0, 2) === "./") {
-										const p = path.join(outputDirectory, module);
-										const fn = vm.runInThisContext("(function(require, module, exports, __dirname, it) {" + fs.readFileSync(p, "utf-8") + "\n})", p);
-										const m = {
-											exports: {},
-											webpackTestSuiteModule: true
-										};
-										fn.call(m.exports, _require, m, m.exports, outputDirectory, _it);
-										return m.exports;
-									} else return require(module);
-								}
-								_require.webpackTestSuiteRequire = true;
-								_require("./bundle.js");
-								if(exportedTest === 0) return done(new Error("No tests exported by test case"));
-								done();
-							});
+							}, 30000);
 						});
 					});
 				});
