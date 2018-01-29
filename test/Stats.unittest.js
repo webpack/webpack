3e0baf8303d9af89124c0cb7915db25e1cb7dce6
Francis John <fjohn@nerdwallet.com>
2018-01-28 18:21:36 -0800

    Expose output path in Stats object
    https://github.com/webpack/webpack/issues/6373

--- test/Stats.unittest.js
+++ test/Stats.unittest.js
@@ -2,8 +2,8 @@
 "use strict";
 
 require("should");
-
 const Stats = require("../lib/Stats");
+const packageJson = require("../package.json")
 
 describe("Stats", () => {
 	describe("Error Handling", () => {
@@ -94,6 +94,9 @@ describe("Stats", () => {
 				children: [],
 				hash: "1234",
 				mainTemplate: {
+					outputOptions: {
+						path: ""
+					},
 					getPublicPath: () => "path"
 				},
 				compiler: {
@@ -102,6 +105,43 @@ describe("Stats", () => {
 			});
 			const obj = mockStats.toJson();
 			obj.errors[0].should.be.equal("firstError");
+		});
+	});
+	describe("toJson", () => {
+		it("returns plain object representation", () => {
+			const mockStats = new Stats({
+				errors: [],
+				warnings: [],
+				assets: [],
+				entrypoints: {},
+				chunks: [],
+				modules: [],
+				children: [],
+				hash: "1234",
+				mainTemplate: {
+					outputOptions: {
+						path: "/"
+					},
+					getPublicPath: () => "path"
+				}
+			});
+			const result = mockStats.toJson();
+			result.should.deepEqual({
+				assets: [],
+				assetsByChunkName: {},
+				children: [],
+				chunks: [],
+				entrypoints: {},
+				errors: [],
+				filteredAssets: 0,
+				filteredModules: 0,
+				hash: "1234",
+				modules: [],
+				outputPath: "/",
+				publicPath: "path",
+				version: packageJson.version,
+				warnings: []
+			});
 		});
 	});
 	describe("Presets", () => {
