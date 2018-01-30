bfcee78d2506dc5d1bfa0119708fc37ca35c3ef2
kingdaro <kingdaro@gmail.com>
2018-01-30 03:36:09 -0500

    test: fixed removed plugin error case

--- test/cases/errors/removed-plugins/index.js
+++ test/cases/errors/removed-plugins/index.js
@@ -1,17 +1,16 @@
 const webpack = require("../../../../lib/webpack");
-const should = require("should");
-const RemovedPluginError = require('../../../../lib/RemovedPluginError')
+const RemovedPluginError = require("../../../../lib/RemovedPluginError");
 
 it("should error when accessing removed plugins", () => {
-	should.throws(
-		() => webpack.optimize.UglifyJsPlugin,
-		RemovedPluginError,
-		"webpack.optimize.UglifyJsPlugin has been removed, please use config.optimization.minimize instead."
-	);
+	(() => webpack.optimize.UglifyJsPlugin)
+		.should.throw(
+			RemovedPluginError,
+			/webpack.optimize.UglifyJsPlugin has been removed, please use config.optimization.minimize instead./
+		);
 
-	should.throws(
-		() => webpack.optimize.CommonsChunkPlugin,
-		RemovedPluginError,
-		"webpack.optimize.CommonsChunkPlugin has been removed, please use config.optimization.splitChunks instead."
-	);
-});
+	(() => webpack.optimize.CommonsChunkPlugin)
+		.should.throw(
+			RemovedPluginError,
+			/webpack.optimize.CommonsChunkPlugin has been removed, please use config.optimization.splitChunks instead./
+		);
+})
