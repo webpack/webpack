69c2f1a635a5d75eb517d9b328ca94dd9d175df5
kingdaro <kingdaro@gmail.com>
2018-01-30 03:28:04 -0500

    refactor(removed plugin errors): use custom error class

--- test/cases/errors/removed-plugins/index.js
+++ test/cases/errors/removed-plugins/index.js
@@ -1,16 +1,17 @@
 const webpack = require("../../../../lib/webpack");
 const should = require("should");
+const RemovedPluginError = require('../../../../lib/RemovedPluginError')
 
 it("should error when accessing removed plugins", () => {
 	should.throws(
 		() => webpack.optimize.UglifyJsPlugin,
-		Error,
+		RemovedPluginError,
 		"webpack.optimize.UglifyJsPlugin has been removed, please use config.optimization.minimize instead."
 	);
 
 	should.throws(
 		() => webpack.optimize.CommonsChunkPlugin,
-		Error,
+		RemovedPluginError,
 		"webpack.optimize.CommonsChunkPlugin has been removed, please use config.optimization.splitChunks instead."
 	);
 });
