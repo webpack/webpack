0d780b5e7e7e1000b6b6fc0619c2d75c9e90a0ef
kingdaro <kingdaro@gmail.com>
2018-01-30 03:08:16 -0500

    style: convert spaces to tabs

--- test/cases/errors/removed-plugins/index.js
+++ test/cases/errors/removed-plugins/index.js
@@ -2,15 +2,15 @@ const webpack = require("../../../../lib/webpack");
 const should = require("should");
 
 it("should error when accessing removed plugins", () => {
-  should.throws(
-    () => webpack.optimize.UglifyJsPlugin,
-    Error,
-    "webpack.optimize.UglifyJsPlugin has been removed, please use config.optimization.minimize instead."
-  );
+	should.throws(
+		() => webpack.optimize.UglifyJsPlugin,
+		Error,
+		"webpack.optimize.UglifyJsPlugin has been removed, please use config.optimization.minimize instead."
+	);
 
 	should.throws(
-    () => webpack.optimize.CommonsChunkPlugin,
-    Error,
-    "webpack.optimize.CommonsChunkPlugin has been removed, please use config.optimization.splitChunks instead."
-  );
+		() => webpack.optimize.CommonsChunkPlugin,
+		Error,
+		"webpack.optimize.CommonsChunkPlugin has been removed, please use config.optimization.splitChunks instead."
+	);
 });
