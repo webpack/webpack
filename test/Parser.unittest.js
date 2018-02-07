490a32cfea2ab88dd0d46b1dcd2d09cf4ee49f43
Florent Cailhol <florent.cailhol@gmail.com>
2018-02-07 10:39:06 +0100

    Update parser to support EcmaScript 2018

--- test/Parser.unittest.js
+++ test/Parser.unittest.js
@@ -468,13 +468,13 @@ describe("Parser", () => {
 			const cases = {
 				"async function": "async function x() {}",
 				"async arrow function": "async () => {}",
-				"await expression": "async function x(y) { await y }"
+				"await expression": "async function x(y) { await y }",
+				"await iteration": "async function f() { for await (x of xs); }"
 			};
-			const parser = new Parser();
 			Object.keys(cases).forEach((name) => {
 				const expr = cases[name];
 				it(name, () => {
-					const actual = parser.parse(expr);
+					const actual = Parser.parse(expr);
 					should.strictEqual(typeof actual, "object");
 				});
 			});
@@ -507,6 +507,22 @@ describe("Parser", () => {
 				it(name, () => {
 					const actual = parser.parse(cases[name][0]);
 					actual.should.be.eql(cases[name][1]);
+				});
+			});
+		});
+	});
+
+	describe("object rest/spread support", () => {
+		describe("should accept", () => {
+			const cases = {
+				"object spread": "({...obj})",
+				"object rest": "({...obj} = foo)"
+			};
+			Object.keys(cases).forEach((name) => {
+				const expr = cases[name];
+				it(name, () => {
+					const actual = Parser.parse(expr);
+					should.strictEqual(typeof actual, "object");
 				});
 			});
 		});
