d4115a65a700e175170438217a678f217f500cbe
matheus.g.silva <matheus.g.silva@hotmail.com>
2018-02-07 19:28:38 -0500

    Add installation if webpack-cli is not present

--- bin/webpack.js
+++ bin/webpack.js
@@ -1,4 +1,15 @@
-#!/usr/bin/env node
+const {exec} = require('child_process');
+
+
+function runCommand(command) {
+	exec(command, (err, data) => {
+		if(data) {
+			console.log(data);
+		} else {
+			console.log(err);
+		}
+	});
+}
 
 let webpackCliInstalled = false;
 try {
@@ -11,10 +22,35 @@ try {
 if (webpackCliInstalled) {
 	require("webpack-cli"); // eslint-disable-line node/no-missing-require, node/no-extraneous-require, node/no-unpublished-require
 } else {
+	const path = require('path');
+	const fs = require('fs');
+	const isYarn = fs.existsSync(path.resolve(process.cwd(), 'yarn.lock'));
+	const {prompt} = require('inquirer');
+	let command;
+	
+	
+	const question = {
+		type: 'confirm',
+		name: 'shouldInstall',
+		message: 'Would you like to install webpack-cli?',
+		default: true
+	}
+
+	if(isYarn){
+		command = 'yarn add webpack-cli -D';
+	}else{
+		command = 'npm install --save-dev webpack-cli';
+	}
+	
 	console.error("The CLI moved into a separate package: webpack-cli.");
-	console.error(
-		"Please install 'webpack-cli' in addition to webpack itself to use the CLI."
-	);
+	prompt(question).then((anwswer) => {
+		if(answer){
+			console.error('Installing webpack-cli')
+			runCommand(command);
+		}
+	})
+	
+	console.error("Please install 'webpack-cli' in addition to webpack itself to use the CLI.");
 	console.error("-> When using npm: npm install webpack-cli -D");
 	console.error("-> When using yarn: yarn add webpack-cli -D");
 	process.exitCode = 1;
