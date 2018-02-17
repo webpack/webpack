ff224044f75aaca84b3da333df38089d194a649b
Tobias Koppers <tobias.koppers@googlemail.com>
2018-02-17 13:35:06 +0100

    fix example

--- examples/common-chunk-and-vendor-chunk/webpack.config.js
+++ examples/common-chunk-and-vendor-chunk/webpack.config.js
@@ -13,12 +13,14 @@ module.exports = {
 				commons: {
 					chunks: "initial",
 					minChunks: 2,
+					maxInitialRequests: 5, // The default limit is too small to showcase the effect
 					minSize: 0 // This is example is too small to create commons chunks
 				},
 				vendor: {
 					test: /node_modules/,
 					chunks: "initial",
 					name: "vendor",
+					priority: 10,
 					enforce: true
 				}
 			}
