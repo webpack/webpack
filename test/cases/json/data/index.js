7282e9514ce21503dbec639cdcbe2ff6511e931e
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2018-02-25 01:21:09 +0100

    fix expect's of one more test

--- test/cases/json/data/index.js
+++ test/cases/json/data/index.js
@@ -1,8 +1,8 @@
 it("should require json via require", function() {
-	({ data: require("./a.json") }expect()).toEqual({ data: null });
-	({ data: require("./b.json") }expect()).toEqual({ data: 123 });
-	({ data: require("./c.json") }expect()).toEqual({ data: [1, 2, 3, 4] });
-	({ data: require("./e.json") }expect()).toEqual({ data: {
+	expect({ data: require("./a.json") }).toEqual({ data: null });
+	expect({ data: require("./b.json") }).toEqual({ data: 123 });
+	expect({ data: require("./c.json") }).toEqual({ data: [1, 2, 3, 4] });
+	expect({ data: require("./e.json") }).toEqual({ data: {
 		"aa": 1,
 		"bb": 2,
 		"1": "x"
