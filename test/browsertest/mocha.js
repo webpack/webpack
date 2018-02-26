4ecd495a5bb573ff008d474ab02df191b56365d1
Josh Soref <jsoref@users.noreply.github.com>
2018-02-26 02:35:18 +0000

    spelling: number

--- test/browsertest/mocha.js
+++ test/browsertest/mocha.js
@@ -2777,7 +2777,7 @@ function NyanCat(runner) {
     , width = Base.window.width * .75 | 0
     , rainbowColors = this.rainbowColors = self.generateColors()
     , colorIndex = this.colorIndex = 0
-    , numerOfLines = this.numberOfLines = 4
+    , numberOfLines = this.numberOfLines = 4
     , trajectories = this.trajectories = [[], [], [], []]
     , nyanCatWidth = this.nyanCatWidth = 11
     , trajectoryWidthMax = this.trajectoryWidthMax = (width - nyanCatWidth)
