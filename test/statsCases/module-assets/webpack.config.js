81235e133121f1896a1b469d10f40e8381539d90
Clark Du <clark.duxin@gmail.com>
2018-03-04 13:24:10 +0800

    Add stats.moduleAssets to show assets inside modules in the Stats

--- test/statsCases/module-assets/webpack.config.js
+++ test/statsCases/module-assets/webpack.config.js
@@ -2,15 +2,24 @@ module.exports = {
 	mode: "production",
 	entry: "./index",
 	stats: {
+		assets: false,
 		chunks: true,
 		chunkModules: true,
-		modules: true
+		modules: true,
+		moduleAssets: true
 	},
 	module: {
 		rules: [
 			{
 				test: /\.png$/,
-				use: ["file-loader"]
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]"
+						}
+					}
+				]
 			}
 		]
 	}
