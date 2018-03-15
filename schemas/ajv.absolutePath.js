57ea856cc837f5ea0207001cf1f99d5bb5b84e71
Fernando Montoya <montogeek@gmail.com>
2018-03-15 12:47:24 +0100

    misc: Add schemas to ESLint and Prettier commands

--- schemas/ajv.absolutePath.js
+++ schemas/ajv.absolutePath.js
@@ -1,29 +1,32 @@
 "use strict";
 
 const getErrorFor = (shouldBeAbsolute, data, schema) => {
-	const message = shouldBeAbsolute ?
-		`The provided value ${JSON.stringify(data)} is not an absolute path!`
-		: `A relative path is expected. However the provided value ${JSON.stringify(data)} is an absolute path!`;
+	const message = shouldBeAbsolute
+		? `The provided value ${JSON.stringify(data)} is not an absolute path!`
+		: `A relative path is expected. However the provided value ${JSON.stringify(
+				data
+			)} is an absolute path!`;
 
 	return {
 		keyword: "absolutePath",
 		params: { absolutePath: data },
 		message: message,
-		parentSchema: schema,
+		parentSchema: schema
 	};
 };
-module.exports = (ajv) => ajv.addKeyword("absolutePath", {
-	errors: true,
-	type: "string",
-	compile(expected, schema) {
-		function callback(data) {
-			const passes = expected === /^(?:[A-Za-z]:\\|\/)/.test(data);
-			if(!passes) {
-				callback.errors = [getErrorFor(expected, data, schema)];
+module.exports = ajv =>
+	ajv.addKeyword("absolutePath", {
+		errors: true,
+		type: "string",
+		compile(expected, schema) {
+			function callback(data) {
+				const passes = expected === /^(?:[A-Za-z]:\\|\/)/.test(data);
+				if (!passes) {
+					callback.errors = [getErrorFor(expected, data, schema)];
+				}
+				return passes;
 			}
-			return passes;
+			callback.errors = [];
+			return callback;
 		}
-		callback.errors = [];
-		return callback;
-	}
-});
+	});
