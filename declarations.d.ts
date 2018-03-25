41e4a2e69fde4f022d629a8b84331e1d6043c3f6
Mohsen Azimi <mazimi@lyft.com>
2018-03-25 09:07:41 -0700

    Add infrastructure

---
+++ declarations.d.ts
@@ -0,0 +1,19 @@
+declare module '*.json';
+declare module 'webpack-cli';
+
+// Globals
+declare const $hash$;
+declare const $requestTimeout$;
+declare const installedModules;
+declare const $require$;
+declare const hotDownloadManifest;
+declare const hotDownloadUpdateChunk;
+declare const hotDisposeChunk;
+declare const modules;
+declare const installedChunks
+declare const hotAddUpdateChunk
+declare const parentHotUpdateCallback
+// declare const XMLHttpRequest
+declare const $hotChunkFilename$
+declare const $hotMainFilename$
+declare const $WebAssembly
