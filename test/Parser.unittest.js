acbc29c757ed7ec7e641631a814ea78e5faeb687
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2018-04-11 20:31:43 +0200

    fix placement of eslint-disables after merge

--- test/Parser.unittest.js
+++ test/Parser.unittest.js
@@ -473,10 +473,13 @@ describe("Parser", () => {
 				"wrapped=['str' string=str]+['four' string=four]",
 			"'str'.concat(obj, 'one', 'two').concat('three', obj, 'four', 'five')":
 				"wrapped=['str' string=str]+['four', 'five' string=fourfive]",
+			// eslint-disable-next-line no-template-curly-in-string
 			"`start${obj}mid${obj2}end`":
-				"template=[start string=start],[mid string=mid],[end string=end]", // eslint-disable-line no-template-curly-in-string
+				"template=[start string=start],[mid string=mid],[end string=end]",
+			// eslint-disable-next-line no-template-curly-in-string
 			"`start${'str'}mid${obj2}end`":
-				"template=[start${'str'}mid string=startstrmid],[end string=end]", // eslint-disable-line no-template-curly-in-string
+				// eslint-disable-next-line no-template-curly-in-string
+				"template=[start${'str'}mid string=startstrmid],[end string=end]",
 			"'abc'.substr(1)": "string=bc",
 			"'abcdef'.substr(2, 3)": "string=cde",
 			"'abcdef'.substring(2, 3)": "string=c",
