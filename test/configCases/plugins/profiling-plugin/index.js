b419a7b20b49425d6fa9d29ca254036263138f82
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2018-04-18 21:15:14 +0200

    profiling-plugin test: add a timeout to address a CI failures
    agreed to by @sokra here: https://github.com/webpack/webpack/pull/6565#issuecomment-381925287

--- test/configCases/plugins/profiling-plugin/index.js
+++ test/configCases/plugins/profiling-plugin/index.js
@@ -1,18 +1,28 @@
 import "./test.json";
 
-it("should generate a events.json file", () => {
+it("should generate a events.json file", (done) => {
     var fs = require("fs"),
         path = require("path"),
-        os = require("os");
-    expect(fs.existsSync(path.join(__dirname, "events.json"))).toBe(true);
+		os = require("os");
+
+	// HACK: we need this timeout on the CI only,
+	// because the filesystem is less responsive
+	setTimeout(() => {
+		expect(fs.existsSync(path.join(__dirname, "events.json"))).toBe(true);
+		done();
+	}, 500)
 });
 
-it("should have proper setup record inside of the json stream", () => {
+it("should have proper setup record inside of the json stream", (done) => {
     var fs = require("fs"),
         path = require("path"),
         os = require("os");
 
-    // convert json stream to valid
-    var source = JSON.parse(fs.readFileSync(path.join(__dirname, "events.json"), "utf-8").toString() + "{}]");
-    expect(source[0].id).toEqual(1);
+	// HACK: we need this timeout on the CI only,
+	// because the filesystem is less responsive
+	setTimeout(() => {
+		// convert json stream to valid
+		var source = JSON.parse(fs.readFileSync(path.join(__dirname, "events.json"), "utf-8").toString() + "{}]");
+		expect(source[0].id).toEqual(1);
+	}, 500)
 });
