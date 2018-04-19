073566778493228db264ccfdebb48db8fcf69fe4
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2018-04-19 12:55:15 +0200

    revert 76e295e7ef2fabe993251b5a82266542cae7b3aa

--- test/configCases/plugins/profiling-plugin/index.js
+++ test/configCases/plugins/profiling-plugin/index.js
@@ -5,12 +5,7 @@ it("should generate a events.json file", (done) => {
         path = require("path"),
 		os = require("os");
 
-	// HACK: we need this timeout on the CI only,
-	// because the filesystem is less responsive
-	setTimeout(() => {
-		expect(fs.existsSync(path.join(__dirname, "events.json"))).toBe(true);
-		done();
-	}, 500)
+	expect(fs.existsSync(path.join(__dirname, "events.json"))).toBe(true);
 });
 
 it("should have proper setup record inside of the json stream", (done) => {
@@ -18,12 +13,7 @@ it("should have proper setup record inside of the json stream", (done) => {
         path = require("path"),
         os = require("os");
 
-	// HACK: we need this timeout on the CI only,
-	// because the filesystem is less responsive
-	setTimeout(() => {
-		// convert json stream to valid
-		var source = JSON.parse(fs.readFileSync(path.join(__dirname, "events.json"), "utf-8").toString() + "{}]");
-		expect(source[0].id).toEqual(1);
-		done();
-	}, 500)
+	// convert json stream to valid
+	var source = JSON.parse(fs.readFileSync(path.join(__dirname, "events.json"), "utf-8").toString() + "{}]");
+	expect(source[0].id).toEqual(1);
 });
