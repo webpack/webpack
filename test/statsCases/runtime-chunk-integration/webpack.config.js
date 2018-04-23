3b6d7d873ecbbdc7b09e255adc15a0010355bfc5
Alejandro Madariaga Angeles <amndell@gmail.com>
2018-04-23 17:30:14 -0500

    fix bug where sometimes the runtime would not be present on output

---
+++ test/statsCases/runtime-chunk-integration/webpack.config.js
@@ -0,0 +1,28 @@
+const MinChunkSizePlugin = require("../../../lib/optimize/MinChunkSizePlugin");
+
+module.exports = {
+	mode: "production",
+	target: "web",
+	entry: {
+		main1: "./main1"
+	},
+	output: {
+		filename: "[name].js"
+	},
+	optimization: {
+		runtimeChunk: {
+			name: "manifest"
+		}
+	},
+	stats: {
+		hash: false,
+		timings: false,
+		builtAt: false,
+		reasons: false
+	},
+	plugins: [
+		new MinChunkSizePlugin({
+			minChunkSize: 30
+		})
+	]
+};
