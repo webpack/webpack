d250eacac13217a5672be55ebde81b74479d8abf
Max Burmagin <mhmad.hamster@gmail.com>
2018-05-01 01:42:18 +0300

    fix test to use jest

--- test/configCases/module-name/different-issuers-for-same-module/test.js
+++ test/configCases/module-name/different-issuers-for-same-module/test.js
@@ -1,9 +1,8 @@
-require("should");
 it("should assign different names to the same module with different issuers ", function() {
 	var fs = require("fs");
 	var path = require("path");
 	var bundle = fs.readFileSync(path.join(__dirname, "main.js"), "utf-8");
-	bundle.should.match(/\.\/c\.js\?\w{4}/g);
-	require("./a").should.be.equal("loader-a");
-	require("./b").should.be.equal("loader-b");
+	expect(bundle.match(/"\.\/c\.js\?\w{4}":/g)).toHaveLength(2);
+	expect(require("./a")).toEqual("loader-a");
+	expect(require("./b")).toEqual("loader-b");
 });
