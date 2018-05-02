edddefbea0c745ce839c90a696bab7304f6a30f3
Tobias Koppers <tobias.koppers@googlemail.com>
2018-05-02 11:30:55 +0200

    add test cases for decoding wasm

---
+++ test/cases/wasm/table/index.js
@@ -0,0 +1,28 @@
+it("should support tables", function() {
+	return import("./wasm-table.wasm").then(function(wasm) {
+		expect(wasm.callByIndex(0)).toEqual(42);
+		expect(wasm.callByIndex(1)).toEqual(13);
+		expect(() => wasm.callByIndex(2)).toThrow("fefef");
+	});
+});
+
+it("should support exported tables", function() {
+	return import("./wasm-table-export.wasm").then(function(wasm) {
+		expect(wasm.table).toBeInstanceOf(WebAssembly.Table);
+		expect(wasm.table.length).toBe(2);
+		const e0 = wasm.table.get(0);
+		const e1 = wasm.table.get(1);
+		expect(e0).toBeInstanceOf(Function);
+		expect(e1).toBeInstanceOf(Function);
+		expect(e0()).toEqual(42);
+		expect(e1()).toEqual(13);
+	});
+});
+
+it("should support imported tables", function() {
+	return import("./wasm-table-imported.wasm").then(function(wasm) {
+		expect(wasm.callByIndex(0)).toEqual(42);
+		expect(wasm.callByIndex(1)).toEqual(13);
+		expect(() => wasm.callByIndex(2)).toThrow("fefef");
+	});
+});
