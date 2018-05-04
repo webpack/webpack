a3d3af88a53d1917780ec977b1edd899b3e79d93
byzyk <bohdan.kh@gmail.com>
2018-05-04 15:32:49 +0400

    add test

--- test/configCases/library/umd/webpack.config.js
+++ test/configCases/library/umd/webpack.config.js
@@ -1,10 +1,22 @@
-module.exports = {
-	output: {
-		libraryTarget: "umd",
-		library: {
-			root: "testLibrary",
-			amd: "test-library",
-			commonjs: "test-library"
+module.exports = [
+	{
+		output: {
+			libraryTarget: "umd",
+			library: {
+				root: "testLibrary",
+				amd: "test-library",
+				commonjs: "test-library"
+			}
+		}
+	},
+	{
+		output: {
+			libraryTarget: "umd",
+			library: {
+				root: ["test", "library"],
+				amd: "test-library",
+				commonjs: "test-library"
+			}
 		}
 	}
-};
+];
