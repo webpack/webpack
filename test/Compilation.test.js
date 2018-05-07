552d05e26828afcaea9dd81aeb4a72c7535a2f00
Jordan West <jordwest@gmail.com>
2018-05-07 11:54:36 +1000

    lint fix

--- test/Compilation.test.js
+++ test/Compilation.test.js
@@ -9,19 +9,33 @@ const Compilation = require("../lib/Compilation");
 describe("Compilation", () => {
 	describe('sortModules', () => {
 		it('should sort modules by index', () => {
-			let modules = [
-				{index: 5},
-				{index: 4},
-				{index: 8},
-				{index: 1},
+			let modules = [{
+					index: 5
+				},
+				{
+					index: 4
+				},
+				{
+					index: 8
+				},
+				{
+					index: 1
+				},
 			];
 
 			Compilation.prototype.sortModules(modules);
-			modules.should.match([
-				{index: 1},
-				{index: 4},
-				{index: 5},
-				{index: 8},
+			modules.should.match([{
+					index: 1
+				},
+				{
+					index: 4
+				},
+				{
+					index: 5
+				},
+				{
+					index: 8
+				},
 			]);
 		});
 	});
