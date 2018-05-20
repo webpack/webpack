02e44f5e97c7f50c40832952302ebc7a72af55d4
Tobias Koppers <tobias.koppers@googlemail.com>
2018-05-20 21:46:56 +0200

    remove hanging tests

--- test/configCases/plugins/loader-options-plugin/loader.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-	return "module.exports = " + JSON.stringify({
-		minimize: this.minimize,
-		jsfile: this.jsfile
-	});
-};
--- test/configCases/plugins/min-chunk-size/webpack.config.js
+++
@@ -1,9 +0,0 @@
-var webpack = require("../../../../");
-
-module.exports = {
-	plugins: [
-		new webpack.optimize.MinChunkSizePlugin({
-			minChunkSize: 30
-		})
-	]
-};
