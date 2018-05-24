acc45fd806145e1d1f42aa8042a76c79121eaf1f
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-05-24 14:12:11 +0200

    refactor: switch to wast

--- test/TestCases.template.js
+++ test/TestCases.template.js
@@ -141,6 +141,11 @@ const describeCases = config => {
 										{
 											test: /\.pug/,
 											loader: "pug-loader"
+										},
+										{
+											test: /\.wat$/i,
+											loader: "wast-loader",
+											type: "webassembly/experimental"
 										}
 									]
 								},
