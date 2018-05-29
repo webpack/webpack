504148c79b34a09fc592e3003b36fdc047cc12ad
Tobias Koppers <tobias.koppers@googlemail.com>
2018-05-29 12:40:32 +0200

    add test cases for #7401

---
+++ test/statsCases/split-chunks-issue-7401/webpack.config.js
@@ -0,0 +1,29 @@
+const stats = {
+	hash: false,
+	timings: false,
+	builtAt: false,
+	assets: false,
+	chunks: true,
+	chunkOrigins: true,
+	entrypoints: true,
+	chunkGroups: true,
+	modules: false
+};
+module.exports = {
+	name: "default",
+	mode: "production",
+	entry: {
+		a: "./a",
+		b: "./b"
+	},
+	output: {
+		filename: "[name].js"
+	},
+	optimization: {
+		splitChunks: {
+			minSize: 0, // enforce all
+			chunks: "all"
+		}
+	},
+	stats
+};
