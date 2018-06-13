54977cd33181b325e4b3523cbb7bdc808106e77c
byzyk <bohdan.kh@gmail.com>
2018-06-13 15:04:23 +0400

    fix benchmark linter issues

--- benchmark/createBenchmark/entry.js
+++ benchmark/createBenchmark/entry.js
@@ -1,5 +1,5 @@
+// eslint-disable-next-line node/no-extraneous-require
 const webpack = require("webpack");
-const MemoryFs = require("memory-fs");
 const path = require("path");
 
 const testCase = process.argv[2];
@@ -15,11 +15,13 @@ const config = {
 
 const compiler = webpack(config);
 compiler.run((err, stats) => {
-	if(err) {
+	if (err) {
 		console.error(err);
 	} else {
-		console.log(stats.toString({
-			errorDetails: true
-		}));
+		console.log(
+			stats.toString({
+				errorDetails: true
+			})
+		);
 	}
 });
