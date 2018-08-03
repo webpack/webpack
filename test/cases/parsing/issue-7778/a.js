61633aa91c61328a31c132d5afdd2acd8219cb5c
Tobias Koppers <tobias.koppers@googlemail.com>
2018-08-03 09:20:23 +0200

    fix #7778 remove prepend in favor of replaces improve code style in ContextDependencyHelpers

--- test/cases/parsing/issue-7778/a.js
+++ test/cases/parsing/issue-7778/a.js
@@ -1,3 +1,3 @@
 export default function a() {
-    return 'a'
+    return 'a' + __resourceQuery
 }
--- test/cases/parsing/issue-7778/index.js
+++ test/cases/parsing/issue-7778/index.js
@@ -1,13 +1,57 @@
 it("should detect query strings in dynamic import as a static value 1 ", function() {
-    import("./a?queryString").then(({ default: a }) => {
-        expect(a()).toBe("a");
-    });
+	return Promise.all([
+		import("./a").then(({ default: a }) => {
+			expect(a()).toBe("a");
+		}),
+		import("./abc").then(({ default: a }) => {
+			expect(a()).toBe("abc");
+		}),
+		import("./a?queryString").then(({ default: a }) => {
+			expect(a()).toBe("a?queryString");
+		}),
+		import("./abc?query?String").then(({ default: a }) => {
+			expect(a()).toBe("abc?query?String");
+		}),
+	]);
 });
 
 it("should detect query strings in dynamic import as a static value 2", function() {
-    var testFileName = "a";
-    
-    import(`./${testFileName}?queryString`).then(({ default: a }) => {
-        expect(a()).toBe("a");
-    });
+	var testFileName = "a";
+
+	return Promise.all([
+		import(`./${testFileName}`).then(({ default: a }) => {
+			expect(a()).toBe("a");
+		}),
+		import(`./${testFileName}bc`).then(({ default: a }) => {
+			expect(a()).toBe("abc");
+		}),
+		import(`./${testFileName}?queryString`).then(({ default: a }) => {
+			expect(a()).toBe("a?queryString");
+		}),
+		import(`./${testFileName}bc?query?String`).then(({ default: a }) => {
+			expect(a()).toBe("abc?query?String");
+		})
+	]);
+});
+
+it("should detect query strings in dynamic import as a static value 2", function() {
+	var testFileName = "a";
+
+	return Promise.all([
+		import("./" + testFileName).then(({ default: a }) => {
+			expect(a()).toBe("a");
+		}),
+		import("./" + testFileName + "").then(({ default: a }) => {
+			expect(a()).toBe("a");
+		}),
+		import("./" + testFileName + "bc").then(({ default: a }) => {
+			expect(a()).toBe("abc");
+		}),
+		import("./" + testFileName + "?queryString").then(({ default: a }) => {
+			expect(a()).toBe("a?queryString");
+		}),
+		import("./" + testFileName + "bc?query?String").then(({ default: a }) => {
+			expect(a()).toBe("abc?query?String");
+		})
+	]);
 });
