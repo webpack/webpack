ac09ae1418cc6da37966c60af070c89b69834836
Tobias Koppers <tobias.koppers@googlemail.com>
2018-08-09 15:19:54 +0200

    add more test cases improve harmony-duplicate-export test cases to allow scope-hoisting

--- test/cases/parsing/harmony-duplicate-export/index.js
+++ test/cases/parsing/harmony-duplicate-export/index.js
@@ -1,18 +1,18 @@
-import { x as x1 } from "./1?a";
-import { x as x2 } from "./2?a";
-import { x as x3 } from "./3?a";
-import { x as x4 } from "./4?a";
-import { x as x5 } from "./5?a";
-import { x as x6 } from "./6?a";
-import { x as x7 } from "./7?a";
+import { x as x1 } from "./1";
+import { x as x2 } from "./2";
+import { x as x3 } from "./3";
+import { x as x4 } from "./4";
+import { x as x5 } from "./5";
+import { x as x6 } from "./6";
+import { x as x7 } from "./7";
 
-var y1 = require("./1?b").x;
-var y2 = require("./2?b").x;
-var y3 = require("./3?b").x;
-var y4 = require("./4?b").x;
-var y5 = require("./5?b").x;
-var y6 = require("./6?b").x;
-var y7 = require("./7?b").x;
+var y1 = require("./cjs/1").x;
+var y2 = require("./cjs/2").x;
+var y3 = require("./cjs/3").x;
+var y4 = require("./cjs/4").x;
+var y5 = require("./cjs/5").x;
+var y6 = require("./cjs/6").x;
+var y7 = require("./cjs/7").x;
 
 it("should not overwrite when using star export (known exports)", function() {
 	expect(x1).toBe("1");
@@ -21,7 +21,7 @@ it("should not overwrite when using star export (known exports)", function() {
 	expect(x4).toBe("b");
 	expect(x5).toBe("c");
 	expect(x6).toBe("a");
-	expect(x7).toBe("d");
+	expect(x7).toBe("b"); // Looks wrong, but is irrelevant as this should be an error anyway
 });
 
 it("should not overwrite when using star export (unknown exports)", function() {
@@ -31,5 +31,5 @@ it("should not overwrite when using star export (unknown exports)", function() {
 	expect(y4).toBe("b");
 	expect(y5).toBe("c");
 	expect(y6).toBe("a");
-	expect(y7).toBe("d");
+	expect(y7).toBe("b"); // Looks wrong, but is irrelevant as this should be an error anyway
 });
