22dc2814e233f59c4fbe710d81152dd39125613e
byzyk <bohdan.kh@gmail.com>
2018-09-17 16:09:13 +0400

    add AMD test

--- test/configCases/library/function-umd/webpack.config.js
+++ test/configCases/library/function-umd/webpack.config.js
@@ -1,11 +1,18 @@
-module.exports = {
-	output: {
-		libraryTarget: "umd",
-		library: () => ({
-			root: ["test", "library"],
-			amd: "test-library",
-			commonjs: "test-library"
-		}),
-		libraryExport: () => "default"
+module.exports = [
+	{
+		output: {
+			libraryTarget: "umd",
+			library: () => ({
+				root: ["test", "library"],
+				amd: "test-library",
+				commonjs: "test-library"
+			})
+		}
+	},
+	{
+		output: {
+			libraryTarget: "umd",
+			library: ({ chunk }) => chunk.name
+		}
 	}
-};
+];
