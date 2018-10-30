84f92abcb95da9e7b3fa3545533ed8b2dd74646d
Zhibin Liu <ljqx@live.cn>
2018-10-30 16:22:31 +0800

    add tests

--- test/configCases/parsing/issue-8293/index.js
+++ test/configCases/parsing/issue-8293/index.js
@@ -1,10 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-it("should be able to replace import param in DefinePlugin", function() {
-	const source = fs.readFileSync(path.join(__dirname, "bundle1.js"), "utf-8");
-	expect(source).toContain(`(\`./\${foobar}/\${"suffix"}\`)`);
-	expect(source).not.toContain(
-		`(\`./\${DEFINED_EXPRESSION}/\${CONST_SUFFIX}\`)`
-	);
-});
+["import", "amd-require", "amd-define", "commonjs", "require.resolve"].forEach(
+	method => {
+		it(`should be able to replace ${method} param in DefinePlugin`, function() {
+			const source = fs.readFileSync(
+				path.join(__dirname, `bundle-${method}.js`),
+				"utf-8"
+			);
+			expect(source).toContain(`\`./\${foobar}/\${"suffix0"}\``);
+			// buggy for param.isConditional() cases, big work, resolve in later PRs
+			// expect(source).toContain(`\`./\${foobar}/\${"suffix1"}\``);
+			// expect(source).toContain(`\`./\${foobar}/\${"suffix2"}\``);
+			expect(source).toContain(`\`./\${foobar}/\${"suffix3"}\``);
+			expect(source).not.toContain(`\`./\${foobar}/\${"suffix4"}\``);
+		});
+	}
+);
