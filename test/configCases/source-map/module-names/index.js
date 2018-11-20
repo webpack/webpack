2849281702e17d822afcb33162a5053256ab1a0d
Tobias Koppers <tobias.koppers@googlemail.com>
2018-11-20 12:05:12 +0100

    remove generateContext from RuntimeModule.generate add public path runtime module

--- test/configCases/source-map/module-names/index.js
+++ test/configCases/source-map/module-names/index.js
@@ -9,12 +9,12 @@ it("should include test.js in SourceMap", function() {
 	var map = getSourceMap("bundle0.js");
 	expect(map.sources).toContain("module");
 	expect(map.sources).toContain("fallback");
-	expect(map.sources).toContain("fallback**");
-	map = getSourceMap("chunk-a.js");
-	expect(map.sources).toContain("fallback*");
-	map = getSourceMap("chunk-b.js");
-	expect(map.sources).toContain("fallback*");
 	expect(map.sources).toContain("fallback***");
+	map = getSourceMap("chunk-a.js");
+	expect(map.sources).toContain("fallback**");
+	map = getSourceMap("chunk-b.js");
+	expect(map.sources).toContain("fallback**");
+	expect(map.sources).toContain("fallback****");
 });
 
 require.ensure(["./test.js"], function(require) {}, "chunk-a");
