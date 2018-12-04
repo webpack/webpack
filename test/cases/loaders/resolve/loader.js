56feccc3699d1c2bf29e2482670eccd97704fb8b
Tobias Koppers <tobias.koppers@googlemail.com>
2018-12-04 09:55:24 +0100

    convert test case to normal function for node.js 6 support

--- test/cases/loaders/resolve/loader.js
+++ test/cases/loaders/resolve/loader.js
@@ -1,11 +1,16 @@
 const path = require("path");
-module.exports = async function() {
+module.exports = function() {
 	const resolve1 = this.getResolve();
 	const resolve2 = this.getResolve({
 		extensions: [".xyz", ".js"]
 	});
-	return `module.exports = ${JSON.stringify({
-		one: path.basename(await resolve1(__dirname, "./index")),
-		two: path.basename(await resolve2(__dirname, "./index")),
-	})}`;
+	return Promise.all([
+		resolve1(__dirname, "./index"),
+		resolve2(__dirname, "./index")
+	]).then(([one, two]) => {
+		return `module.exports = ${JSON.stringify({
+			one: path.basename(one),
+			two: path.basename(two),
+		})}`;
+	});
 };
