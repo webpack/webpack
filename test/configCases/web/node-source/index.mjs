6b54a46842a21694787cbec33699955d11a799fc
Isiah Meadows <contact@isiahmeadows.com>
2018-12-18 16:55:56 -0500

    Fix failing test.

--- test/configCases/web/node-source/index.mjs
+++ test/configCases/web/node-source/index.mjs
@@ -2,5 +2,11 @@
 eval("require = undefined")
 
 it("should compile fine", () => {
-    global
+    // It's okay if this executes fine or if `global` is not defined. If it
+    // results in a `require()` call, this will throw a `TypeError` instead.
+    try {
+        global
+    } catch (e) {
+        if (!(e instanceof ReferenceError)) throw e
+    }
 })
