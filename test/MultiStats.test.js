3a4f01b9dea797d778064374f4fd4c921b8ef79e
Jahred Hope <jahredhope@gmail.com>
2018-12-27 21:21:26 +1100

    Allow MultiStats to be called with no options

---
+++ test/MultiStats.test.js
@@ -0,0 +1,31 @@
+/*globals describe it */
+"use strict";
+
+const webpack = require("..");
+const MemoryFs = require("memory-fs");
+
+describe("MultiStats", () => {
+	it("should create JSON of children stats", done => {
+		const compiler = webpack([{
+			context: __dirname,
+			entry: "./fixtures/a"
+		},{
+			context: __dirname,
+			entry: "./fixtures/b"
+		}]);
+		compiler.outputFileSystem = new MemoryFs();
+		compiler.run((err, stats) => {
+			if (err) return done(err);
+			try {
+				const statsObject = stats.toJson();
+				expect(
+					statsObject
+				).toEqual(expect.objectContaining({children: expect.any(Array)}));
+				expect(statsObject.children).toHaveLength(2);
+				done();
+			} catch (e) {
+				done(e);
+			}
+		});
+	});
+});
