ea815d8f50ae46c311d0c684013f975be2818272
defcc <chengchao1@gmail.com>
2019-01-09 00:34:50 +0800

    Support function in Library #7904
    - Output.library function will be invoked with current chunk data

--- declarations/WebpackOptions.d.ts
+++ declarations/WebpackOptions.d.ts
@@ -1094,7 +1094,7 @@ export interface OutputOptions {
 	/**
 	 * If set, export the bundle as library. `output.library` is the name.
 	 */
-	library?: string | string[] | LibraryCustomUmdObject;
+	library?: string | Function | string[] | LibraryCustomUmdObject;
 	/**
 	 * Specify which export should be exposed as library
 	 */
