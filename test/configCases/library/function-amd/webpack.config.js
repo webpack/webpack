ea06de353b3f4b51d8ebc641ca4f386bb57a298c
defcc <chengchao1@gmail.com>
2019-01-10 15:14:25 +0800

    fix test case

--- test/configCases/library/function-amd/webpack.config.js
+++ test/configCases/library/function-amd/webpack.config.js
@@ -1,6 +1,13 @@
+const webpack = require("../../../../");
 module.exports = {
 	output: {
 		libraryTarget: "amd",
 		library: ({ chunk }) => chunk.name
-	}
+	},
+	plugins: [
+		new webpack.BannerPlugin({
+			raw: true,
+			banner: "function define(name, deps, fn) { fn(); }\n"
+		})
+	]
 }
