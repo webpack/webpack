cb0e8cd58b6dd46955a725e0a13511ca89ebb9df
Steven Hargrove <hulkish@users.noreply.github.com>
2019-02-06 11:53:12 -0500

    fix spacing

--- test/configCases/runtime/invalid-esm-export/index.js
+++ test/configCases/runtime/invalid-esm-export/index.js
@@ -2,6 +2,6 @@ it("should throw exception when module.exports is assigned in es module", functi
 	expect(function() {
 		require("./esm-import-cjs-export");
 	}).toThrowError(
-		'ES Modules may not assign module.exports or exports.*, Use ESM  export syntax, instead: ./esm-import-cjs-export.js'
+		'ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ./esm-import-cjs-export.js'
 	);
 });
