7ac532924f5cc235b2b9678fbe2ba0ecb094b8e8
Ben Regenspan <bregenspan@gmail.com>
2019-04-21 14:22:28 -0400

    Output stats JSON for examples

--- examples/build-common.js
+++ examples/build-common.js
@@ -12,20 +12,26 @@ const async = require("neo-async");
 
 const extraArgs = "";
 
-const targetArgs = global.NO_TARGET_ARGS ? "" : " ./example.js -o dist/output.js ";
-const displayReasons = global.NO_REASONS ? "" : " --display-reasons --display-used-exports --display-provided-exports";
+const targetArgs = global.NO_TARGET_ARGS
+	? ""
+	: " ./example.js -o dist/output.js";
+const displayReasons = global.NO_REASONS
+	? ""
+	: " --display-reasons --display-used-exports --display-provided-exports";
 const commonArgs = `--display-chunks --display-max-modules 99999 --display-origins --display-entrypoints --output-public-path "dist/" ${extraArgs} ${targetArgs}`;
 
-let readme = fs.readFileSync(require("path").join(process.cwd(), "template.md"), "utf-8");
+let readme = fs.readFileSync(
+	require("path").join(process.cwd(), "template.md"),
+	"utf-8"
+);
 
 const doCompileAndReplace = (args, prefix, callback) => {
-	if(!tc.needResults(readme, prefix)) {
+	if (!tc.needResults(readme, prefix)) {
 		callback();
 		return;
 	}
-	if(fs.existsSync("dist"))
-		for(const file of fs.readdirSync("dist"))
-			fs.unlinkSync(`dist/${file}`);
+	if (fs.existsSync("dist"))
+		for (const file of fs.readdirSync("dist")) fs.unlinkSync(`dist/${file}`);
 
 	try {
 		require.resolve("webpack-cli");
@@ -33,26 +39,45 @@ const doCompileAndReplace = (args, prefix, callback) => {
 		throw new Error("Please install webpack-cli at root.");
 	}
 
-	cp.exec(`node ${path.resolve(__dirname, "../bin/webpack.js")} ${args} ${displayReasons} ${commonArgs}`, (error, stdout, stderr) => {
-		if(stderr)
-			console.log(stderr);
-		if(error !== null)
-			console.log(error);
-		try {
-			readme = tc.replaceResults(readme, process.cwd(), stdout.replace(/[\r?\n]*$/, ""), prefix);
-		} catch(e) {
-			console.log(stderr);
-			throw e;
+	cp.exec(
+		`node ${path.resolve(
+			__dirname,
+			"../bin/webpack.js"
+		)} ${args} ${displayReasons} ${commonArgs} --json > dist/stats.json`,
+		(error, stdout, stderr) => {
+			if (stderr) console.log(stderr);
+			if (error !== null) console.log(error);
+			try {
+				readme = tc.replaceResults(
+					readme,
+					process.cwd(),
+					stdout.replace(/[\r?\n]*$/, ""),
+					prefix
+				);
+			} catch (e) {
+				console.log(stderr);
+				throw e;
+			}
+			callback();
 		}
-		callback();
-	});
+	);
 };
 
-async.series([
-	callback => doCompileAndReplace("--mode production", "production", callback),
-	callback => doCompileAndReplace("--mode development --devtool none", "development", callback),
-	callback => doCompileAndReplace("--mode none --output-pathinfo", "", callback)
-], () => {
-	readme = tc.replaceBase(readme);
-	fs.writeFile("README.md", readme, "utf-8", function() {});
-});
+async.series(
+	[
+		callback =>
+			doCompileAndReplace("--mode production", "production", callback),
+		callback =>
+			doCompileAndReplace(
+				"--mode development --devtool none",
+				"development",
+				callback
+			),
+		callback =>
+			doCompileAndReplace("--mode none --output-pathinfo", "", callback)
+	],
+	() => {
+		readme = tc.replaceBase(readme);
+		fs.writeFile("README.md", readme, "utf-8", function() {});
+	}
+);
