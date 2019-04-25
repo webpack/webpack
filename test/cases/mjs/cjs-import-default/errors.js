df5e118da4a58f1472826654e8b6baf777d7c08f
alreadyExisted <vit.polyanskiy@gmail.com>
2019-04-25 18:58:20 +0300

    fix(mjs): remove unused namespace build errors

--- test/cases/mjs/cjs-import-default/errors.js
+++ test/cases/mjs/cjs-import-default/errors.js
@@ -1,18 +1,9 @@
 module.exports = [
 	[
-		/Can't import the namespace object from non EcmaScript module \(only default export is available\)/
-	],
-	[
-		/Can't import the namespace object from non EcmaScript module \(only default export is available\)/
-	],
-	[
 		/Can't import the named export 'data' from non EcmaScript module \(only default export is available\)/
 	],
 	[
 		/Can't import the named export 'data' from non EcmaScript module \(only default export is available\)/
-	],
-	[
-		/Can't reexport the namespace object from non EcmaScript module \(only default export is available\)/
 	],
 	[
 		/Can't reexport the named export 'data' from non EcmaScript module \(only default export is available\)/
