92f8c36ca2733e5c35ecdcc1561947ddae2a83ba
Tobias Koppers <tobias.koppers@googlemail.com>
2019-05-13 13:10:23 +0200

    add `minRemainingSize` option for splitChunks to ensure leftover chunks from splitting have a minimum size defaults to `minSize` of cache group defaults to global option when not specified global option defaults to zero in development

--- test/statsCases/named-chunk-groups/b.js
+++ test/statsCases/named-chunk-groups/b.js
@@ -1,3 +1,6 @@
 import "./shared";
 
 export default "b";
+
+// content content content content content content content content
+// content content content content content content content content
---
+++ test/statsCases/split-chunks-keep-remaining-size/b.js
@@ -0,0 +1,2 @@
+import "./shared";
+export default "b";
