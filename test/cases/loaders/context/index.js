faebdac88efdb187e0fd6c22ab02becc131b0879
Tobias Koppers <tobias.koppers@googlemail.com>
2019-08-05 15:52:52 +0200

    upgrade raw-loader

--- test/cases/loaders/context/index.js
+++ test/cases/loaders/context/index.js
@@ -1,5 +1,8 @@
 it("should be able to use a context with a loader", function() {
-	var abc = "abc", scr = "script.coffee";
+	var abc = "abc",
+		scr = "script.coffee";
 	expect(require("../_resources/" + scr)).toBe("coffee test");
-	expect(require("raw-loader!../_resources/" + abc + ".txt")).toBe("abc");
+	expect(require("raw-loader!../_resources/" + abc + ".txt").default).toBe(
+		"abc"
+	);
 });
--- test/cases/loaders/less-loader/index.js
+++ test/cases/loaders/less-loader/index.js
@@ -1,11 +1,11 @@
 it("should handle the less loader (piped with raw loader) correctly", function() {
 	expect(
-        require("!raw-loader!less-loader!./less/stylesheet.less").indexOf(".less-rule-direct")
-    ).not.toEqual(-1);
+		require("!raw-loader!less-loader!./less/stylesheet.less").default
+	).toContain(".less-rule-direct");
 	expect(
-        require("!raw-loader!less-loader!./less/stylesheet.less").indexOf(".less-rule-import1")
-    ).not.toEqual(-1);
+		require("!raw-loader!less-loader!./less/stylesheet.less").default
+	).toContain(".less-rule-import1");
 	expect(
-        require("!raw-loader!less-loader!./less/stylesheet.less").indexOf(".less-rule-import2")
-    ).not.toEqual(-1);
+		require("!raw-loader!less-loader!./less/stylesheet.less").default
+	).toContain(".less-rule-import2");
 });
