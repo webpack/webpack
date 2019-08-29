ab169893a196303f7fe562b92901cf10a3813a82
Tobias Koppers <tobias.koppers@googlemail.com>
2019-08-29 15:28:19 +0200

    refactor Parser to allow variable tagging
    use variable tagging in harmony pluging (instead of magic renaming)
    Parser no longer calls hooks.expression for x in `x = ...`, hooks.pattern is called instead
    remove rootName argument from hooks.expressionMemberChain and hooks.callMemberChain

--- test/cases/loaders/css-loader/index.js
+++ test/cases/loaders/css-loader/index.js
@@ -1,11 +1,11 @@
 it("should handle the css loader correctly", function() {
-	expect(
-        (require("!css-loader!../_css/stylesheet.css") + "").indexOf(".rule-direct")
-    ).not.toEqual(-1);
-	expect(
-        (require("!css-loader!../_css/stylesheet.css") + "").indexOf(".rule-import1")
-    ).not.toEqual(-1);
-	expect(
-        (require("!css-loader!../_css/stylesheet.css") + "").indexOf(".rule-import2")
-    ).not.toEqual(-1);
+	expect(require("!css-loader!../_css/stylesheet.css") + "").toContain(
+		".rule-direct"
+	);
+	expect(require("!css-loader!../_css/stylesheet.css") + "").toContain(
+		".rule-import1"
+	);
+	expect(require("!css-loader!../_css/stylesheet.css") + "").toContain(
+		".rule-import2"
+	);
 });
