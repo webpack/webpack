ba1a3e6f261b0165ad46d6b97ce1784b72424df7
Tobias Koppers <tobias.koppers@googlemail.com>
2019-09-05 14:10:16 +0200

    change assetEmitted hook to include more info

--- test/configCases/asset-emitted/normal/webpack.config.js
+++ test/configCases/asset-emitted/normal/webpack.config.js
@@ -1,10 +1,21 @@
+const Compilation = require("../../../../lib/Compilation");
+const Source = require("webpack-sources").Source;
+
 module.exports = {
 	plugins: [
 		compiler => {
 			const files = {};
-			compiler.hooks.assetEmitted.tap("Test", (file, buffer) => {
-				files[file] = Buffer.isBuffer(buffer);
-			});
+			compiler.hooks.assetEmitted.tap(
+				"Test",
+				(file, { content, source, outputPath, compilation, targetPath }) => {
+					expect(Buffer.isBuffer(content)).toBe(true);
+					expect(source).toBeInstanceOf(Source);
+					expect(typeof outputPath).toBe("string");
+					expect(typeof targetPath).toBe("string");
+					expect(compilation).toBeInstanceOf(Compilation);
+					files[file] = true;
+				}
+			);
 			compiler.hooks.afterEmit.tap("Test", () => {
 				expect(files).toMatchInlineSnapshot(`
 Object {
