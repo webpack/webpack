8393656e8602454a9a9e6479a6f634948e8691fc
aneeshjose1 <aneeshjose94@gmail.com>
2019-09-07 08:43:15 +0530

    Relational operator replaced by method

--- test/configCases/split-chunks/chunk-filename-delimiter-default/index.js
+++ test/configCases/split-chunks/chunk-filename-delimiter-default/index.js
@@ -1,14 +1,12 @@
 it("should run", function() {
-	Promise.all(
-		[
-			import(/* webpackChunkName: "a" */ "./a"),
-			import(/* webpackChunkName: "b" */ "./b"),
-			import(/* webpackChunkName: "c" */ "./c")
-		]
-	);
+	Promise.all([
+		import(/* webpackChunkName: "a" */ "./a"),
+		import(/* webpackChunkName: "b" */ "./b"),
+		import(/* webpackChunkName: "c" */ "./c")
+	]);
 
 	const files = require("fs").readdirSync(__dirname);
-	const hasFile = files.indexOf('a~b~c.bundle.js') !== -1;
+	const hasFIle = files.contains("a~b~c.bundle.js");
 
 	expect(hasFile).toBe(true);
 });
