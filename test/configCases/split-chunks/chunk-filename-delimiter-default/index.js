4ae7da77725ee9734c318a76179f91bf6b78bab2
codognito <54967040+codognito@users.noreply.github.com>
2019-09-08 06:27:13 +0530

    Update index.js

--- test/configCases/split-chunks/chunk-filename-delimiter-default/index.js
+++ test/configCases/split-chunks/chunk-filename-delimiter-default/index.js
@@ -6,7 +6,7 @@ it("should run", function() {
 	]);
 
 	const files = require("fs").readdirSync(__dirname);
-	const hasFIle = files.includes("a~b~c.bundle.js");
+	const hasFile = files.includes("a~b~c.bundle.js");
 
 	expect(hasFile).toBe(true);
 });
