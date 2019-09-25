1e4743dea47030d6fded7053f6d5e746721ad3f1
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-09-25 16:54:36 +0300

    Make maxChunks property required. Same as minChunkSize for MinChunkSizePlugin

--- declarations/plugins/optimize/LimitChunkCountPlugin.d.ts
+++ declarations/plugins/optimize/LimitChunkCountPlugin.d.ts
@@ -16,5 +16,5 @@ export interface LimitChunkCountPluginOptions {
 	/**
 	 * Limit the maximum number of chunks using a value greater greater than or equal to 1
 	 */
-	maxChunks?: number;
+	maxChunks: number;
 }
