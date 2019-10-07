9e7844bbfcad86f16982010fb53590e2a08200d1
Tobias Koppers <tobias.koppers@googlemail.com>
2019-10-07 14:42:33 +0200

    add "use strict" to all runtime modules

--- test/configCases/code-generation/use-strict/index.js
+++ test/configCases/code-generation/use-strict/index.js
@@ -20,6 +20,8 @@ it("should include only one use strict per module", function() {
 	expect(matches).toEqual([
 		'/* unused harmony default export */ var _unused_webpack_default_export = ("a");',
 		"/******/ 		// Check if module is in cache",
+		"/******/ 		// define __esModule on exports",
+		"/******/ 		// define getter functions for harmony exports",
 		"__webpack_require__.r(__webpack_exports__);",
 		"__webpack_require__.r(__webpack_exports__);",
 		"__webpack_require__.r(__webpack_exports__);",
