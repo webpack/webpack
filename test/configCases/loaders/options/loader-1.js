4e808cb6137ef2640b691db3e1867d202c2faaa2
evilebottnawi <sheo13666q@gmail.com>
2019-11-21 16:36:31 +0300

    feat: getOptions util for loader

---
+++ test/configCases/loaders/options/loader-1.js
@@ -0,0 +1,12 @@
+const schema = require('./loader-1.options');
+
+module.exports = function() {
+	const options = this.getOptions('Loader Name', schema);
+
+	const json = JSON.stringify(options)
+		.replace(/\u2028/g, '\\u2028')
+		.replace(/\u2029/g, '\\u2029');
+
+
+	return `module.exports = ${json}`;
+};
