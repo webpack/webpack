fa9ec727ba954fa4e230fa06d2c6faa4ebea0524
Tobias Koppers <tobias.koppers@googlemail.com>
2019-11-30 21:46:05 +0100

    strings are also valid json data
    simplify code
    remove incorrect test case

--- test/configCases/custom-modules/json-custom/webpack.config.js
+++ test/configCases/custom-modules/json-custom/webpack.config.js
@@ -17,22 +17,5 @@ module.exports = [
 				}
 			]
 		}
-	},
-	{
-		mode: "development",
-		module: {
-			rules: [
-				{
-					test: /\.toml$/,
-					type: "json",
-					parser: {
-						parse(input) {
-							expect(arguments.length).toBe(1);
-							return JSON.stringify(toml.parse(input));
-						}
-					}
-				}
-			]
-		}
 	}
 ];
