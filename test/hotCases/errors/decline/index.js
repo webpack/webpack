3b4a7455df6bdafbb8e84129b54c08e94af508c3
Tobias Koppers <tobias.koppers@googlemail.com>
2019-12-07 01:04:38 +0100

    use parser state instead of module to store harmony/dynamic exports state this is important for incremental build

--- test/hotCases/errors/decline/index.js
+++ test/hotCases/errors/decline/index.js
@@ -4,8 +4,8 @@ it("should abort when module is declined by parent", (done) => {
 	expect(a).toBe(1);
 	NEXT(require("../../update")((err) => {
 		try {
-			expect(/Aborted because of declined dependency: \.\/b\.js in \.\/a\.js/.test(err.message)).toBe(true);
-			expect(/Update propagation: \.\/c\.js -> \.\/b\.js -> \.\/a\.js/.test(err.message)).toBe(true);
+			expect(err.message).toMatch(/Aborted because of declined dependency: \.\/b\.js in \.\/a\.js/);
+			expect(err.message).toMatch(/Update propagation: \.\/c\.js -> \.\/b\.js -> \.\/a\.js/);
 			done();
 		} catch(e) {
 			done(e);
