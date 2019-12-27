6b9884ea50e49488f1070e1af188d014f1a612d7
Tobias Koppers <tobias.koppers@googlemail.com>
2019-12-27 14:18:06 +0100

    handle `webpack://` prefix in SourceMap sources

---
+++ test/configCases/source-map/relative-source-maps-by-loader/loader-source-root.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = function() {
+	this.callback(null, "module.exports = 'ok';", {
+		version: 3,
+		file: "/should/be/removed",
+		sourceRoot: path.join(__dirname, "folder"),
+		sources: ["test1.txt"],
+		sourcesContent: ["Test"],
+		mappings: "AAAA"
+	});
+};
