545a6370aaf063dfab73f404750935c423c9f637
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-01-15 18:20:00 +0530

    fix(auto-import): typo

--- test/hotCases/harmony/auto-import/index.js
+++ test/hotCases/harmony/auto-import/index.js
@@ -1,6 +1,6 @@
 import { value } from "./file";
 
-it("should auto-import a ES6 imported value on accept", function(done) {
+it("should auto-import an ES6 imported value on accept", function(done) {
 	expect(value).toBe(1);
 	module.hot.accept("./file", function() {
 		expect(value).toBe(2);
