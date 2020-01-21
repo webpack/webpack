aea4fd97b5c3ce0d4c58778493e5620c37716bf8
Gergely Nagy <ngg@ngg.hu>
2020-01-21 18:25:26 +0100

    do not evaluate constants in asm.js, fix IIFE mode detection
    fixes #10283

--- test/Compiler.test.js
+++ test/Compiler.test.js
@@ -170,6 +170,26 @@ describe("Compiler", () => {
 			done();
 		});
 	});
+
+	it("should not evaluate constants in asm.js", done => {
+		compile("./asmjs", {}, (stats, files) => {
+			expect(Object.keys(files)).toEqual(["/main.js"]);
+			const bundle = files["/main.js"];
+			expect(bundle).toMatch('"use asm";');
+			expect(bundle).toMatch("101");
+			expect(bundle).toMatch("102");
+			expect(bundle).toMatch("103");
+			expect(bundle).toMatch("104");
+			expect(bundle).toMatch("105");
+			expect(bundle).not.toMatch("106");
+			expect(bundle).not.toMatch("107");
+			expect(bundle).not.toMatch("108");
+			expect(bundle).toMatch("109");
+			expect(bundle).toMatch("110");
+			done();
+		});
+	});
+
 	describe("methods", () => {
 		let compiler;
 		beforeEach(() => {
