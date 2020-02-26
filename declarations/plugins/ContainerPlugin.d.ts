d33c0d144efca6c3a9ee934a66ad3db8734e6417
Tobias Koppers <tobias.koppers@googlemail.com>
2020-02-26 21:52:58 +0100

    use new library system in ContainerPlugin

--- declarations/plugins/ContainerPlugin.d.ts
+++ declarations/plugins/ContainerPlugin.d.ts
@@ -4,7 +4,48 @@
  * Run `yarn special-lint-fix` to update
  */
 
-export interface ContainerPlugin {
+/**
+ * Add a comment in the UMD wrapper.
+ */
+export type AuxiliaryComment = string | LibraryCustomUmdCommentObject;
+/**
+ * Specify which export should be exposed as library
+ */
+export type LibraryExport = string | ArrayOfStringValues;
+/**
+ * Array of strings
+ */
+export type ArrayOfStringValues = string[];
+/**
+ * The name of the library (some types allow unnamed libraries too)
+ */
+export type LibraryName = string | string[] | LibraryCustomUmdObject;
+/**
+ * Type of library
+ */
+export type LibraryType =
+	| "var"
+	| "module"
+	| "assign"
+	| "this"
+	| "window"
+	| "self"
+	| "global"
+	| "commonjs"
+	| "commonjs2"
+	| "commonjs-module"
+	| "amd"
+	| "amd-require"
+	| "umd"
+	| "umd2"
+	| "jsonp"
+	| "system";
+/**
+ * If `output.libraryTarget` is set to umd and `output.library` is set, setting this to true will name the AMD module.
+ */
+export type UmdNamedDefine = boolean;
+
+export interface ContainerPluginOptions {
 	/**
 	 * A map of modules you wish to expose
 	 */
@@ -18,21 +59,9 @@ export interface ContainerPlugin {
 	 */
 	filename?: string;
 	/**
-	 * Type of library
+	 * Options for library
 	 */
-	libraryTarget?:
-		| "var"
-		| "this"
-		| "window"
-		| "self"
-		| "global"
-		| "commonjs"
-		| "commonjs2"
-		| "amd"
-		| "amd-require"
-		| "umd"
-		| "umd2"
-		| "system";
+	library?: LibraryOptions;
 	/**
 	 * The name for this container
 	 */
@@ -40,7 +69,72 @@ export interface ContainerPlugin {
 	/**
 	 * An object for requests to override from host to this container
 	 */
-	overridables?: {
-		[k: string]: any;
-	};
+	overridables?:
+		| {
+				[k: string]: any;
+		  }
+		| any[];
+}
+/**
+ * Options for library
+ */
+export interface LibraryOptions {
+	/**
+	 * Add a comment in the UMD wrapper.
+	 */
+	auxiliaryComment?: AuxiliaryComment;
+	/**
+	 * Specify which export should be exposed as library
+	 */
+	export?: LibraryExport;
+	/**
+	 * The name of the library (some types allow unnamed libraries too)
+	 */
+	name?: LibraryName;
+	/**
+	 * Type of library
+	 */
+	type: LibraryType;
+	/**
+	 * If `output.libraryTarget` is set to umd and `output.library` is set, setting this to true will name the AMD module.
+	 */
+	umdNamedDefine?: UmdNamedDefine;
+}
+/**
+ * Set explicit comments for `commonjs`, `commonjs2`, `amd`, and `root`.
+ */
+export interface LibraryCustomUmdCommentObject {
+	/**
+	 * Set comment for `amd` section in UMD
+	 */
+	amd?: string;
+	/**
+	 * Set comment for `commonjs` (exports) section in UMD
+	 */
+	commonjs?: string;
+	/**
+	 * Set comment for `commonjs2` (module.exports) section in UMD
+	 */
+	commonjs2?: string;
+	/**
+	 * Set comment for `root` (global variable) section in UMD
+	 */
+	root?: string;
+}
+/**
+ * Description object for all UMD variants of the library name
+ */
+export interface LibraryCustomUmdObject {
+	/**
+	 * Name of the exposed AMD library in the UMD
+	 */
+	amd?: string;
+	/**
+	 * Name of the exposed commonjs export in the UMD
+	 */
+	commonjs?: string;
+	/**
+	 * Name of the property exposed globally by a UMD library
+	 */
+	root?: string | ArrayOfStringValues;
 }
--- test/configCases/container/container-entry/index.js
+++ test/configCases/container/container-entry/index.js
@@ -1,12 +1,11 @@
 it("should expose modules from the container", async () => {
-	const file = __non_webpack_require__("./container-file.js");
-	expect(file).toBeTypeOf("object");
-	expect(file.container).toBeTypeOf("object");
-	expect(file.container.get).toBeTypeOf("function");
-	const testFactory = await file.container.get("test");
+	const container = __non_webpack_require__("./container-file.js");
+	expect(container).toBeTypeOf("object");
+	expect(container.get).toBeTypeOf("function");
+	const testFactory = await container.get("test");
 	expect(testFactory).toBeTypeOf("function");
 	expect(testFactory()).toBe("test");
-	const test2Factory = await file.container.get("test2");
+	const test2Factory = await container.get("test2");
 	expect(test2Factory).toBeTypeOf("function");
 	expect(test2Factory()).toEqual(
 		nsObj({
