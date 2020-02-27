70b44a7f86c1b508a0b5e1f08cd0ca9bb8fc61a4
evilebottnawi <sheo13666q@gmail.com>
2020-02-27 19:49:47 +0300

    feat: support `[query]` for asset modules

---
+++ test/configCases/asset-modules/query/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.(png|svg|jpg)$/,
+				type: "asset/resource"
+			}
+		]
+	},
+	experiments: {
+		asset: true
+	}
+};
