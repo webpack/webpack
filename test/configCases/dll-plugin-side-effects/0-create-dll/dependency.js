c1aa9d45068c6920e281a447e9be799b8424a614
Tobias Koppers <tobias.koppers@googlemail.com>
2020-02-28 16:18:02 +0100

    flag all modules as used for Dll
    fix problem that modules were removed before they were flagged as used
    
    fixes #10189
    
    backport of #10205
    
    fixes #10475

---
+++ test/configCases/dll-plugin-side-effects/0-create-dll/dependency.js
@@ -0,0 +1,3 @@
+export default function createB() {
+	return "b";
+}
---
+++ test/configCases/dll-plugin/1-issue-10475/webpack.config.js
@@ -0,0 +1,12 @@
+var webpack = require("../../../../");
+
+module.exports = {
+	plugins: [
+		new webpack.DllReferencePlugin({
+			manifest: require("../../../js/config/dll-plugin/issue-10475.json"), // eslint-disable-line node/no-missing-require
+			name: "../0-issue-10475/dll.js",
+			scope: "dll",
+			sourceType: "commonjs2"
+		})
+	]
+};
