784ff508d0f2943c4023e7c8a4f55189ccb121ac
evilebottnawi <sheo13666q@gmail.com>
2020-03-02 14:09:54 +0300

    feat: `bigint` for the `DefinePlugin` plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
 		es6: true
 	},
 	parserOptions: {
-		ecmaVersion: 2018
+		ecmaVersion: 2020
 	},
 	rules: {
 		"prettier/prettier": "error",
--- test/configCases/plugins/define-plugin/index.js
+++ test/configCases/plugins/define-plugin/index.js
@@ -49,8 +49,6 @@ it("should define ONE", function() {
 it("should define BIGINT", function() {
 	expect(BIGINT).toBe(9007199254740991n);
 	expect((typeof BIGINT)).toBe("bigint");
-	if(BIGINT !== 9007199254740991n) require("fail");
-	if(typeof BIGINT !== "bigint") require("fail");
 });
 it("should define POSITIVE_ZERO", function() {
 	expect(POSITIVE_ZERO).toBe(+0);
