c8eb0242e13abf58bde31cb99527e54d175fbb67
Ivan Kopeykin <ikopeykin@ozon.ru>
2020-04-04 01:11:41 +0300

    update test case

--- test/cases/inner-graph/reexport-namespace-and-default/index.js
+++ test/cases/inner-graph/reexport-namespace-and-default/index.js
@@ -1,9 +1,11 @@
-import {exportDefaultUsed} from './package/script';
+import {exportDefaultUsed as export1} from './package/script';
+import {exportDefaultUsed as export2} from './package/script2';
 
 it("should load module correctly", () => {
 	require('./module');
 });
 
 it("default export should be unused", () => {
-	expect(exportDefaultUsed).toBe(false)
+	expect(export1).toBe(false);
+	expect(export2).toBe(false);
 });
--- test/cases/inner-graph/reexport-namespace-and-default/package/script1.js
+++ test/cases/inner-graph/reexport-namespace-and-default/package/script1.js
@@ -1,1 +1,2 @@
+export * from './script2'
 export default 1
