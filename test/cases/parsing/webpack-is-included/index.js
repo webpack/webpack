0a3b0c63d47b5e1ffe76f9e836f7cb96028ad68e
Ivan Kopeykin <ikopeykin@ozon.ru>
2020-04-14 02:05:02 +0300

    ignore error/warning when module does not exists

--- test/cases/parsing/webpack-is-included/index.js
+++ test/cases/parsing/webpack-is-included/index.js
@@ -2,7 +2,8 @@ import "./module1";
 import {
 	isWebpackIncludedFunction,
 	used,
-	unused
+	unused,
+	notPresented
 } from "./module2";
 
 it("__webpack_is_included__ should be a function", () => {
@@ -12,4 +13,8 @@ it("__webpack_is_included__ should be a function", () => {
 it("__webpack_is_included__ should be true for bundled modules, otherwise false", () => {
 	expect(used).toBe(true);
 	expect(unused).toBe(false);
+});
+
+it("__webpack_is_included__ should return false for missing module", () => {
+	expect(notPresented).toBe(false);
 });
