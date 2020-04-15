51ab478ce41312efa7c27e1f99abed199a79b861
fi3ework <fi3ework@gmail.com>
2020-04-16 06:47:50 +0800

    fix(ProfilingPlugin): crash under watch mode (fix #9114)

--- test/ProfilingPlugin.test.js
+++ test/ProfilingPlugin.test.js
@@ -6,7 +6,7 @@ const webpack = require("../");
 const rimraf = require("rimraf");
 
 describe("Profiling Plugin", function () {
-	jest.setTimeout(15000);
+	jest.setTimeout(30000);
 
 	it("should handle output path with folder creation", done => {
 		const outputPath = path.join(__dirname, "js/profilingPath");
@@ -29,6 +29,87 @@ describe("Profiling Plugin", function () {
 				if (!fs.existsSync(outputPath))
 					return done(new Error("Folder should be created."));
 				done();
+			});
+		});
+	});
+
+	it("should profiling on every compilation under watch mode", done => {
+		const fixturePath = path.join(
+			__dirname,
+			"fixtures",
+			"temp-watch-" + Date.now()
+		);
+		const filePath = path.join(fixturePath, "file.js");
+
+		try {
+			fs.mkdirSync(fixturePath);
+		} catch (e) {
+			// skip
+		}
+		try {
+			fs.writeFileSync(filePath, "'foo'", "utf-8");
+		} catch (e) {
+			// skip
+		}
+
+		const outputPath = path.join(__dirname, "js/profilingPathUnderWatchMode");
+		const finalPath = path.join(outputPath, "events.json");
+		const finalPath1 = path.join(outputPath, "events_1.json");
+		const finalPath2 = path.join(outputPath, "events_2.json");
+		let compilationIndex = 1;
+
+		rimraf(outputPath, () => {
+			const compiler = webpack({
+				mode: "development",
+				context: __dirname,
+				entry: filePath,
+				output: {
+					path: path.join(__dirname, "js/profilingOut")
+				},
+				plugins: [
+					new webpack.debug.ProfilingPlugin({
+						outputPath: finalPath
+					})
+				]
+			});
+
+			const watching = compiler.watch({ aggregateTimeout: 50 }, () => {});
+
+			setTimeout(() => {
+				fs.writeFileSync(filePath, "'bar'", "utf-8");
+			}, 1500);
+
+			setTimeout(() => {
+				fs.writeFileSync(filePath, "'baz'", "utf-8");
+			}, 3000);
+
+			compiler.hooks.done.tap("ProfilingUnderWatchTest", () => {
+				if (compilationIndex === 1) {
+					if (
+						!fs.existsSync(finalPath) ||
+						fs.existsSync(finalPath1) ||
+						fs.existsSync(finalPath2)
+					)
+						return done(new Error("Initial build output should be created."));
+				}
+
+				if (compilationIndex === 2) {
+					if (!fs.existsSync(finalPath1) || fs.existsSync(finalPath2))
+						return done(
+							new Error("Incremental build output should be created 1.")
+						);
+				}
+
+				if (compilationIndex === 3) {
+					if (!fs.existsSync(finalPath2))
+						return done(
+							new Error("Incremental build output should be created 2.")
+						);
+					watching.close();
+					done();
+				}
+
+				compilationIndex++;
 			});
 		});
 	});
