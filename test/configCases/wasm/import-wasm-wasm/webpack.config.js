49a6184e2040fe37a30b360134be5978e91a46c8
Tobias Koppers <tobias.koppers@googlemail.com>
2020-04-16 09:37:11 +0200

    add tests to increase code coverage

---
+++ test/configCases/wasm/import-wasm-wasm/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	entry: "./index",
+	module: {
+		rules: [
+			{
+				test: /\.wat$/,
+				loader: "wast-loader",
+				type: "webassembly/sync"
+			}
+		]
+	},
+	experiments: {
+		syncWebAssembly: true
+	}
+};
