c40d1085c217537b7a6759cc4d63b2aa8cc95e9d
chenxsan <chenxsan@gmail.com>
2020-05-08 20:39:50 +0800

    add schema for HotModuleReplacementPlugin

---
+++ declarations/plugins/HotModuleReplacementPlugin.d.ts
@@ -0,0 +1,17 @@
+/**
+ * This file was automatically generated.
+ * DO NOT MODIFY BY HAND.
+ * Run `yarn special-lint-fix` to update
+ */
+
+export interface HotModuleReplacementPluginOptions {
+	/**
+	 * The delay between the two steps when `multiStep` is enabled.
+	 */
+	fullBuildTimeout?: number;
+	/**
+	 * If true, the plugin will build in two steps -- first compiling the hot update chunks, and then the remaining normal assets.
+	 */
+	multiStep?: boolean;
+	[k: string]: any;
+}
