f65ab7e21d399176f7342d8ccfd9a99cf54424f1
goganchic <goganchic@gmail.com>
2020-05-17 15:43:09 +0300

    add test with combining data-uri and coffee-loader

--- test/cases/resolving/data-uri/index.js
+++ test/cases/resolving/data-uri/index.js
@@ -15,3 +15,9 @@ it("should import js module from base64 data-uri", function() {
 	expect(mod.number).toBe(42);
 	expect(mod.fn()).toBe("Hello world");
 });
+
+it("should require coffee module from base64 data-uri", function() {
+	const mod = require('coffee-loader!data:text/javascript;charset=utf-8;base64,bW9kdWxlLmV4cG9ydHMgPQogIG51bWJlcjogNDIKICBmbjogKCkgLT4gIkhlbGxvIHdvcmxkIg==');
+	expect(mod.number).toBe(42);
+	expect(mod.fn()).toBe("Hello world");
+});
