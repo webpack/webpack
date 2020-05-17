b9a818386aff5c745cd7ce3526fd1ee40896e42d
goganchic <goganchic@gmail.com>
2020-05-17 15:42:33 +0300

    add test for ascii data-uri

--- test/cases/resolving/data-uri/index.js
+++ test/cases/resolving/data-uri/index.js
@@ -1,10 +1,16 @@
-it("should require module from data-uri", function() {
+it("should require js module from base64 data-uri", function() {
 	const mod = require("data:text/javascript;charset=utf-8;base64,bW9kdWxlLmV4cG9ydHMgPSB7CiAgbnVtYmVyOiA0MiwKICBmbjogKCkgPT4gIkhlbGxvIHdvcmxkIgp9Ow==");
 	expect(mod.number).toBe(42);
 	expect(mod.fn()).toBe("Hello world");
 });
 
-it("should import module from data-uri", function() {
+it("should require js module from ascii data-uri", function() {
+	const mod = require("data:text/javascript;charset=utf-8;ascii,module.exports={number:42,fn:()=>\"Hello world\"}");
+	expect(mod.number).toBe(42);
+	expect(mod.fn()).toBe("Hello world");
+});
+
+it("should import js module from base64 data-uri", function() {
 	const mod = require('./module-with-imports');
 	expect(mod.number).toBe(42);
 	expect(mod.fn()).toBe("Hello world");
