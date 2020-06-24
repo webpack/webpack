d082f437522354201d20eff8863c1f99d4fe47d5
Tobias Koppers <tobias.koppers@googlemail.com>
2020-06-24 09:30:11 +0200

    fix testcases for new behavior

--- test/configCases/split-chunks/chunk-filename-delimiter/index.js
+++ test/configCases/split-chunks/chunk-filename-delimiter/index.js
@@ -1,12 +1,10 @@
-it("should run", function() {
-	Promise.all(
-		[
-			import(/* webpackChunkName: "a" */ "./a"),
-			import(/* webpackChunkName: "b" */ "./b")
-		]
-	);
+it("should run", function () {
+	Promise.all([
+		import(/* webpackChunkName: "a" */ "./a"),
+		import(/* webpackChunkName: "b" */ "./b")
+	]);
 
 	const files = require("fs").readdirSync(__dirname);
-	expect(files).toContain('a---a_js---2a91f0ff.bundle.js');
-	expect(files).toContain('b---b_js---c441f481.bundle.js');
+	expect(files).toContain("a.bundle.js");
+	expect(files).toContain("b---b_js---c441f481.bundle.js");
 });
