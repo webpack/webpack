da64c4bee460bebc8f97abb44680c761a10fa611
Tobias Koppers <tobias.koppers@googlemail.com>
2020-06-30 17:45:17 +0200

    fix watch test case

--- test/watchCases/sharing/provide-unsafe-cache/0/index.js
+++ test/watchCases/sharing/provide-unsafe-cache/0/index.js
@@ -4,7 +4,7 @@ it("should provide a module even when watching", () =>
 	import("package").then(async ({ default: value }) => {
 		await __webpack_init_sharing__("default");
 		expect(value).toBe("package");
-		const pkg = __webpack_share_scopes__.default.package;
+		const pkg = __webpack_share_scopes__.default.package["1,2,3"];
 		expect(pkg).toBeTypeOf("object");
 		expect(pkg.version).toEqual([1, 2, 3]);
 	}));
