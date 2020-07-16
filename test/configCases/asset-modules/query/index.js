29c3c5579d9db2c0df242aa5a168631ed716d554
Tobias Koppers <tobias.koppers@googlemail.com>
2020-07-16 08:01:22 +0200

    use parseResource util function add support for [fragment] too

--- test/configCases/asset-modules/query/index.js
+++ test/configCases/asset-modules/query/index.js
@@ -4,6 +4,6 @@ import jpg from "../_images/file.jpg?foo=bar#hash";
 
 it("should output various asset types", () => {
 	expect(png).toMatch(/^[\da-f]{20}\.png\?foo=bar$/);
-	expect(svg).toMatch(/^[\da-f]{20}\.svg$/);
+	expect(svg).toMatch(/^[\da-f]{20}\.svg\?$/);
 	expect(jpg).toMatch(/^[\da-f]{20}\.jpg\?foo=bar$/);
 });
