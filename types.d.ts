dfdc554b80056ab64a0ff2cf7a6357baea7d4149
Ivan Kopeykin <ikopeykin@ozon.ru>
2020-07-23 15:38:40 +0300

    add optional chaining support

--- types.d.ts
+++ types.d.ts
@@ -3562,6 +3562,7 @@ declare abstract class JavascriptParser extends Parser {
 	walkTemplateLiteral(expression?: any): void;
 	walkTaggedTemplateExpression(expression?: any): void;
 	walkClassExpression(expression?: any): void;
+	walkChainExpression(expression: ChainExpression): void;
 	walkImportExpression(expression?: any): void;
 	walkCallExpression(expression?: any, args?: any): void;
 	walkMemberExpression(expression?: any): void;
