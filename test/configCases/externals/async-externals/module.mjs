2bf2417fe01aeef6afad84eb9285274fd09116b6
Tobias Koppers <tobias.koppers@googlemail.com>
2020-08-07 17:34:36 +0200

    improve tests and fix fake type

--- test/configCases/externals/async-externals/module.mjs
+++ test/configCases/externals/async-externals/module.mjs
@@ -2,16 +2,15 @@ import value from "promise-external";
 import value2 from "module-promise-external";
 import value3 from "object-promise-external";
 import request from "import-external";
-import "./module.mjs";
 
-it("should allow async externals", () => {
+it("should allow async externals (in strict esm)", () => {
 	expect(value).toBe(42);
-	expect(value2).toBe(42);
+	expect(value2).toEqual({ __esModule: true, default: 42, named: true });
 	expect(value3).toEqual({ default: 42, named: true });
 	expect(request).toBe("/hello/world.js");
 });
 
-it("should allow to catch errors of async externals", () => {
+it("should allow to catch errors of async externals (in strict esm)", () => {
 	return expect(() => import("failing-promise-external")).rejects.toEqual(
 		expect.objectContaining({
 			message: "external reject"
@@ -19,23 +18,24 @@ it("should allow to catch errors of async externals", () => {
 	);
 });
 
-it("should allow dynamic import promise externals", () => {
+it("should allow dynamic import promise externals (in strict esm)", () => {
 	return import("promise-external").then(module => {
 		expect(module).toMatchObject({ default: 42 });
 	});
 });
 
-it("should allow dynamic import promise externals that are modules", () => {
+it("should allow dynamic import promise externals that are modules (in strict esm)", () => {
 	return import("module-promise-external").then(module => {
-		expect(module).toMatchObject({ default: 42, named: true });
+		expect(module).toMatchObject({
+			default: { __esModule: true, default: 42, named: true }
+		});
 	});
 });
 
-it("should allow dynamic import promise externals that are objects", () => {
+it("should allow dynamic import promise externals that are objects (in strict esm)", () => {
 	return import("object-promise-external").then(module => {
 		expect(module).toMatchObject({
-			default: { default: 42, named: true },
-			named: true
+			default: { default: 42, named: true }
 		});
 	});
 });
