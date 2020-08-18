ad1a53e3898e5f8f35f88bbb7b785f27df7a654a
chenxsan <chenxsan@gmail.com>
2020-08-18 12:39:16 +0800

    normalize charset

--- test/configCases/output/charset/index.js
+++ test/configCases/output/charset/index.js
@@ -5,6 +5,10 @@ it("should not add charset attribute", () => {
 	expect(document.head._children).toHaveLength(1);
 
 	const script = document.head._children[0];
+
+	__non_webpack_require__("./chunk1.js");
+	script.onload();
+
 	expect(script._type).toBe("script");
 	expect(script.src).toBe("https://example.com/public/path/chunk1.js");
 	expect(script.getAttribute("charset")).toBeUndefined();
