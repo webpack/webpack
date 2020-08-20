57bb2dd397b56160f47f28552e4379b04680f2a5
Daniel Abrao <danielpaladar@gmail.com>
2020-08-20 13:28:58 -0300

    Adjust test cases to pass production tests

--- test/cases/parsing/issue-11353/async_generator_function.js
+++ test/cases/parsing/issue-11353/async_generator_function.js
@@ -3,7 +3,6 @@
 export default async function* asyncIdMaker(start = 1, end = 5){
 	for (let i = start; i <= end; i++) {
 
-		// yay, can use await!
 		await new Promise(resolve => setTimeout(resolve, 1000));
 
 		yield i;
