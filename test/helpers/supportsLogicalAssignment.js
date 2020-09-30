b687d82ad4383f5a4a444db0881ac7baf7b2f793
Huáng Jùnliàng <jlhwung@gmail.com>
2020-09-29 22:29:33 -0400

    nit

--- test/helpers/supportsLogicalAssignment.js
+++ test/helpers/supportsLogicalAssignment.js
@@ -1,4 +1,4 @@
-module.exports = function supportsNullishCoalescing() {
+module.exports = function supportsLogicalAssignment() {
 	try {
 		var f = eval(
 			"(function f() { var x = null; x ??= true; x &&= true; return x ||= false; })"
