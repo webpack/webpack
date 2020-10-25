50c7c7e5343d5b96e3e51396332aa8c5459bdd81
Sam Cooke <sam.c@canva.com>
2020-10-25 10:14:48 +1000

    Try and improve test coverage?

---
+++ test/configCases/rebuild/finishModules/a.js
@@ -0,0 +1,9 @@
+
+import { doThings, foo } from './other-file';
+
+export {
+	doThings,
+	foo,
+}
+
+export const valueFromA = 'A';
--- test/configCases/rebuild/finishModules/index.js
+++ test/configCases/rebuild/finishModules/index.js
@@ -1,10 +1,11 @@
-import { doThings, foo }from "./other-file";
 
+import { doThings, foo, valueFromA } from './a';
 it("should compile", function (done) {
 	doThings(true);
 
+	// Should be replaced by the code in the config.
 	expect(foo.foo).toBe('bar');
-
+	expect(valueFromA).toBe('A')
 
 	done();
 });
