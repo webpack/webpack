d6aeae3fbc9dd4f9e1ac699cfd690d7141d0c760
Tobias Koppers <tobias.koppers@googlemail.com>
2020-10-27 21:13:41 +0100

    fix test cases where shared modules were used incorrectly

--- test/configCases/sharing/consume-multiple-versions/index.js
+++ test/configCases/sharing/consume-multiple-versions/index.js
@@ -35,16 +35,32 @@ it("should be able to consume different shared module version depending on conte
 			}
 		}
 	};
-	expect(require("shared")).toBe("shared@1.9.9");
-	expect(require("my-module")).toBe("shared@2.9.9");
-	expect(require("my-module2")).toBe("shared@2.3.9");
-	expect(() => require("my-module3")).toThrowError(
+	await expect(import("shared")).resolves.toHaveProperty(
+		"default",
+		"shared@1.9.9"
+	);
+	await expect(import("my-module")).resolves.toHaveProperty(
+		"default",
+		"shared@2.9.9"
+	);
+	await expect(import("my-module2")).resolves.toHaveProperty(
+		"default",
+		"shared@2.3.9"
+	);
+	await expect(import("my-module3")).rejects.toHaveProperty(
+		"message",
 		"No satisfying version (^3.4.5) of shared module shared found in shared scope default.\n" +
 			"Available versions: 9.9.9 from undefined, 1.9.9 from undefined, 1.2.9 from undefined, 1.2.3 from mfe1, 2.9.9 from undefined, 2.3.9 from undefined, 2.3.4 from undefined, 3.0.0 from undefined"
 	);
-	expect(require("my-module4")).toBe("shared@9.9.9");
+	await expect(import("my-module4")).resolves.toHaveProperty(
+		"default",
+		"shared@9.9.9"
+	);
 	expectWarning();
-	expect(require("shared2")).toBe("shared2@9.9.9");
+	await expect(import("shared2")).resolves.toHaveProperty(
+		"default",
+		"shared2@9.9.9"
+	);
 	expectWarning(
 		/No satisfying version \(=1\.2\.3 =3\.2\.1\) of shared module shared2 found in shared scope default/
 	);
