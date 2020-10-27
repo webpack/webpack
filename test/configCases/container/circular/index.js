48c53771314377d98fa61eaed6eff7302cb65b9d
Tobias Koppers <tobias.koppers@googlemail.com>
2020-10-27 16:01:15 +0100

    allow to use non-eager shared and remote modules in the initial page load
    bundle will become async to load the required remotes and chunks
    fix a bug with circular remote module loading
    fix duplicate remote and shared runtime module when using plugin multiple times

--- test/configCases/container/circular/index.js
+++ test/configCases/container/circular/index.js
@@ -1,13 +1,12 @@
-it("should allow circular dependencies between containers (a)", async () => {
-	const { default: value, b, ba } = await import("container/a");
-	expect(value).toBe("a");
+it("should allow circular dependencies between containers (parallel)", async () => {
+	const [
+		{ default: value1, b, ba },
+		{ default: value2, a, ab }
+	] = await Promise.all([import("container/a"), import("container2/b")]);
+	expect(value1).toBe("a");
 	expect(b).toBe("b");
 	expect(ba).toBe("a");
-});
-
-it("should allow circular dependencies between containers (b)", async () => {
-	const { default: value, a, ab } = await import("container2/b");
-	expect(value).toBe("b");
+	expect(value2).toBe("b");
 	expect(a).toBe("a");
 	expect(ab).toBe("b");
 });
