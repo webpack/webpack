6092ff345371966d4cec13a7e35235279ef4601b
evilebottnawi <sheo13666q@gmail.com>
2020-10-30 21:16:58 +0300

    fix: more

--- test/target.unittest.js
+++ test/target.unittest.js
@@ -4,10 +4,17 @@ describe("browserslist target", () => {
 	const tests = [
 		["ie 11"],
 		["chrome 80"],
-		["chrome 80.0"],
-		["node 6"],
-		["node 6.0"],
-		["node 6.0.0"],
+		["node 0.12"],
+		["node 10"],
+		["node 10.0"],
+		["node 10.17"],
+		["node 10.0.0"],
+		["node 10.17.0"],
+		["node 12"],
+		["safari 10"],
+		["safari TP"],
+		["safari 11"],
+		["safari 12.4"],
 		["firefox 80 ", "chrome 80"]
 	];
 
