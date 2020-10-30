7f691aacdfe91180c40816c4c9b24e76a8567ac6
Tobias Koppers <tobias.koppers@googlemail.com>
2020-10-30 16:59:35 +0100

    enable named exports for commonjs as node.js supports it now
    fixes #11878

--- test/cases/mjs/cjs-import-default/index.mjs
+++ test/cases/mjs/cjs-import-default/index.mjs
@@ -1,12 +1,17 @@
 import { data } from "./cjs.js";
 import * as star from "./cjs.js";
 import def from "./cjs.js";
-import { ns, default as def1, def as def2, data as data2 } from "./reexport.mjs";
+import {
+	ns,
+	default as def1,
+	def as def2,
+	data as data2
+} from "./reexport.mjs";
 import * as reexport from "./reexport.mjs";
 
-it("should get correct values when importing named exports from a CommonJs module from mjs", function() {
-	expect(typeof data).toBe("undefined");
-	expect({ data }).toEqual({ data: undefined });
+it("should get correct values when importing named exports from a CommonJs module from mjs", function () {
+	expect(typeof data).toBe("string");
+	expect({ data }).toEqual({ data: "ok" });
 	expect(def).toEqual({
 		data: "ok",
 		default: "default"
@@ -17,30 +22,37 @@ it("should get correct values when importing named exports from a CommonJs modul
 			default: "default"
 		}
 	});
-	expect(star).toEqual(nsObj({
-		default: {
-			data: "ok",
-			default: "default"
-		}
-	}));
+	expect(star).toEqual(
+		nsObj({
+			default: {
+				data: "ok",
+				default: "default"
+			},
+			data: "ok"
+		})
+	);
 	expect({ star }).toEqual({
 		star: nsObj({
 			default: {
 				data: "ok",
 				default: "default"
-			}
+			},
+			data: "ok"
 		})
 	});
 	expect(star.default).toEqual({
 		data: "ok",
 		default: "default"
 	});
-	expect(ns).toEqual(nsObj({
-		default: {
-			data: "ok",
-			default: "default"
-		}
-	}));
+	expect(ns).toEqual(
+		nsObj({
+			default: {
+				data: "ok",
+				default: "default"
+			},
+			data: "ok"
+		})
+	);
 	expect(def1).toEqual({
 		data: "ok",
 		default: "default"
@@ -49,22 +61,25 @@ it("should get correct values when importing named exports from a CommonJs modul
 		data: "ok",
 		default: "default"
 	});
-	expect((typeof data2)).toBe("undefined");
-	expect(reexport).toEqual(nsObj({
-		ns: nsObj({
+	expect(typeof data2).toBe("string");
+	expect(reexport).toEqual(
+		nsObj({
+			ns: nsObj({
+				default: {
+					data: "ok",
+					default: "default"
+				},
+				data: "ok"
+			}),
 			default: {
 				data: "ok",
 				default: "default"
-			}
-		}),
-		default: {
-			data: "ok",
-			default: "default"
-		},
-		def: {
-			data: "ok",
-			default: "default"
-		},
-		data: undefined
-	}));
+			},
+			def: {
+				data: "ok",
+				default: "default"
+			},
+			data: "ok"
+		})
+	);
 });
