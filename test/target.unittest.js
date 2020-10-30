6d5c69e37a4b44d34b5b73079f941ead67385b7c
evilebottnawi <sheo13666q@gmail.com>
2020-10-30 21:38:39 +0300

    test: remove invalid

--- test/target.unittest.js
+++ test/target.unittest.js
@@ -4,18 +4,15 @@ describe("browserslist target", () => {
 	const tests = [
 		["ie 11"],
 		["chrome 80"],
-		["node 0.10"],
-		["node 0.12"],
-		["node 10"],
-		["node 10.0"],
-		["node 10.17"],
+		["node 0.10.0"],
+		["node 0.12.0"],
 		["node 10.0.0"],
 		["node 10.17.0"],
 		["node 12"],
 		["safari 10"],
 		["safari TP"],
 		["safari 11"],
-		["safari 12.4"],
+		["safari 12.1"],
 		["firefox 80 ", "chrome 80"]
 	];
 
