fd7c4c8dd5b48b46b7bf93cbdc1c3e495b6fb980
evilebottnawi <sheo13666q@gmail.com>
2020-10-31 16:54:47 +0300

    fix: more

--- test/target.unittest.js
+++ test/target.unittest.js
@@ -8,14 +8,15 @@ describe("browserslist target", () => {
 		["node 0.12.0"],
 		["node 10.0.0"],
 		["node 10.17.0"],
-		["node 12"],
+		["node 12.19.0"],
 		["safari 10"],
 		["safari TP"],
 		["safari 11"],
 		["safari 12.0"],
 		["safari 12.1"],
 		["safari 13"],
-		["firefox 80 ", "chrome 80"]
+		["firefox 80", "chrome 80"],
+		["chrome 80", "node 12.19.0"]
 	];
 
 	for (const test of tests) {
