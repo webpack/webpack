8a1b008a4631a4197f4f101a5e3eec0981fb693a
Jason Williams <jase.williams@gmail.com>
2020-11-07 15:10:12 +0000

    simplify the testcase

--- test/configCases/source-map/namespace-source-path-no-truncate/index.js
+++ test/configCases/source-map/namespace-source-path-no-truncate/index.js
@@ -1,8 +1,8 @@
-it("should include webpack://mynamespace/./[id].js in SourceMap", function () {
+it("should include [id].js in SourceMap", function () {
 	var fs = require("fs");
 	var source = fs.readFileSync(__filename + ".map", "utf-8");
 	var map = JSON.parse(source);
-	expect(map.sources).toContain("webpack://mynamespace/./[id].js");
+	expect(map.sources).toContain("webpack:///./[id].js");
 });
 
 if (Math.random() < 0) require("./[id].js");
--- test/configCases/source-map/namespace-source-path-no-truncate/webpack.config.js
+++ test/configCases/source-map/namespace-source-path-no-truncate/webpack.config.js
@@ -1,12 +1,11 @@
 /** @type {import("../../../../").Configuration} */
 module.exports = {
-	mode: "development",
-	output: {
-		devtoolNamespace: "mynamespace"
-	},
 	node: {
 		__dirname: false,
 		__filename: false
 	},
-	devtool: "cheap-source-map"
+	devtool: "source-map",
+	optimization: {
+		minimize: true
+	}
 };
