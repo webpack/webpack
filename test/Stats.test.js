590aacc2ae7e734bef5834075f0aefb4cbad5511
Tobias Koppers <tobias.koppers@googlemail.com>
2020-11-16 17:19:36 +0100

    make visible exports from non-harmony modules more consistent
    fixes #11995

--- test/Stats.test.js
+++ test/Stats.test.js
@@ -173,10 +173,10 @@ describe("Stats", () => {
 			      "assets": Array [
 			        Object {
 			          "name": "entryB.js",
-			          "size": 2706,
+			          "size": 2908,
 			        },
 			      ],
-			      "assetsSize": 2706,
+			      "assetsSize": 2908,
 			      "auxiliaryAssets": undefined,
 			      "auxiliaryAssetsSize": 0,
 			      "childAssets": undefined,
@@ -221,10 +221,10 @@ describe("Stats", () => {
 			      "info": Object {
 			        "javascriptModule": false,
 			        "minimized": true,
-			        "size": 2706,
+			        "size": 2908,
 			      },
 			      "name": "entryB.js",
-			      "size": 2706,
+			      "size": 2908,
 			      "type": "asset",
 			    },
 			    Object {
---
+++ test/cases/mjs/non-enumerable/warnings.js
@@ -0,0 +1,6 @@
+module.exports = [
+	[/export 'default' \(imported as 'm3'\) was not found in '\.\/esModule\.js'/],
+	[/export 'default' \(imported as 'd3'\) was not found in '\.\/esModule\.js'/],
+	[/export 'default' \(imported as 'm3'\) was not found in '\.\/esModule\.js'/],
+	[/export 'default' \(imported as 'd3'\) was not found in '\.\/esModule\.js'/]
+];
