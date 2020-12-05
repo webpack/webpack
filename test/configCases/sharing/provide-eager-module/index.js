b258a4d84d94f42c1654110099ee8e7e8a2fca6b
Jake Northey <jake@creativemines.dev>
2020-12-05 14:58:11 -0500

    test: fix eager module tests

--- test/configCases/sharing/provide-eager-module/index.js
+++ test/configCases/sharing/provide-eager-module/index.js
@@ -15,9 +15,6 @@ it("should overwrite non-eager shared module with eager shared module", async ()
 		}
 	}
 	await __webpack_init_sharing__("default");
-	//await __webpack_require__.I("default");
-	console.log(__webpack_share_scopes__);
-	console.log(__webpack_share_scopes__.default);
 	expect(Object.keys(__webpack_share_scopes__["default"])).toContain(
 		"common"
 	);
@@ -33,7 +30,7 @@ it("should not overwrite already shared eager module with non-eager module", asy
 			uncommon: {
 				'2.0.0': {
 					eager: true,
-					from: 'dashboard'
+					from: 'aaa'
 				}
 			}
 		}
@@ -42,9 +39,9 @@ it("should not overwrite already shared eager module with non-eager module", asy
 	expect(Object.keys(__webpack_share_scopes__["default"])).toContain(
 		"uncommon"
 	);
-	const uncommonModule = __webpack_share_scopes__.default.common
-	expect(Object.keys(commonModule)).toContain(
+	const uncommonModule = __webpack_share_scopes__.default.uncommon
+	expect(Object.keys(uncommonModule)).toContain(
 		"2.0.0"
 	);
-	expect(commonModule["2.0.0"].eager).toBe(true)
+	expect(uncommonModule["2.0.0"].eager).toBe(true)
 });
