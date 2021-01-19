f976ddd6e1fe67638397ad9369ac01e1f930873b
Tobias Koppers <tobias.koppers@googlemail.com>
2021-01-19 17:24:37 +0100

    update test cases for css-loader update

--- test/cases/parsing/bom/index.js
+++ test/cases/parsing/bom/index.js
@@ -1,14 +1,14 @@
-it("should load a utf-8 file with BOM", function() {
+it("should load a utf-8 file with BOM", function () {
 	var result = require("./bomfile");
 	expect(result).toEqual("ok");
 });
 
-it("should load a css file with BOM", function() {
-	var css = require("!css-loader!./bomfile.css") + "";
+it("should load a css file with BOM", function () {
+	var css = require("!css-loader!./bomfile.css").default + "";
 	expect(css).toBe("body{color:#abc}");
 });
 
-it("should load a json file with BOM", function() {
+it("should load a json file with BOM", function () {
 	var result = require("./bomfile.json");
 	expect(result.message).toEqual("ok");
 });
