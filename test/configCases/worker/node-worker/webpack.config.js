ca0df823c8a7f6269a331c1f2b0fba859c036296
Tobias Koppers <tobias.koppers@googlemail.com>
2021-02-14 21:21:16 +0100

    order runtime chunks correctly for hashing
    fixes #12676

--- test/configCases/worker/node-worker/webpack.config.js
+++ test/configCases/worker/node-worker/webpack.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+	entry: {
+		a: { import: "./index.js?a", filename: "[name].js" },
+		b: { import: "./index.js?b", filename: "[name].js" },
+		c: { import: "./index.js?c", filename: "[name].js" },
+		d: { import: "./index.js?d", filename: "[name].js" }
+	},
 	output: {
-		filename: "[name].js"
+		filename: "[name].[contenthash].js"
 	}
 };
