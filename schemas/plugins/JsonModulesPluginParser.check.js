e21b1d46fe7f3bab89260c28ad0ac501e96c8f44
Tobias Koppers <tobias.koppers@googlemail.com>
2021-04-16 15:35:18 +0200

    precompile schemas for improved startup performance

---
+++ schemas/plugins/JsonModulesPluginParser.check.js
@@ -0,0 +1,6 @@
+/*
+ * This file was automatically generated.
+ * DO NOT MODIFY BY HAND.
+ * Run `yarn special-lint-fix` to update
+ */
+"use strict";function r(t,{instancePath:e="",parentData:a,parentDataProperty:o,rootData:n=t}={}){if(!t||"object"!=typeof t||Array.isArray(t))return r.errors=[{params:{type:"object"}}],!1;{const e=0;for(const e in t)if("parse"!==e)return r.errors=[{params:{additionalProperty:e}}],!1;if(0===e&&void 0!==t.parse&&!(t.parse instanceof Function))return r.errors=[{params:{}}],!1}return r.errors=null,!0}module.exports=r,module.exports.default=r;
---
+++ schemas/plugins/optimize/LimitChunkCountPlugin.check.js
@@ -0,0 +1,6 @@
+/*
+ * This file was automatically generated.
+ * DO NOT MODIFY BY HAND.
+ * Run `yarn special-lint-fix` to update
+ */
+"use strict";function r(e,{instancePath:t="",parentData:n,parentDataProperty:i,rootData:a=e}={}){if(!e||"object"!=typeof e||Array.isArray(e))return r.errors=[{params:{type:"object"}}],!1;{let t;if(void 0===e.maxChunks&&(t="maxChunks"))return r.errors=[{params:{missingProperty:t}}],!1;{const t=0;for(const t in e)if("chunkOverhead"!==t&&"entryChunkMultiplicator"!==t&&"maxChunks"!==t)return r.errors=[{params:{additionalProperty:t}}],!1;if(0===t){if(void 0!==e.chunkOverhead){let t=e.chunkOverhead;const n=0;if("number"!=typeof t||!isFinite(t))return r.errors=[{params:{type:"number"}}],!1;var s=0===n}else s=!0;if(s){if(void 0!==e.entryChunkMultiplicator){let t=e.entryChunkMultiplicator;const n=0;if("number"!=typeof t||!isFinite(t))return r.errors=[{params:{type:"number"}}],!1;s=0===n}else s=!0;if(s)if(void 0!==e.maxChunks){let t=e.maxChunks;const n=0;if(0===n){if("number"!=typeof t||!isFinite(t))return r.errors=[{params:{type:"number"}}],!1;if(t<1||isNaN(t))return r.errors=[{params:{comparison:">=",limit:1}}],!1}s=0===n}else s=!0}}}}return r.errors=null,!0}module.exports=r,module.exports.default=r;
