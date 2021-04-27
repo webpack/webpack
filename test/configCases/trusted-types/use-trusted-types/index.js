cff99ee06688ccbe2aab5f5eb5deb57cfaab7966
Bjarki <bjarki@google.com>
2021-04-27 10:52:27 +0000

    Split use-trusted-types into two test cases
    This test checks for behaviour in environments with and without Trusted
    Types support. Separate this into two tests so that they each run in a
    clean environment.

--- test/configCases/trusted-types/use-trusted-types/index.js
+++ test/configCases/trusted-types/use-trusted-types/index.js
@@ -1,13 +1,3 @@
-it("should load chunk when there are no trusted types", function() {
-	const promise = import("./empty?a" /* webpackChunkName: "no-trusted-types" */);
-
-	var script = document.head._children.pop();
-	__non_webpack_require__("./no-trusted-types.web.js");
-	expect(script.src).toBe("https://test.cases/path/no-trusted-types.web.js");
-
-	return promise;
-});
-
 it("should load chunk using trusted types", function() {
 	// emulate trusted types in a window object
 	const noop = (i) => i
