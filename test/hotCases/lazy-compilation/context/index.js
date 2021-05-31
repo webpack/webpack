00bacbec2575ccb590ac0433996f1c8bb3adde07
Juho Vepsäläinen <bebraw@gmail.com>
2021-05-31 15:06:58 +0300

    chore: Add a todo

--- test/hotCases/lazy-compilation/context/index.js
+++ test/hotCases/lazy-compilation/context/index.js
@@ -1,8 +1,11 @@
+// TODO: Why is this giving "No tests exported by test case"?
 it("should compile to lazy imported context", done => {
 	const req = require.context("./modules", /^\.\/.*\.js$/);
 	const result = req("./demo");
 
-	expect(result).toBe(42);
-
-	done();
+	// It's not clear why timeout would be needed now since req is a sync call
+	setTimeout(() => {
+		expect(result).toBe(42);
+		done();
+	}, 1000);
 });
