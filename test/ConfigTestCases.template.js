f52adf35b6ab873555af10828ee9eeb904ac1d28
Yujun Jung <yj01jung@gmail.com>
2021-08-16 14:21:51 +0900

    remove mistake

--- test/ConfigTestCases.template.js
+++ test/ConfigTestCases.template.js
@@ -565,7 +565,6 @@ const describeCases = config => {
 											if (key.includes("webpack")) delete global[key];
 										}
 										if (getNumberOfTests() < filesCount) {
-											console.log(getNumberOfTests() + " / " + filesCount);
 											return done(new Error("No tests exported by test case"));
 										}
 										done();
