15393ca45978c126d5c2ab7e3ad0a0db25b59278
Ivan Kopeykin <ikopeykin@ozon.ru>
2021-09-20 15:54:09 +0300

    more complex test case

--- test/cases/parsing/issue-7519/a.js
+++ test/cases/parsing/issue-7519/a.js
@@ -4,9 +4,17 @@ export function inc() {
 	count++;
 }
 
+export function incTruthy() {
+	count++;
+	return true;
+}
+
 export function mult(n) {
 	count *= n;
 }
 
+export function setCount(c) {
+	count = c;
+}
+
 export const multUsed = __webpack_exports_info__.mult.used;
-export const incUsed = __webpack_exports_info__.inc.used;
--- test/cases/parsing/issue-7519/index.js
+++ test/cases/parsing/issue-7519/index.js
@@ -2,16 +2,30 @@ import {
 	count,
 	mult,
 	inc,
+	incTruthy,
+	setCount,
 	multUsed,
 	incUsed
 } from "./a";
 
-it("simple logical expression should work", () => {
-	inc() && true && false && mult(2);
+it("logical 'and' should work", () => {
+	setCount(1);
+	inc() && "true" && 0 && mult(2);
 	expect(count).toBe(2);
 	inc() && false && mult(2);
 	expect(count).toBe(3);
-	true && inc() && false && mult(2);
+	true && inc() && inc() && false && mult(2);
+	/* inc itself returns undefined */
+	expect(count).toBe(4);
+	true && incTruthy() && incTruthy() && false && mult(2);
+	expect(count).toBe(6);
+});
+
+it("logical 'or' should work", () => {
+	setCount(1);
+	false || "" || inc();
+	expect(count).toBe(2);
+	(0 || "" || inc() || inc()) && false && mult(2);
 	expect(count).toBe(4);
 });
 
