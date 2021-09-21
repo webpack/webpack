bf016616f46aab85a51ee536774e8d5be855a33e
Ivan Kopeykin <ikopeykin@ozon.ru>
2021-09-21 10:10:17 +0300

    fix test case

--- test/configCases/output-module/node-globals/index.js
+++ test/configCases/output-module/node-globals/index.js
@@ -1,9 +1,11 @@
 import { dir, file } from './cjs/file.js'
 
 it("should generate correct __dirname", () => {
-	expect(dir.slice(dir.lastIndexOf("/", dir.length - 2))).toMatch(/[\\/]node-globals[\\/]/);
+	const match = dir.match(/[\\/][^\\/]+[\\/]$/);
+	expect(match && match[0]).toMatch(/[\\/]node-globals[\\/]/);
 });
 
 it("should generate correct __filename", () => {
-	expect(file.slice(file.lastIndexOf("/"))).toMatch(/[\\/]main.mjs$/);
+	const match = file.match(/[\\/][^\\/]+$/);
+	expect(match && match[0]).toMatch(/[\\/]main.mjs$/);
 });
