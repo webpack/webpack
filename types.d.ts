0e13c7dea4160f96cad7f26cca93c17ab841de20
Tobias Koppers <tobias.koppers@googlemail.com>
2021-10-07 15:34:21 +0200

    track chunk combinations for modules
    improves caching
    improves SplitChunksPlugin

--- types.d.ts
+++ types.d.ts
@@ -757,6 +757,17 @@ declare class Chunk {
 		filterFn?: (c: Chunk, chunkGraph: ChunkGraph) => boolean
 	): Record<string | number, Record<string, (string | number)[]>>;
 }
+declare abstract class ChunkCombination {
+	debugId: number;
+	size: number;
+	readonly chunksIterable: Iterable<Chunk>;
+	with(chunk: Chunk): ChunkCombination;
+	without(chunk: Chunk): ChunkCombination;
+	withAll(other?: any): any;
+	hasSharedChunks(other?: any): boolean;
+	isSubset(other: ChunkCombination): boolean;
+	getChunks(): Chunk[];
+}
 declare class ChunkGraph {
 	constructor(moduleGraph: ModuleGraph, hashFunction?: string | typeof Hash);
 	moduleGraph: ModuleGraph;
@@ -774,6 +785,7 @@ declare class ChunkGraph {
 	isModuleInChunk(module: Module, chunk: Chunk): boolean;
 	isModuleInChunkGroup(module: Module, chunkGroup: ChunkGroup): boolean;
 	isEntryModule(module: Module): boolean;
+	getModuleChunkCombination(module: Module): ChunkCombination;
 	getModuleChunksIterable(module: Module): Iterable<Chunk>;
 	getOrderedModuleChunksIterable(
 		module: Module,
