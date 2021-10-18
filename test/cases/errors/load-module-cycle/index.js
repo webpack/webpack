91659f2bcb369e6d0307aba51c367a884a58df99
Hal Blackburn <hwtb2@cam.ac.uk>
2021-10-18 20:08:56 +0100

    refactor load-module-cycle test loader
    It can now load more than one ref per module, which is needed to test
    issue #14379.

--- test/cases/errors/load-module-cycle/index.js
+++ test/cases/errors/load-module-cycle/index.js
@@ -1,15 +1,21 @@
 it("should error loadModule when a cycle with 2 modules is requested", () => {
-	expect(require("./loader!./2/a")).toMatch(
-		/^source: err: There is a circular build dependency/
-	);
+	expect(require("./loader!./2/a")).toEqual([
+		["./b.json", [
+			["./a.json", "err: There is a circular build dependency, which makes it impossible to create this module"]
+		]]
+	]);
 });
 it("should error loadModule when a cycle with 3 modules is requested", () => {
-	expect(require("./loader!./3/a")).toMatch(
-		/^source: source: err: There is a circular build dependency/
-	);
+	expect(require("./loader!./3/a")).toEqual([
+		["./b.json", [
+			["./c.json", [
+				["./a.json", "err: There is a circular build dependency, which makes it impossible to create this module"]
+			]]
+		]]
+	]);
 });
 it("should error loadModule when requesting itself", () => {
-	expect(require("./loader!./1/a")).toMatch(
-		/^err: There is a circular build dependency/
-	);
+	expect(require("./loader!./1/a")).toEqual([
+		["./a.json", "err: There is a circular build dependency, which makes it impossible to create this module"]
+	]);
 });
