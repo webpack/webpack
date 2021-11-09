393fb6e22c27cae0c6cffe19ba1a5ae710c0f9b5
Tobias Koppers <tobias.koppers@googlemail.com>
2021-11-09 10:42:40 +0100

    fix a bug with experiments.cacheUnaffected
    that causes missing invalidation for modules when only the module id has changed

--- test/watchCases/cache/changing-module-id/0/index.js
+++ test/watchCases/cache/changing-module-id/0/index.js
@@ -2,8 +2,9 @@ import value from "./module";
 import value2 from "./unrelated";
 import value3 from "./other-module";
 
-it("should work when modules change ids", () => {
+it("should work when modules change ids", async () => {
 	expect(value).toBe(42);
 	expect(value2).toBe(42);
 	expect(value3).toBe(42 + +WATCH_STEP);
+	expect(import("./module?async")).resolves.toEqual(nsObj({ default: 42 }));
 });
