110e8044dc99e6b6e0ddfe51755cae393cf3a72f
Tobias Koppers <tobias.koppers@googlemail.com>
2021-12-01 09:50:13 +0100

    add test case

--- test/cases/css-modules/basic/index.js
+++
@@ -1,3 +0,0 @@
-import "./style.css";
-
-it("should compile", () => {});
---
+++ test/configCases/css/basic/index.js
@@ -0,0 +1,12 @@
+import * as style from "./style.css";
+
+it("should compile and load style on demand", done => {
+	expect(style).toEqual(nsObj({}));
+	import("./style2.css").then(x => {
+		expect(x).toEqual(nsObj({}));
+		const style = getComputedStyle(document.body);
+		expect(style.getPropertyValue("background")).toBe(" red");
+		expect(style.getPropertyValue("color")).toBe(" green");
+		done();
+	}, done);
+});
