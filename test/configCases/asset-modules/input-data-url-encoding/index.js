420e8b68151e2a3befac5b3550e2ac8c9ceb3d53
evilebottnawi <sheo13666q@gmail.com>
2022-01-17 18:47:49 +0300

    fix: logic

--- test/configCases/asset-modules/input-data-url-encoding/index.js
+++ test/configCases/asset-modules/input-data-url-encoding/index.js
@@ -4,7 +4,7 @@ it("should keep original encoding", () => {
 		import.meta.url
 	);
 	expect(url.href).toBe(
-		"data:image/svg+xml;p=1;q=2,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20viewBox%3D%270%200%2016%2016%27%3E%3Cpath%20fill%3D%27none%27%20stroke%3D%22%23343a40%22%20stroke-linecap%3D%27round%27%20stroke-linejoin%3D%27round%27%20stroke-width%3D%272%27%20d%3D%27M2%205l6%206%206-6%27%2F%3E%3C%2Fsvg%3E"
+		"data:image/svg+xml;p=1;q=2,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke=\"%23343a40\" stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e"
 	);
 });
 
