f1d329cc0e5241327575d3b3d6f491ff95773c8b
Ivan Kopeykin <ivan.kopeykin@gmail.com>
2022-02-01 13:35:12 +0300

    use nonNumericOnlyHash function for contentHash

---
+++ test/nonNumericOnlyHash.unittest.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const nonNumericOnlyHash = require("../lib/util/nonNumericOnlyHash");
+
+it("hashLength=0", () => {
+	expect(nonNumericOnlyHash("111", 0)).toBe("");
+});
+
+it("abc", () => {
+	expect(nonNumericOnlyHash("abc", 10)).toBe("abc");
+});
+
+it("abc1", () => {
+	expect(nonNumericOnlyHash("abc1", 3)).toBe("abc");
+});
+
+it("ab11", () => {
+	expect(nonNumericOnlyHash("ab11", 3)).toBe("ab1");
+});
+
+it("0111", () => {
+	expect(nonNumericOnlyHash("0111", 3)).toBe("a11");
+});
+
+it("911a", () => {
+	expect(nonNumericOnlyHash("911a", 3)).toBe("j11");
+});
