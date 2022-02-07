03144929c748f9f9c26a9a109275dd94f3f604ce
成仕伟 <cheng_deeplove@163.com>
2022-02-08 00:00:34 +0800

    add config test case

---
+++ test/configCases/chunk-graph/issue-15173/commonAsync/index.js
@@ -0,0 +1,5 @@
+import { commonUtil } from "../commonSync";
+
+export function getCommonAsync() {
+	return commonUtil();
+}
---
+++ test/configCases/chunk-graph/issue-15173/test.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	findBundle: function () {
+		return ["entryA.js"];
+	}
+};
