cdfb610bfadb55d591a5c8ed62b58f06f7f85323
Ivan Kopeykin <ivan.kopeykin@gmail.com>
2022-02-08 15:19:21 +0300

    fix restoring from cache

--- test/statsCases/context-independence/b/index.js
+++ test/statsCases/context-independence/b/index.js
@@ -1,4 +1,4 @@
 console.log("test");
 import("./chunk");
 const module = Math.round(Math.random() * 100) % 2 === 0 ? "a" : "b";
-import(`c/${module}`).then(({ default: d }) => console.log(d));
+import(/* webpackMode: "eager" */`c/${module}`).then(({ default: d }) => console.log(d));
