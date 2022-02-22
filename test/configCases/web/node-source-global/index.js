82a0f2add68552e47f5bb3bc053e871faea265f6
Ivan Kopeykin <ivan.kopeykin@gmail.com>
2022-02-22 13:37:58 +0300

    fix renaming

--- test/configCases/web/node-source-global/index.js
+++ test/configCases/web/node-source-global/index.js
@@ -4,5 +4,6 @@ it("should disallow rename global", () => {
 	var method = shouldBeGlobal[String.fromCharCode(40, 40, 40)];
 	method && method();
 	eval("expect(shouldBeGlobal.value1).toBe('value1')");
+	expect(shouldBeGlobal.test).toBe("test");
 	expect(global.test).toBe("test");
 });
