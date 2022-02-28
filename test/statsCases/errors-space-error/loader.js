8667b683bb0f94705d950e45cd43a2020ebc6579
Ivan Kopeykin <ivan.kopeykin@gmail.com>
2022-02-28 16:23:47 +0300

    support filteredErrorDetailsCount/filteredWarningDetailsCount

--- test/statsCases/errors-space-error/loader.js
+++ test/statsCases/errors-space-error/loader.js
@@ -1,6 +1,11 @@
 "use strict";
 
 module.exports = function (content) {
-	this.emitError(new Error("loader error"));
+	let error = new Error("loader error1");
+	error.stack = "stack1\nstack2\nstack3";
+	this.emitError(error);
+	error = new Error("loader error2");
+	error.stack = "stack1\nstack2";
+	this.emitError(error);
 	return content;
 }
--- test/statsCases/errors-space-error/webpack.config.js
+++ test/statsCases/errors-space-error/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = [
 		entry: "./loader!./index.js",
 		mode: "production",
 		stats: {
-			errorsSpace: 2,
+			errorsSpace: 0,
 			errors: true
 		}
 	},
@@ -12,7 +12,31 @@ module.exports = [
 		entry: "./loader!./index.js",
 		mode: "production",
 		stats: {
-			errorsSpace: 0,
+			errorsSpace: 2, // 2 errors (2 errors without details)
+			errors: true
+		}
+	},
+	{
+		entry: "./loader!./index.js",
+		mode: "production",
+		stats: {
+			errorsSpace: 3, // 2 errors (2 errors without details)
+			errors: true
+		}
+	},
+	{
+		entry: "./loader!./index.js",
+		mode: "production",
+		stats: {
+			errorsSpace: 4, // 2 errors + 2 lines (2 errors, one with partial details)
+			errors: true
+		}
+	},
+	{
+		entry: "./loader!./index.js",
+		mode: "production",
+		stats: {
+			errorsSpace: 5, // 2 errors + 3 lines (2 errors, one full details)
 			errors: true
 		}
 	},
