8f56fcba728f06842bf38fcfca1e04039e079271
Tobias Speicher <rootcommander@gmail.com>
2022-03-13 22:54:18 +0100

    Replace deprecated String.prototype.substr()
    String.prototype.substr() is deprecated (see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substr) so we replace it with slice() which works similarily but isn't deprecated.
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- test/cases/compile/long-module-chain/module.js
+++ test/cases/compile/long-module-chain/module.js
@@ -1,5 +1,5 @@
 if(__resourceQuery === "?0") {
 	module.exports = "module";
 } else {
-	module.exports = require("./module?" + (+__resourceQuery.substr(1) - 1));
+	module.exports = require("./module?" + (+__resourceQuery.slice(1) - 1));
 }
