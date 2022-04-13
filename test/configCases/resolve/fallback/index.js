68f22ac3aceccd47fa5c65db59a3e44a63d71ba8
Ivan Kopeykin <ivan.kopeykin@gmail.com>
2022-04-13 12:44:39 +0300

    up enhanced-resolve

--- test/configCases/resolve/fallback/index.js
+++ test/configCases/resolve/fallback/index.js
@@ -7,3 +7,8 @@ it("can fallback if the module does not exist", () => {
 	const one = require("./b/1");
 	expect(one).toBe(1);
 });
+
+it("# alias should work", () => {
+	const one = require("#/a");
+	expect(one).toBe(1);
+});
