e91b530fff139b2e675870b402e5be8c88fd7c43
Ivan Kopeykin <ivan.kopeykin@gmail.com>
2022-04-20 16:40:23 +0300

    read map.sources

--- test/configCases/source-map/extract-source-map2/index.js
+++ test/configCases/source-map/extract-source-map2/index.js
@@ -5,6 +5,7 @@ require("./a");
 
 it("should extract source map", () => {
 	const fileData = fs.readFileSync(path.resolve(__dirname, "bundle0.js.map")).toString("utf-8");
-	const { sources } = JSON.parse(fileData);
+	const { sources, sourcesContent } = JSON.parse(fileData);
 	expect(sources).toContain("webpack:///external-source-map.txt");
+	expect(sourcesContent.map(s => s.trim())).toContain("source");
 });
