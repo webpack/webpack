37cba1da968d5f108cb7812deffec724ef375241
Nitin Kumar <nitin.kumar@razorpay.com>
2022-09-28 08:22:16 +0530

    fix: lint issues

--- bin/webpack.js
+++ bin/webpack.js
@@ -54,13 +54,15 @@ const isInstalled = packageName => {
 	} while (dir !== (dir = path.dirname(dir)));
 
 	// https://github.com/nodejs/node/blob/v18.9.1/lib/internal/modules/cjs/loader.js#L1274
-    for (const internalPath of require("module").globalPaths) {
+	// eslint-disable-next-line no-warning-comments
+	// @ts-ignore
+	for (const internalPath of require("module").globalPaths) {
 		try {
-		  if (fs.statSync(path.join(internalPath, packageName)).isDirectory()) {
-			return true;
-		  }
+			if (fs.statSync(path.join(internalPath, packageName)).isDirectory()) {
+				return true;
+			}
 		} catch (_error) {
-		  // Nothing
+			// Nothing
 		}
 	}
 
