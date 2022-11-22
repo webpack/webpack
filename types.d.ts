e7e2aecd1c3e5fc893664f85a1da3bf1083533bc
Jack Works <jackworks@protonmail.com>
2022-11-22 11:57:02 +0800

    update dts

--- types.d.ts
+++ types.d.ts
@@ -5476,7 +5476,9 @@ declare class JavascriptParser extends Parser {
 	evaluatedVariable(tagInfo?: any): VariableInfo;
 	parseCommentOptions(
 		range?: any
-	): { options: null; errors: null } | { options: object; errors: unknown[] };
+	):
+		| { options: null; errors: null }
+		| { options: object; errors: { comment: any; cause: unknown }[] };
 	extractMemberExpressionChain(expression: MemberExpression): {
 		members: string[];
 		object:
