3902ac5ffc1c40f7806fa108635b368315f9e8e8
xiaoxiaojx <784487301@qq.com>
2023-02-07 17:01:44 +0800

    fix: fix pureDep returns null in some js files

--- types.d.ts
+++ types.d.ts
@@ -1241,9 +1241,14 @@ declare interface CodeGenerationContext {
 	chunkGraph: ChunkGraph;
 
 	/**
-	 * the runtimes code should be generated for
+	 * the runtime code should be generated for
 	 */
 	runtime: RuntimeSpec;
+
+	/**
+	 * the runtimes code should be generated for
+	 */
+	runtimes?: RuntimeSpec[];
 
 	/**
 	 * when in concatenated module, information about other concatenated modules
@@ -2765,9 +2770,14 @@ declare interface DependencyTemplateContext {
 	module: Module;
 
 	/**
-	 * current runtimes, for which code is generated
+	 * current runtime, for which code is generated
 	 */
 	runtime: RuntimeSpec;
+
+	/**
+	 * current runtimes, for which code is generated
+	 */
+	runtimes?: RuntimeSpec[];
 
 	/**
 	 * mutable array of init fragments for the current module
@@ -4370,6 +4380,11 @@ declare interface GenerateContext {
 	 * the runtime
 	 */
 	runtime: RuntimeSpec;
+
+	/**
+	 * the runtimes
+	 */
+	runtimes?: RuntimeSpec[];
 
 	/**
 	 * when in concatenated module, information about other concatenated modules
@@ -13005,6 +13020,10 @@ declare namespace exports {
 			export let runtimeEqual: (a: RuntimeSpec, b: RuntimeSpec) => boolean;
 			export let compareRuntime: (a: RuntimeSpec, b: RuntimeSpec) => 0 | 1 | -1;
 			export let mergeRuntime: (a: RuntimeSpec, b: RuntimeSpec) => RuntimeSpec;
+			export let deepMergeRuntime: (
+				runtimes: RuntimeSpec[],
+				runtime: RuntimeSpec
+			) => RuntimeSpec;
 			export let mergeRuntimeCondition: (
 				a: RuntimeCondition,
 				b: RuntimeCondition,
