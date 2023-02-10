ea07777ef6fedab68779e27cc6b940067de784fc
Thomas Tay <Tay.Thomas@microsoft.com>
2023-02-09 18:20:44 -0800

    stats test is flaky, revert to just one

--- test/statsCases/worker-public-path/webpack.config.js
+++ test/statsCases/worker-public-path/webpack.config.js
@@ -1,29 +1,8 @@
-const baseConfig = {
+/** @type {import("../../../types").Configuration} */
+module.exports = {
 	mode: "production",
-	cache: false,
 	entry: "./index.js",
 	output: {
 		filename: "[name]-[contenthash].js"
-	},
-	stats: {
-		chunks: true
 	}
 };
-
-/** @type {import("../../../types").Configuration} */
-module.exports = [
-	{
-		...baseConfig,
-		name: "With worker public path",
-		...{
-			output: {
-				filename: "[name]-[contenthash].js",
-				workerPublicPath: "/workerPublicPath2/"
-			}
-		}
-	},
-	{
-		...baseConfig,
-		name: "No worker public path"
-	}
-];
