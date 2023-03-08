07283fabc43a440db046037f7231ee362f31a21c
David Michon <dmichon-msft@users.noreply.github.com>
2023-03-07 16:15:36 -0800

    Respect output.hashSalt in RealContentHashPlugin
    Fix #16788
    
    Update RealContentHashPlugin to initialize hash instances with
    the value of `output.hashSalt`, if provided.

---
+++ test/configCases/contenthash/salt/test.config.js
@@ -0,0 +1,24 @@
+const findOutputFiles = require("../../../helpers/findOutputFiles");
+
+const allAssets = new Set();
+const allBundles = new Set();
+
+module.exports = {
+	findBundle: function(i, options) {
+		const bundle = findOutputFiles(options, new RegExp(`^bundle${i}`))[0];
+		allBundles.add(/\.([^.]+)\./.exec(bundle)[1]);
+
+		const assets = findOutputFiles(options, /^img/);
+		for (const asset of assets) {
+			allAssets.add(asset);
+		}
+
+		return `./${bundle}`;
+	},
+	afterExecute: () => {
+		// Since there are exactly 2 unique values of output.hashSalt,
+		// there should be exactly 2 unique output hashes for each file.
+		expect(allBundles.size).toBe(2);
+		expect(allAssets.size).toBe(2);
+	}
+};
