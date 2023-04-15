80b88c5618df283014fecd4310028f1ad22e369e
Ivan Kopeykin <ivan.kopeykin@gmail.com>
2023-04-15 21:06:58 +0300

    fix prettier

--- test/cases/esm/import-meta/warnings.js
+++ test/cases/esm/import-meta/warnings.js
@@ -1,3 +1,5 @@
 module.exports = [
-	[/Accessing import.meta directly is unsupported \(only property access or destructuring is supported\)/]
+	[
+		/Accessing import.meta directly is unsupported \(only property access or destructuring is supported\)/
+	]
 ];
