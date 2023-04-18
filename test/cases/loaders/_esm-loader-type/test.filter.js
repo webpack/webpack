98b976e8bfdce04de75c4c2a2690d3e9cb4486a1
alexander.akait <sheo13666q@gmail.com>
2023-04-18 14:58:18 +0300

    chore: fix lint

--- test/cases/loaders/_esm-loader-type/test.filter.js
+++ test/cases/loaders/_esm-loader-type/test.filter.js
@@ -1,4 +1,4 @@
-module.exports = function(config) {
+module.exports = function (config) {
 	// TODO need fix in v8 https://github.com/nodejs/node/issues/35889
 	// TODO otherwise this test case cause segment fault
 	return false;
