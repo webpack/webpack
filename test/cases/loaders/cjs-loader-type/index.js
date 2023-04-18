65d17428daf2d6921e0344d2ea70a578143fd4c4
alexander.akait <sheo13666q@gmail.com>
2023-04-18 15:02:05 +0300

    fix: bug

--- test/cases/loaders/cjs-loader-type/index.js
+++ test/cases/loaders/cjs-loader-type/index.js
@@ -1,11 +1,11 @@
 it("should pass package.json type to loader", function () {
-	// expect(require("cjs/loader.js!")).toBe("commonjs");
-	// expect(require("./loader.js!")).toBe("undefined");
+	expect(require("cjs/loader.js!")).toBe("commonjs");
+	expect(require("./loader.js!")).toBe("undefined");
 });
 
 it("should pass 'commonjs' type to loader for .cjs", function () {
-	// expect(require("cjs/loader.cjs!")).toBe("commonjs");
-	expect(require("./loader.cjs!")).toBe("undefined");
+	expect(require("cjs/loader.cjs!")).toBe("commonjs");
+	expect(require("./loader.cjs!")).toBe("commonjs");
 	// TODO need fix in v8 https://github.com/nodejs/node/issues/35889
 	// TODO otherwise this test case cause segment fault
 	// expect(require("esm/loader.cjs!")).toBe("commonjs");
