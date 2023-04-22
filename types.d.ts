94b594511ef7d33d17ee490c19e471194eea59f5
alexander.akait <sheo13666q@gmail.com>
2023-04-22 05:42:04 +0300

    fix: types

--- types.d.ts
+++ types.d.ts
@@ -6204,9 +6204,9 @@ declare class LazySet<T> {
 	has(item: T): boolean;
 	keys(): IterableIterator<T>;
 	values(): IterableIterator<T>;
-	serialize(__0: {}): void;
+	serialize(__0: ObjectSerializerContext): void;
 	[Symbol.iterator](): IterableIterator<T>;
-	static deserialize(__0: {}): LazySet<any>;
+	static deserialize<T>(__0: ObjectDeserializerContext): LazySet<T>;
 }
 declare interface LibIdentOptions {
 	/**
