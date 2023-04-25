23ac879ca01a258adeeb4bfd4bdb7bd98eadcdac
alexander.akait <sheo13666q@gmail.com>
2023-04-25 03:38:01 +0300

    test: add test cases

---
+++ test/configCases/css/css-import/index.js
@@ -0,0 +1,7 @@
+import * as style from "./style.css";
+
+it("should compile and load style on demand", done => {
+	const style = getComputedStyle(document.body);
+	expect(style.getPropertyValue("background")).toBe(" red");
+	done();
+});
