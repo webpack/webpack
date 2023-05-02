dd89b19c1a49d47d72c67c030b8e77595481cb12
alexander.akait <sheo13666q@gmail.com>
2023-05-02 13:12:41 +0300

    chore: fix lint

--- test/configCases/css/css-import-at-middle/index.js
+++ test/configCases/css/css-import-at-middle/index.js
@@ -2,7 +2,6 @@ import "./style.css";
 
 it("should compile with warnings", done => {
 	const style = getComputedStyle(document.body);
-	console.log(style)
 	expect(style.getPropertyValue("background")).toBe(" blue");
 	expect(style.getPropertyValue("color")).toBe(" green");
 
