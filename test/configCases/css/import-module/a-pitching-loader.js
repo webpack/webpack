dea8300beb9d8d0be5f21b5ea76d9740a5f1cb2e
alexander.akait <sheo13666q@gmail.com>
2023-05-06 00:22:29 +0300

    test: fix

---
+++ test/configCases/css/import-module/a-pitching-loader.js
@@ -0,0 +1,8 @@
+exports.pitch = async function (remaining) {
+	const result = await this.importModule(
+    this.resourcePath + '.webpack[javascript/auto]' + '!=!' + remaining, {
+    publicPath: ''
+    });
+
+	return result.default || result;
+};
