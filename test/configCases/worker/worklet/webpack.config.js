20c690025d2104a141510674525e023e00db3517
alexander.akait <sheo13666q@gmail.com>
2023-05-17 19:59:51 +0300

    feat: introduce a new syntax for worklets - `*context.audioWorklet.addModule()`

---
+++ test/configCases/worker/worklet/webpack.config.js
@@ -0,0 +1,26 @@
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	output: {
+		assetModuleFilename: "asset-[name][ext]",
+		filename: "[name].js"
+	},
+	target: "web",
+	module: {
+		rules: [
+			{
+				test: /\.[cm]?js$/,
+				parser: {
+					worker: [
+						"CSS.paintWorklet.addModule()",
+						"CSS.layoutWorklet.addModule()",
+						"CSS.animationWorklet.addModule()",
+						"*context.audioWorklet.addModule()",
+						"*audioWorklet.addModule()",
+						// *addModule() is not valid syntax
+						"..."
+					]
+				}
+			}
+		]
+	}
+};
