e063ddfbe73c31063b6c23bde0c2e2e42de84d49
alexander.akait <sheo13666q@gmail.com>
2023-05-19 17:37:38 +0300

    fix: more stricky handle

--- test/configCases/css/css-import/warnings.js
+++ test/configCases/css/css-import/warnings.js
@@ -1,5 +1,6 @@
 module.exports = [
 	/Expected URL for @import/,
 	/Expected URL for @import/,
-	/Expected URL for @import/
+	/Expected URL for @import/,
+	/The layer\(\.\.\.\) in @import at .+ should be before @supports/
 ];
