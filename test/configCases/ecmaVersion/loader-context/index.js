dd94f64a0b0271d580c027352f2252fd05945ed0
alexander.akait <sheo13666q@gmail.com>
2023-05-29 21:47:51 +0300

    test: added

---
+++ test/configCases/ecmaVersion/loader-context/index.js
@@ -0,0 +1,8 @@
+import mod from "./loader.js!./module";
+
+it("should compile and export target and environment", function() {
+	expect(mod.target).toBe("node");
+	expect(mod.environment.globalThis).toBe(false);
+	expect(mod.environment.optionalChaining).toBe(true);
+	expect(mod.environment.templateLiteral).toBe(true);
+});
---
+++ test/configCases/ecmaVersion/loader-context/loader.js
@@ -0,0 +1,6 @@
+module.exports = function loader(content) {
+	const target = this.target;
+	const environment = this.environment;
+
+	return `export default ${JSON.stringify({ target, environment})}`;
+}
