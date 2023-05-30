0ea2e99742afe95411c2bff31b6e4d63886b7570
alexander.akait <sheo13666q@gmail.com>
2023-05-30 20:09:38 +0300

    refactor: store some bigints as numbers

--- test/Compiler-filesystem-caching.test.js
+++ test/Compiler-filesystem-caching.test.js
@@ -79,15 +79,18 @@ describe("Compiler (filesystem caching)", () => {
 									}
 
 									if (isBigIntSupported) {
-										expect(result.bigint).toEqual(BigInt(123));
-										expect(result.bigint1).toEqual(12345678901234567890n);
-										expect(result.bigint2).toEqual(5n);
-										expect(result.bigint3).toEqual(1000000n);
+										expect(result.bigint).toEqual(5n);
+										expect(result.bigint1).toEqual(124n);
+										expect(result.bigint2).toEqual(125n);
+										expect(result.bigint3).toEqual(12345678901234567890n);
+										expect(result.bigint4).toEqual(5n);
+										expect(result.bigint5).toEqual(1000000n);
 										expect(result.obj.foo).toBe(BigInt(-10));
 										expect(Array.from(result.set)).toEqual([
 											BigInt(1),
 											BigInt(2)
 										]);
+										expect(result.arr).toEqual([256n, 257n, 258n]);
 									}
 
 									return;
@@ -110,12 +113,15 @@ describe("Compiler (filesystem caching)", () => {
 								}
 
 								if (isBigIntSupported) {
-									storeValue.bigint = BigInt(123);
-									storeValue.bigint1 = 12345678901234567890n;
-									storeValue.bigint2 = 5n;
-									storeValue.bigint3 = 1000000n;
+									storeValue.bigint = BigInt(5);
+									storeValue.bigint1 = BigInt(124);
+									storeValue.bigint2 = BigInt(125);
+									storeValue.bigint3 = 12345678901234567890n;
+									storeValue.bigint4 = 5n;
+									storeValue.bigint5 = 1000000n;
 									storeValue.obj = { foo: BigInt(-10) };
 									storeValue.set = new Set([BigInt(1), BigInt(2)]);
+									storeValue.arr = [256n, 257n, 258n];
 								}
 
 								await cacheItem.storePromise(storeValue);
