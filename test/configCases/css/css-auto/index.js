4d41b508d512836b0e1d0321f49049216e348c38
ahabhgk <ahabhgk@gmail.com>
2023-06-10 23:13:18 +0800

    fix test

--- test/configCases/css/css-auto/index.js
+++ test/configCases/css/css-auto/index.js
@@ -2,10 +2,9 @@ import "./global.less";
 import * as style1 from "./style1.module.less";
 import * as style2 from "./style2.modules.less";
 
-it("should correct compile css/auto", done => {
+it("should correctly compile css/auto", () => {
 	const style = getComputedStyle(document.body);
 	expect(style.getPropertyValue("color")).toBe(" green");
-	console.log(style1, style2)
 	expect(style1.class).toBe("./style1.module.less-class");
 	expect(style2.class).toBe("./style2.modules.less-class");
 });
