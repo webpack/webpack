39b883ec4fafb9f190514fc68d3f7cbcc0cc5066
alexander.akait <sheo13666q@gmail.com>
2023-06-16 00:39:16 +0300

    test: fixed

--- test/configCases/layer/context-and-css/dark.js
+++ test/configCases/layer/context-and-css/dark.js
@@ -1,8 +1,9 @@
-it("should contain only black", function() {
-	require.context('./test1', true, /\.less$/);
-	require('./test2/shared.less');
-	/*const style = getComputedStyle(document.body);
+require.context('./test1', true, /\.less$/);
+require('./test2/shared.less');
 
-	expect(style.color).toBe(" black");
-	expect(style.background).toBe(" black");*/
+it("should contain only black", function() {
+	const style = getComputedStyle(document.body);
+
+	expect(style["color-dark"]).toBe(" black");
+	expect(style["background-dark"]).toBe(" black");
 });
--- test/configCases/layer/context-and-css/test.config.js
+++ test/configCases/layer/context-and-css/test.config.js
@@ -4,8 +4,12 @@ module.exports = {
 		light.rel = "stylesheet";
 		light.href = "light.css";
 		scope.window.document.head.appendChild(light);
+		const dark = scope.window.document.createElement("link");
+		dark.rel = "stylesheet";
+		dark.href = "dark.css";
+		scope.window.document.head.appendChild(dark);
 	},
 	findBundle: function () {
-		return ["./light.js"];
+		return ["./runtime.js", "./light.js", "./dark.js"];
 	}
 };
