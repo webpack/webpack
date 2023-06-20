90472f847c91f07930d1462d249855d91d7b01ea
alexander.akait <sheo13666q@gmail.com>
2023-06-20 23:03:02 +0300

    test: prefer relative

---
+++ test/configCases/css/prefer-relative/index.js
@@ -0,0 +1,12 @@
+import * as styles1 from "./style.css";
+import * as styles2 from "./style.modules.css";
+
+it("should prefer relative", () => {
+	expect(styles1).toEqual(nsObj({}));
+	expect(styles2).toEqual(nsObj({}));
+
+	const style = getComputedStyle(document.body);
+
+	expect(style.getPropertyValue("background")).toBe(" red");
+	expect(style.getPropertyValue("color")).toBe(" red");
+});
