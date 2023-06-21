831094e87440323118e2d43469bbee37ebdad9fe
alexander.akait <sheo13666q@gmail.com>
2023-06-21 12:56:40 +0300

    test: fix

--- test/configCases/css/runtime-data-webpack/test.config.js
+++ test/configCases/css/runtime-data-webpack/test.config.js
@@ -3,6 +3,7 @@ module.exports = {
 		const link = scope.window.document.createElement("link");
 		link.rel = "stylesheet";
 		link.href = "bundle0.css";
+		link.setAttribute("data-webpack", "test:chunk-main");
 		scope.window.document.head.appendChild(link);
 	}
 };
