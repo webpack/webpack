19028dcadc81071f182b288a150254dc377dd830
ahabhgk <ahabhgk@gmail.com>
2023-07-06 20:33:30 +0800

    fix: non amd externals

--- test/configCases/target/amd-unnamed/index.js
+++ test/configCases/target/amd-unnamed/index.js
@@ -4,5 +4,5 @@ it("should name define", function() {
 	var fs = require("fs");
 	var source = fs.readFileSync(__filename, "utf-8");
 
-	expect(source).toMatch(/define\(\[[^\]]*\], (function)?\(/);
+	expect(source).toMatch(/define\((function)?\(/);
 });
