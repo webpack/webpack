a160c3937922eafdcc6b847e20b374558664436e
Chunan Ye <yechunan@umich.edu>
2023-08-08 23:07:57 -0700

    Evaluate Symbol.iterator and Symbol.asyncIterator

--- test/configCases/optimization/issue-17565/index.js
+++ test/configCases/optimization/issue-17565/index.js
@@ -1,8 +1,10 @@
-var myModule = require("./module.esm");
+var moduleA = require("./moduleA");
+var moduleB = require("./moduleB");
 
 it("should not throw when instantiating the class", (done) => {
     try {
-        const object = new myModule.default.MyClass(1);
+        const objectA = new moduleA.default.MyClass(1);
+        const objectB = new moduleB.default.MyClass(1);
         done();
     } catch (error) {
         done(error);
