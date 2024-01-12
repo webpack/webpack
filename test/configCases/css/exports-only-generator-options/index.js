0107036c503e8a1bb1d2713d2f8616bed191dee9
ahabhgk <ahabhgk@gmail.com>
2024-01-12 18:04:11 +0800

    update test

--- test/configCases/css/exports-only-generator-options/index.js
+++ test/configCases/css/exports-only-generator-options/index.js
@@ -1,83 +1,26 @@
-import * as style from "../exports/style.module.css?ns";
-import { a, abc } from "../exports/style.module.css?picked";
-import def from "../exports/style.module.css?default";
-
-it("should allow to import a css module", () => {
-	expect(style).toEqual(
-		nsObj({
+it("should not have .css file", (done) => {
+	__non_webpack_require__("./exports_style_module_css.bundle0.js");
+	__non_webpack_require__("./exports_style_module_css_exportsOnly.bundle0.js");
+	Promise.all([
+		import("../exports/style.module.css"),
+		import("../exports/style.module.css?module"),
+		import("../exports/style.module.css?exportsOnly"),
+	]).then(([style1, style2, style3]) => {
+		const ns = nsObj({
 			a: "a",
 			abc: "a b c",
 			comments: "abc      def",
 			"white space": "abc\n\tdef",
 			default: "default"
-		})
-	);
-	expect(a).toBe("a");
-	expect(abc).toBe("a b c");
-	expect(def).toBe("default");
+		});
+		expect(style1).toEqual(ns);
+		expect(style2).toEqual(ns);
+		expect(style3).toEqual(ns);
+	}).then(() => {
+		const fs = __non_webpack_require__("fs");
+		const path = __non_webpack_require__("path");
+		expect(fs.existsSync(path.resolve(__dirname, "exports_style_module_css.bundle0.css"))).toBe(false);
+		expect(fs.existsSync(path.resolve(__dirname, "exports_style_module_css_exportsOnly.bundle0.css"))).toBe(false);
+		done()
+	}).catch(e => done(e))
 });
-
-it("should allow to dynamic import a css module", done => {
-	__non_webpack_require__("./exports_style_module_css.bundle0.js");
-	import("../exports/style.module.css").then(x => {
-		try {
-			expect(x).toEqual(
-				nsObj({
-					a: "a",
-					abc: "a b c",
-					comments: "abc      def",
-					"white space": "abc\n\tdef",
-					default: "default"
-				})
-			);
-		} catch (e) {
-			return done(e);
-		}
-		done();
-	}, done);
-});
-
-it("should allow to reexport a css module", done => {
-	__non_webpack_require__("./exports_reexported_js.bundle0.js");
-	import("../exports/reexported").then(x => {
-		try {
-			expect(x).toEqual(
-				nsObj({
-					a: "a",
-					abc: "a b c",
-					comments: "abc      def",
-					"white space": "abc\n\tdef"
-				})
-			);
-		} catch (e) {
-			return done(e);
-		}
-		done();
-	}, done);
-});
-
-it("should allow to import a css module", done => {
-	__non_webpack_require__("./exports_imported_js.bundle0.js");
-	import("../exports/imported").then(({ default: x }) => {
-		try {
-			expect(x).toEqual(
-				nsObj({
-					a: "a",
-					abc: "a b c",
-					comments: "abc      def",
-					"white space": "abc\n\tdef",
-					default: "default"
-				})
-			);
-		} catch (e) {
-			return done(e);
-		}
-		done();
-	}, done);
-});
-
-it("should not have .css file", async () => {
-	const fs = __non_webpack_require__("fs/promises");
-	const files = await fs.readdir(__dirname);
-	expect(files.every(file => !file.endsWith(".css"))).toBe(true)
-})
