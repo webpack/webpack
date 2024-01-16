b19c422925940998578a7a94da7b711f760eb542
ahabhgk <ahabhgk@gmail.com>
2024-01-16 10:25:30 +0800

    fix: add warning for async external

--- test/configCases/async-module/environment-not-support-async-warning/index.js
+++ test/configCases/async-module/environment-not-support-async-warning/index.js
@@ -1,6 +1,9 @@
-it("should allow to use top-level-await", () => {
-	return import("./reexport").then(({ number, getNumber }) => {
+it("should have warnings for environment not support async/await when using asyncModule", () => {
+	return import("./reexport").then(({ number, getNumber, importRequest, moduleRequest, promiseRequest }) => {
 		expect(number).toBe(1);
 		expect(getNumber()).toBe(42);
+		expect(importRequest).toBe("import.js");
+		expect(moduleRequest).toBe("module.js");
+		expect(promiseRequest).toBe("promise.js");
 	});
 });
