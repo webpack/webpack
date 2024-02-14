f06427a1a551de27b5b8191940917c606a29556f
alexander.akait <sheo13666q@gmail.com>
2024-02-14 16:35:12 +0300

    fix: make dynamic import optional in try/catch

---
+++ test/cases/runtime/missing-module-exception-dynamic-import/warnings.js
@@ -0,0 +1,5 @@
+module.exports = [
+	[/Module not found/, /Can't resolve '\.\/fail-1' /],
+	[/Module not found/, /Can't resolve '\.\/fail-2' /],
+	[/Module not found/, /Can't resolve '\.\/fail-3' /]
+];
