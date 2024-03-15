2ac29782b30e7a84190cbdad489ede318e75043c
ahabhgk <ahabhgk@gmail.com>
2024-03-15 21:01:12 +0800

    rebase and test

--- test/configCases/css/urls-css-filename/index.js
+++ test/configCases/css/urls-css-filename/index.js
@@ -1,18 +1,22 @@
-const testCase = (tagName, impFn) => {
-	it(`should be able to handle styles in ${tagName}.css`, done => {
-		const element = document.createElement(tagName);
-		document.body.appendChild(element);
-		impFn().then(x => {
-			try {
-				expect(x).toEqual(nsObj({}));
-				const style = getComputedStyle(element);
-				expect(style).toMatchSnapshot();
-				done();
-			} catch (e) {
-				done(e);
-			}
-		}, done);
-	});
-};
-
-testCase("div", () => import("./spacing.css"));
+it(`should generate correct url public path with css filename`, done => {
+	const h1 = document.createElement('h1');
+	document.body.appendChild(h1);
+	const h2 = document.createElement('h2');
+	document.body.appendChild(h1);
+	const h3 = document.createElement('h3');
+	document.body.appendChild(h1);
+	import("./index.css").then(x => {
+		try {
+			expect(x).toEqual(nsObj({}));
+			const style1 = getComputedStyle(h1);
+			expect(style1).toMatchSnapshot();
+			const style2 = getComputedStyle(h2);
+			expect(style2).toMatchSnapshot();
+			const style3 = getComputedStyle(h3);
+			expect(style3).toMatchSnapshot();
+			done();
+		} catch (e) {
+			done(e);
+		}
+	}, done);
+});
