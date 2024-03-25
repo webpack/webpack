2feccc099dbdaf2f25cc01d4473fabc6c5493018
David Michon <dmichon-msft@users.noreply.github.com>
2024-03-25 15:01:41 -0700

    (fix) Correct behavior of __webpack_exports_info__.a.b.canMangle

---
+++ test/configCases/mangle/exports-info-can-mangle/c.js
@@ -0,0 +1,3 @@
+export * as c from "./b";
+export const cCanMangle = __webpack_exports_info__.c.canMangle;
+export const c_bbbCanMangle = __webpack_exports_info__.c.bbb.canMangle;
--- test/configCases/mangle/exports-info-can-mangle/index.js
+++ test/configCases/mangle/exports-info-can-mangle/index.js
@@ -1,5 +1,6 @@
 import { aaa, aaaCanMangle } from "./a";
 import * as b from "./b"
+import { c_bbbCanMangle, c, cCanMangle } from "./c";
 
 it("__webpack_exports_info__.xxx.canMangle should be correct", () => {
 	expect(aaa).toBe("aaa");
@@ -7,4 +8,8 @@ it("__webpack_exports_info__.xxx.canMangle should be correct", () => {
 	const { bbb, bbbCanMangle } = b;
 	expect(bbb).toBe("bbb");
 	expect(bbbCanMangle).toBe(false);
+
+	expect(cCanMangle).toBe(true);
+	expect(c.bbb).toBe("bbb");
+	expect(c_bbbCanMangle).toBe(bbbCanMangle);
 });
