8c6f46967dc47193b8bc96f7101c236f634e3436
alexander.akait <sheo13666q@gmail.com>
2024-03-28 15:45:53 +0300

    test: added

--- test/configCases/externals/import-attributes/index.js
+++ test/configCases/externals/import-attributes/index.js
@@ -1,9 +1,15 @@
 import * as staticPkg from "./static-package.json" assert { type: "json" };
 import * as staticPkgStr from "./static-package-str.json" assert { "type": "json" };
 
+import * as staticPkgWith from "./static-package-with.json" with { type: "json" };
+import * as staticPkgStrWith from "./static-package-str-with.json" with { "type": "json" };
+
 it("should allow async externals", async () => {
 	expect(staticPkg.default.foo).toBe("static");
 	expect(staticPkgStr.default.foo).toBe("static-str");
+
+	expect(staticPkgWith.default.foo).toBe("static");
+	expect(staticPkgStrWith.default.foo).toBe("static-str");
 
 	const dynamicPkg = await import("./dynamic-package.json", {
 		assert: { type: "json" }
@@ -11,11 +17,23 @@ it("should allow async externals", async () => {
 
 	expect(dynamicPkg.default.foo).toBe("dynamic");
 
-	const dynamicPkgStr = await import("./dynamic-package-str.json", {
+	const dynamicPkgWith = await import("./dynamic-package-with.json", {
+		with: { type: "json" }
+	})
+
+	expect(dynamicPkgWith.default.foo).toBe("dynamic");
+
+	const dynamicPkgStr = await import("./dynamic-package-str-with.json", {
 		"assert": { "type": "json" }
 	})
 
 	expect(dynamicPkgStr.default.foo).toBe("dynamic-str");
+
+	const dynamicPkgStrWith = await import("./dynamic-package-str.json", {
+		"assert": { "type": "json" }
+	})
+
+	expect(dynamicPkgStrWith.default.foo).toBe("dynamic-str");
 
 	const eagerPkg = await import(/* webpackMode: "eager" */ "./eager.json", {
 		assert: { type: "json" }
