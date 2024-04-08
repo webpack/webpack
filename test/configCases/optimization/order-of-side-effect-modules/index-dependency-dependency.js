be3cb13a1d3cff835961f4e4f3299ba70e4ad207
kosciolek <45483493+kosciolek@users.noreply.github.com>
2024-04-08 23:14:41 +0200

    fix: bad module order and missing modules when optimizing side effect free modules

---
+++ test/configCases/optimization/order-of-side-effect-modules/index-dependency-dependency.js
@@ -0,0 +1,3 @@
+import "./index-dependency-dependency.css";
+
+export const indexDependencyDependency = () => {};
---
+++ test/configCases/optimization/order-of-side-effect-modules/index-dependency.js
@@ -0,0 +1,9 @@
+// this would work
+// import { indexDependencyDependency as A } from "./index-dependency-dependency";
+// export const indexDependencyDependency = A
+
+export { indexDependencyDependency } from "./index-dependency-dependency";
+
+import "./index-dependency.css";
+
+export const indexDependency = () => {};
