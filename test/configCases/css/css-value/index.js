31bd4223c520298866ca2b18d55928279f6c3e83
faga <lzc295592@163.com>
2024-06-27 00:50:40 +0800

    test: fix test case

--- test/configCases/css/css-value/index.js
+++ test/configCases/css/css-value/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 it("should compile", done => {
 	const style = getComputedStyle(document.body);
-	expect(style.getPropertyValue("background")).toBe("#333");
-    expect(style.getPropertyValue("font-size")).toBe("72px");
+	expect(style.getPropertyValue("color")).toBe(" #333");
+    expect(style.getPropertyValue("font-size")).toBe(" 72px");
 	done();
 });
