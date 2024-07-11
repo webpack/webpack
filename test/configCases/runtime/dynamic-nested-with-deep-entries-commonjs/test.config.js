40b1a77183fff37c0cfb50ddb1718ad42a3472db
alexander.akait <sheo13666q@gmail.com>
2024-07-11 18:51:59 +0300

    test: added

---
+++ test/configCases/runtime/dynamic-nested-with-deep-entries-commonjs/test.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	findBundle: function () {
+		return [
+			"./dir5/dir6/runtime~one.js",
+			"./one.js",
+			"./dir5/dir6/runtime~two.js",
+			"./dir2/two.js",
+			"./dir5/dir6/runtime~three.js",
+			"./three.js",
+			"./dir5/dir6/runtime~four.js",
+			"./dir4/four.js",
+			"./dir5/dir6/runtime~five.js",
+			"./dir5/dir6/five.js",
+			"./dir5/dir6/runtime~six.js",
+			"./dir5/dir6/six.js"
+		];
+	}
+};
---
+++ test/configCases/runtime/dynamic-nested-with-deep-entries-esm/test.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	findBundle: function () {
+		return [
+			"./dir5/dir6/runtime~one.mjs",
+			"./one.mjs",
+			"./dir5/dir6/runtime~two.mjs",
+			"./dir2/two.mjs",
+			"./dir5/dir6/runtime~three.mjs",
+			"./three.mjs",
+			"./dir5/dir6/runtime~four.mjs",
+			"./dir4/four.mjs",
+			"./dir5/dir6/runtime~five.mjs",
+			"./dir5/dir6/five.mjs",
+			"./dir5/dir6/runtime~six.mjs",
+			"./dir5/dir6/six.mjs"
+		];
+	}
+};
