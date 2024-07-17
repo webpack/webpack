bea29991f04a0a58391fa6dec9e0e281b8ee236c
faga <lzc295592@163.com>
2024-07-18 02:00:40 +0800

    chore: update test cases

--- test/configCases/css/css-value/index.js
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-
-it("should compile", done => {
-	const style = getComputedStyle(document.body);
-	expect(style.getPropertyValue("color")).toBe(" #333");
-    expect(style.getPropertyValue("font-size")).toBe(" 72px");
-	done();
-});
