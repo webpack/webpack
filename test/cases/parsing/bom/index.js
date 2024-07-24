b233a4cc775a49cb3c7316d539c1f99f275e2777
alexander.akait <sheo13666q@gmail.com>
2024-07-24 16:35:33 +0300

    test: fix

--- test/cases/parsing/bom/index.js
+++ test/cases/parsing/bom/index.js
@@ -5,7 +5,7 @@ it("should load a utf-8 file with BOM", function () {
 
 it("should load a css file with BOM", function () {
 	var css = require("!css-loader!./bomfile.css").default + "";
-	expect(css).toBe("body{color:#abc}");
+	expect(css.replace(/\n\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1')).toBe("body{color:#abc}");
 });
 
 it("should load a json file with BOM", function () {
