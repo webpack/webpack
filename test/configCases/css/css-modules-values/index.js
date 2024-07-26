bff3519a5625a732c5cbf99a7c1eb378c6f47d32
faga <lzc295592@163.com>
2024-07-27 05:51:43 +0800

    test: add more test cases

--- test/configCases/css/css-modules-values/index.js
+++ test/configCases/css/css-modules-values/index.js
@@ -1,23 +1,27 @@
 import './style.css';
 
-it("should compile",  ()=> {
-	const style = getComputedStyle(document.body);
-	expect(style.getPropertyValue("background")).toBe(" blue");
-	expect(style.getPropertyValue("font-size")).toBe(" 72px");
-});
+it("should compile", () => {
+	const links = document.getElementsByTagName("link");
+	const css = [];
 
-it("should export correctly", done => {
-	import("./style.module.css").then((x) => {
+	for (const link of links.slice(1)) {
+		css.push(link.sheet.css);
+	}
+
+	expect(css).toMatchSnapshot();
+})
+
+it("should export constants ",  (done)=> {
+	import("./exports.module.css").then(x => {
 		try{
-			expect(x).toEqual(
-				nsObj({
-					emoji: "😄",
-					"char-1": "*&?!"
-				})
-			)
+			expect(x).toEqual(nsObj({
+				small: "(max-width: 599px)",
+				red: "blue",
+				aaa: "color(red lightness(50%))"
+			}))
 		} catch(e) {
-			done(e);
+			done(e)
 		}
 		done()
-	}, done)
-})
+	}, done);
+});
--- test/configCases/css/css-modules-values/warnings.js
+++ test/configCases/css/css-modules-values/warnings.js
@@ -1,1 +1,5 @@
-module.exports = [[/Invalid value definition: empty/]];
+module.exports = [
+	[/Invalid value definition: v-empty/],
+	[/Invalid value definition: v-empty-1/],
+	[/Invalid value definition: v-empty-2/]
+];
