406ec4f7a850edb8cc80c5a610675aacf8a62166
alexander.akait <sheo13666q@gmail.com>
2024-08-02 21:34:51 +0300

    refactor: simplify logic

--- test/configCases/externals/module-import/index.js
+++ test/configCases/externals/module-import/index.js
@@ -1,8 +1,24 @@
-it("should allow async externals", async () => {
-	const fs1 = await import("fs");
-	const fs2 = await import("node:fs");
-	const fs3 = await import("node-fs");
+import fs, { readFile } from "module-node-fs";
 
+it("should allow async externals", async () => {
+	const { default: fs1, readFile: readFile1 } = await import("fs");
+	const fs2 = (await import("node:fs")).default;
+	const fs3 = (await import("import-node-fs")).default;
+	const path = await import("import-node-path");
+	const data = fs.readFileSync(__STATS__.outputPath + "/bundle0.mjs", "utf-8");
+
+	expect(data.includes("import * as __WEBPACK_EXTERNAL_MODULE_fs__ from \"fs\";")).toBe(true);
+	expect(data.includes("import(\"node:fs\");")).toBe(true);
+
+	expect(readFile).toStrictEqual(readFile1);
+	expect(readFile1).toStrictEqual(fs2.readFile);
+	expect(readFile1).toStrictEqual(fs3.readFile);
+	expect(fs).toStrictEqual(fs2);
 	expect(fs1).toStrictEqual(fs2);
 	expect(fs1).toStrictEqual(fs3);
+	expect(fs.readFile).toStrictEqual(fs2.readFile);
+	expect(fs.readFile).toStrictEqual(fs3.readFile);
+	expect(fs1.readFile).toStrictEqual(fs2.readFile);
+	expect(fs1.readFile).toStrictEqual(fs3.readFile);
+	expect(typeof path.join).toBe("function")
 });
--- test/configCases/externals/module-import/webpack.config.js
+++ test/configCases/externals/module-import/webpack.config.js
@@ -2,15 +2,17 @@ module.exports = {
 	externals: {
 		fs: "module-import fs",
 		"node:fs": "module-import node:fs",
-		"node-fs": "module-import fs"
+		"import-node-fs": "module-import fs",
+		"import-node-path": "module-import path",
+		"module-node-fs": "module-import fs",
+		"module-node-path": "module-import path"
 	},
 	output: {
-		module: true,
 		library: {
 			type: "module"
 		}
 	},
-	target: ["es2020"],
+	target: ["node", "es2020"],
 	experiments: {
 		outputModule: true
 	},
