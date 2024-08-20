12fa71fccf5aa23186c6b71ce738572cf7b91453
faga <lzc295592@163.com>
2024-08-21 01:23:10 +0800

    test: test cases

--- test/configCases/css/icss-import/index.js
+++ test/configCases/css/icss-import/index.js
@@ -3,16 +3,18 @@ import './import.css';
 it("should compile", () => {
 	const style = getComputedStyle(document.body);
 	expect(style.getPropertyValue("background")).toBe(" red");
+	expect(style.getPropertyValue("color")).toBe(" red");
 });
 
 it("should re-export", (done) => {
     import("./reexport.module.css").then((module) => {
         try{
             expect(module).toEqual(nsObj({
-                ["primary-color"]: "red",
+                ['primary-color']: "red",
                 ['secondary-color']: "block"
             }));
-        }catch(e) {
+        } catch(e) {
+            done(e)
         }
        done() 
     }, done)
