032244b1a5f0fca34d3395dcfd070d53cc37492e
faga <lzc295592@163.com>
2024-09-06 02:41:57 +0800

    test: update test case

--- test/configCases/css/css-URL-import/index.js
+++ test/configCases/css/css-URL-import/index.js
@@ -1,9 +1,11 @@
 it("should compile", done => {
-    const url = new URL('./style.css', import.meta.url);
+	const url = new URL("./style.css", import.meta.url);
 	const link = document.createElement("link");
 	link.rel = "stylesheet";
-    link.href = url;
+	link.href = url.href;
 
 	expect(link.sheet.css).toMatchSnapshot();
+
+	expect(link.sheet._element === link).toMatchSnapshot();
 	done();
 });
