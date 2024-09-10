796a024ce0efa9fe87a9520d0ecb426c48438c86
faga <lzc295592@163.com>
2024-09-11 01:53:32 +0800

    chore: rendermanifest update

--- test/configCases/css/new-url/index.js
+++ test/configCases/css/new-url/index.js
@@ -13,13 +13,16 @@ it('new URL should not affect chunk css module', () => {
 })
 it("should compile", done => {
 	const url = new URL("./style.css", import.meta.url);
+	const url1 = new URL('./style.css', import.meta.url);
+	const url2 = new URL('./style.css?foo=1', import.meta.url);
 	
 	const link = document.createElement("link");
 	link.rel = "stylesheet";
 	link.href = url.href;
 
+	expect(url.href).toEqual(url1.href);
 	expect(link.sheet.css).toMatchSnapshot();
-
 	expect(url.href).toMatchSnapshot();
+	expect(url2.href).toMatchSnapshot();
 	done();
 });
