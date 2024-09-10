d8343edbac9efd1b4ba1361e0a55b73c9985eedc
alexander.akait <sheo13666q@gmail.com>
2024-09-10 23:10:31 +0300

    test: fix windows

--- test/cases/context/issue-18752/index.js
+++ test/cases/context/issue-18752/index.js
@@ -1,16 +1,16 @@
 it("should work with importing the same file twice and destructuring", async () => {
 	const type = "file";
 	const { generateSummary } = await import(
-		/* webpackInclude: /[/]folder[/](?!.*\.test).*\.m?js$/ */
-		/* webpackChunkName: "store-selectors" */
+		/* webpackInclude: /[/\\]folder[/\\](?!.*\.test).*\.m?js$/ */
+		/* webpackChunkName: "chunk-name" */
 		/* webpackMode: "lazy-once" */
 		`./folder/${type}.js`
 	);
 	expect(typeof generateSummary).toBe("function");
 
 	const { entityActionQueue } = await import(
-		/* webpackInclude: /[/]folder[/](?!.*\.test).*\.m?js$/ */
-		/* webpackChunkName: "store-selectors" */
+		/* webpackInclude: /[/\\]folder[/\\](?!.*\.test).*\.m?js$/ */
+		/* webpackChunkName: "chunk-name" */
 		/* webpackMode: "lazy-once" */
 		`./folder/${type}.js`
 	);
