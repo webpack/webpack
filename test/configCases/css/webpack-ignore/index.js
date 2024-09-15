d9373a98dbda25b0f3ec66cba9581940e2b3fd7d
faga <lzc295592@163.com>
2024-09-15 13:48:57 +0800

    test: update

--- test/configCases/css/webpack-ignore/index.js
+++ test/configCases/css/webpack-ignore/index.js
@@ -2,12 +2,7 @@ import "./style.css";
 
 it("should compile", done => {
 	const links = document.getElementsByTagName("link");
-	const css = [];
-
-	// Skip first because import it by default
-	for (const link of links.slice(1)) {
-		css.push(link.sheet.css);
-	}
+	const css = links[1].sheet.css;
 
 	expect(css).toMatchSnapshot();
 	done();
