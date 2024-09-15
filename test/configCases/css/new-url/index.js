95973f334ead3219adc3bb375b95ea6136ee814f
faga <lzc295592@163.com>
2024-09-15 14:05:36 +0800

    test: update

--- test/configCases/css/new-url/index.js
+++ test/configCases/css/new-url/index.js
@@ -2,13 +2,7 @@ import './style3.css'
 
 it('new URL should not affect chunk css module', () => {
 	const links = document.getElementsByTagName("link");
-	const css = [];
-
-	// Skip first because import it by default
-	for (const link of links.slice(1)) {
-		css.push(link.sheet.css);
-	}
-
+	const css = links[1].sheet.css;
 	expect(css).toMatchSnapshot();
 })
 it("should compile", done => {
