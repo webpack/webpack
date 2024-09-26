3316bf7e8e0917fa88e870b7c49b4fa23e6d3eef
alexander.akait <sheo13666q@gmail.com>
2024-09-26 17:13:02 +0300

    test: fix

--- test/configCases/race-conditions/import-module/index.js
+++ test/configCases/race-conditions/import-module/index.js
@@ -2,6 +2,6 @@ import * as styles from './style.module.css';
 import * as styles1 from './module.js';
 
 it("should not deadlock when using importModule", () => {
-	expect(styles).toMatchObject({ "someBottom": "8px" });
-	expect(styles1).toMatchObject({ "someBottom": "8px" });
+	expect(styles.someBottom).toBe("8px");
+	expect(styles1.someBottom).toBe("8px");
 });
