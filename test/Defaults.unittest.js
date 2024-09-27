b2e5dd6385ef13a20442b4580408191879b43be3
MarioCadenas <MarioCadenas@users.noreply.github.com>
2024-09-27 17:31:18 +0200

    chore: Use compilation.getPath

--- test/Defaults.unittest.js
+++ test/Defaults.unittest.js
@@ -2462,7 +2462,7 @@ describe("snapshots", () => {
 		"hotUpdateGlobal function",
 		{
 			output: {
-				hotUpdateGlobal: chunk => `__myUpdateGlobal_${chunk.id}`
+				hotUpdateGlobal: ({ chunk }) => `__myUpdateGlobal_${chunk.id}`
 			}
 		},
 		e => {
--- types.d.ts
+++ types.d.ts
@@ -10239,7 +10239,9 @@ declare interface Output {
 	/**
 	 * The global variable used by webpack for loading of hot update chunks.
 	 */
-	hotUpdateGlobal?: string | ((chunk: Chunk) => string);
+	hotUpdateGlobal?:
+		| string
+		| ((__0: { chunk: Chunk; contentHashType: string }) => string);
 
 	/**
 	 * The filename of the Hot Update Main File. It is inside the 'output.path' directory.
@@ -10538,7 +10540,9 @@ declare interface OutputNormalized {
 	/**
 	 * The global variable used by webpack for loading of hot update chunks.
 	 */
-	hotUpdateGlobal?: string | ((chunk: Chunk) => string);
+	hotUpdateGlobal?:
+		| string
+		| ((__0: { chunk: Chunk; contentHashType: string }) => string);
 
 	/**
 	 * The filename of the Hot Update Main File. It is inside the 'output.path' directory.
@@ -13695,11 +13699,6 @@ declare abstract class SortableSet<T> extends Set<T> {
 	 */
 	getFromUnorderedCache<R>(fn: (arg0: SortableSet<T>) => R): R;
 	toJSON(): T[];
-
-	/**
-	 * Iterates over values in the set.
-	 */
-	[Symbol.iterator](): IterableIterator<T>;
 }
 declare class Source {
 	constructor();
