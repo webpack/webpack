d46dee8f831d77aec48fea546dff0b0e7476932b
alexander.akait <sheo13666q@gmail.com>
2024-10-16 19:24:48 +0300

    feat(css): basic support for `url()`

--- test/configCases/css/webpack-ignore/warnings.js
+++ test/configCases/css/webpack-ignore/warnings.js
@@ -1,4 +1,8 @@
 module.exports = [
 	/Compilation error while processing magic comment\(-s\): \/\*\*\*\*webpackIgnore: false\*\*\*\/: Unexpected token '\*\*'/,
-	/Compilation error while processing magic comment\(-s\): \/\* {3}\* {3}\* {3}\* {3}webpackIgnore: {3}false {3}\* {3}\* {3}\*\/: Unexpected token '\*'/
+	/Compilation error while processing magic comment\(-s\): \/\* {3}\* {3}\* {3}\* {3}webpackIgnore: {3}false {3}\* {3}\* {3}\*\/: Unexpected token '\*'/,
+	/`webpackIgnore` expected a boolean, but received: 1\./,
+	/`webpackIgnore` expected a boolean, but received: 1\./,
+	/`webpackIgnore` expected a boolean, but received: 1\./,
+	/`webpackIgnore` expected a boolean, but received: 1\./
 ];
