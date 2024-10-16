9820e0b3d6449524cc0dbdd15c8967b2e5392755
alexander.akait <sheo13666q@gmail.com>
2024-10-16 19:27:22 +0300

    feat(css): basic support for `declaration`

--- test/configCases/css/webpack-ignore/warnings.js
+++ test/configCases/css/webpack-ignore/warnings.js
@@ -4,5 +4,9 @@ module.exports = [
 	/`webpackIgnore` expected a boolean, but received: 1\./,
 	/`webpackIgnore` expected a boolean, but received: 1\./,
 	/`webpackIgnore` expected a boolean, but received: 1\./,
-	/`webpackIgnore` expected a boolean, but received: 1\./
+	/`webpackIgnore` expected a boolean, but received: 1\./,
+	/Compilation error while processing magic comment\(-s\): \/\*\*\*webpackIgnore: {2}true\*\*\*\/: Unexpected token '\*\*'/,
+	/Compilation error while processing magic comment\(-s\): \/\*\*\*webpackIgnore: {2}true\*\*\*\/: Unexpected token '\*\*'/,
+	/Compilation error while processing magic comment\(-s\): \/\*\*\*webpackIgnore: {2}true\*\*\*\/: Unexpected token '\*\*'/,
+	/Compilation error while processing magic comment\(-s\): \/\*\*\*webpackIgnore: {2}true\*\*\*\/: Unexpected token '\*\*'/
 ];
