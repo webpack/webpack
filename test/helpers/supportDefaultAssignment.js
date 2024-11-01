866d559bfefc5888fd407d6175e8bf9827ade819
alexander.akait <sheo13666q@gmail.com>
2024-11-01 14:52:06 +0300

    test: fix

--- test/helpers/supportDefaultAssignment.js
+++ test/helpers/supportDefaultAssignment.js
@@ -1,7 +1,7 @@
 module.exports = function supportDefaultAssignment() {
 	try {
 		// eslint-disable-next-line no-unused-vars
-		var E = eval("class E { toString() { return 'default' } }");
+		var E = eval("(class E { toString() { return 'default' } })");
 		var f1 = eval("(function f1({a, b = E}) {return new b().toString();})");
 		return f1({ a: "test" }) === "default";
 	} catch (_err) {
