780333108a5ea55486b5a8ab156f0b4478eddd6f
Tim Fish <tim@timfish.uk>
2024-11-14 14:08:15 +0000

    Remove long test

--- test/TestCasesDevtoolSourceMapDebugIds.longtest.js
+++
@@ -1,8 +0,0 @@
-const { describeCases } = require("./TestCases.template");
-
-describe("TestCases", () => {
-	describeCases({
-		name: "devtool-source-map-debugids",
-		devtool: "source-map-debugids"
-	});
-});
