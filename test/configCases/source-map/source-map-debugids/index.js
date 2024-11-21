b4f853309f26c2a8c380f8ed27287222b2a584dd
alexander.akait <sheo13666q@gmail.com>
2024-11-21 16:03:36 +0300

    feat: support for eval

--- test/configCases/source-map/source-map-debugids/index.js
+++ test/configCases/source-map/source-map-debugids/index.js
@@ -1,9 +1,12 @@
+const fs = require("fs");
+
 it("source should include debug id that matches debugId key in sourcemap", function() {
-	var fs = require("fs");
-	var source = fs.readFileSync(__filename, "utf-8");
-	var sourceMap = fs.readFileSync(__filename + ".map", "utf-8");
-	var map = JSON.parse(sourceMap);
+	const source = fs.readFileSync(__filename, "utf-8");
+	const sourceMap = fs.readFileSync(__filename + ".map", "utf-8");
+	const map = JSON.parse(sourceMap);
 	expect(map.debugId).toBeDefined();
+	expect(
+		/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/i.test(map.debugId)
+	).toBe(true);
 	expect(source).toContain(`//# debugId=${map.debugId}`);
 });
-
