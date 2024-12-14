9fd9be4b7e60f65f3ccbf66f896bfa36f31ff7c0
hai-x <haijie0619@gmail.com>
2024-12-14 15:37:35 +0800

    test: fix

--- test/configCases/url/disable-dynamic-url/index.js
+++ test/configCases/url/disable-dynamic-url/index.js
@@ -1,8 +1,8 @@
 it("should compile fine", () => {
-	let dyn = "dynamic"
-	let url = new URL(`./${dyn}?foo=bar`, import.meta.url)
-	expect(url.search).toBe("?foo=bar")
-	expect(url.pathname).toMatch("url/disable-dynamic-url/dynamic")
+	let dyn1 = "dynamic"
+	let url1 = new URL(`./${dyn1}?foo=bar`, import.meta.url)
+	expect(url1.search).toBe("?foo=bar")
+	expect(url1.pathname).toMatch("url/disable-dynamic-url/dynamic")
 
 	let dyn2 = "./dynamic"
 	let url2 = new URL(dyn2, import.meta.url)
