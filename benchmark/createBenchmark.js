3af30e2ef99e2e6a4d8c5974e6081589fec5480e
Yuva <94527987+yuvashrikarunakaran@users.noreply.github.com>
2024-12-16 22:20:56 +0530

    Update createBenchmark.js
    Modify your Webpack configuration to include the TerserWebpackPlugin under the optimization.minimizer property. Here's how your updated script would look

--- benchmark/createBenchmark.js
+++ benchmark/createBenchmark.js
@@ -1,30 +1,49 @@
-const webpack = require("../");
-const path = require("path");
+ const webpack = require('../');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 webpack(
-	{
-		context: __dirname,
-		entry: "./createBenchmark/entry.js",
-		output: {
-			path: __dirname,
-			filename: "benchmark-bundle.js"
-		},
-		target: "node",
-		node: {
-			__dirname: false
-		},
-		optimization: {
-			moduleIds: "named"
-		},
-		plugins: [
-			new webpack.IgnorePlugin(/^(fsevents|terser)$/),
-			new webpack.NormalModuleReplacementPlugin(
-				/^.\/loadLoader$/,
-				path.resolve(__dirname, "./createBenchmark/loadLoader")
-			)
-		]
-	},
-	(err, stats) => {
-		console.log(stats.toString());
-	}
+  {
+    context: __dirname,
+    entry: './createBenchmark/entry.js',
+    output: {
+      path: __dirname,
+      filename: 'benchmark-bundle.js',
+    },
+    target: 'node',
+    node: {
+      __dirname: false,
+    },
+    optimization: {
+      moduleIds: 'named',
+      minimize: true, // Enable minimization
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // Example option to remove console.log
+            },
+            format: {
+              comments: false, // Remove comments
+            },
+          },
+          extractComments: false, // Prevent generation of license files
+        }),
+      ],
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^(fsevents|terser)$/),
+      new webpack.NormalModuleReplacementPlugin(
+        /^.\/loadLoader$/,
+        path.resolve(__dirname, './createBenchmark/loadLoader')
+      ),
+    ],
+  },
+  (err, stats) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stats.toString());
+  }
 );
