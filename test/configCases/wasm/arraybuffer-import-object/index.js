bf15c0d301f9b50f420a357b1d74cf065b6caa2c
Arka Pratim Chaudhuri <arka.mcu700@gmail.com>
2024-12-24 21:34:54 +0530

    chore: test

--- test/configCases/wasm/arraybuffer-import-object/index.js
+++ test/configCases/wasm/arraybuffer-import-object/index.js
@@ -1,5 +1,6 @@
 it("should work", () => import("./module.wat").then(({ bar }) => {
     const bytes = new Uint8Array(bar.buffer, 0, 2);
-    const string = new TextDecoder("utf8").decode(bytes);
+    const util = require("util");
+    const string = new util.TextDecoder("utf8").decode(bytes);
     expect(string).toBe("Hi");
 }))
