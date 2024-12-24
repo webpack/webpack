ed7fd528257ff333dfcdbe30c639280a57412f90
Arka Pratim Chaudhuri <arka.mcu700@gmail.com>
2024-12-24 19:27:31 +0530

    fix: remove unnecessary files

--- test/configCases/wasm/arraybuffer-mangled-importObj/index.js
+++
@@ -1,6 +0,0 @@
-it("should work", () => import("./module.wat").then(({ bar, first, second, third }) => {
-    const bytes = new Uint8Array(bar.buffer, 0, 2);
-    const string = new TextDecoder("utf8").decode(bytes);
-    expect(string).toBe("Hi");
-    expect(first() - second() + third()).toBe(4);
-}))
