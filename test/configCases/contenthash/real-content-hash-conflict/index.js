2d1295417e10de21842f95a0957c27d0fa94ca07
ahabhgk <ahabhgk@gmail.com>
2025-01-02 19:08:22 +0800

    add prefix when creating chunk contenthash

--- test/configCases/contenthash/real-content-hash-conflict/index.js
+++ test/configCases/contenthash/real-content-hash-conflict/index.js
@@ -1,6 +1,9 @@
-import(/* webpackChunkName: "async" */ "./async");
+it("should have correct value from async chunk", async () => {
+	const { default: value } = await import(/* webpackChunkName: "async" */ "./async");
+	expect(value).toBe(42);
+})
 
-it("should compile", () => {
+it("should not replace user content when real content hash has a hashPrefix", () => {
 	const userContent = __USER_CONTENT__;
 	const encodedUserContent = __BASE64_USER_CONTENT__;
 	const decodedUserContent = Buffer.from(encodedUserContent, "base64").toString("utf-8");
