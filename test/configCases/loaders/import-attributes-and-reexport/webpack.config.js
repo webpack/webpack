0b1df56d612628659d21dc378efea811f09dc28d
alexander-akait <sheo13666q@gmail.com>
2025-03-06 22:18:18 +0300

    fix: do not duplicate modules with import attributes and reexport

---
+++ test/configCases/loaders/import-attributes-and-reexport/webpack.config.js
@@ -0,0 +1,11 @@
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	module: {
+		rules: [
+			{
+				with: { type: "RANDOM" },
+				use: require.resolve("./test-loader")
+			}
+		]
+	}
+};
