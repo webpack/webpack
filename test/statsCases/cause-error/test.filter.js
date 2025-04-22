cc4389ef28451635527a5d3127b897199be8c37f
alexander-akait <sheo13666q@gmail.com>
2025-04-23 02:02:37 +0300

    refactor: test

--- test/statsCases/cause-error/test.filter.js
+++
@@ -1,6 +0,0 @@
-const supportsErrorCause = require("../../helpers/supportsErrorCause");
-const supportsAggregateError = require("../../helpers/supportsAggregateError");
-
-module.exports = function () {
-	return supportsErrorCause() && supportsAggregateError();
-};
--- test/statsCases/cause-error/webpack.config.js
+++ test/statsCases/cause-error/webpack.config.js
@@ -1,5 +1,70 @@
 const WebpackError = require("../../../lib/WebpackError");
 
+/** @typedef {Error & { cause?: unknown }} ErrorWithCause */
+
+/**
+ * @param {string} message message
+ * @param {{ cause?: unknown }=} options options
+ * @returns {ErrorWithCause} error with cause
+ */
+function createErrorWithCause(message, options) {
+	// @ts-expect-error for tests
+	const error = new Error(message, options);
+
+	if (typeof (/** @type {ErrorWithCause} */ (error).cause) === "undefined") {
+		/** @type {ErrorWithCause} */
+		(error).cause = options.cause;
+	}
+
+	return error;
+}
+
+/** @typedef {WebpackError & { cause?: unknown }} WebpackErrorWithCause */
+
+/**
+ * @param {string} message message
+ * @param {{ cause?: unknown }=} options options
+ * @returns {WebpackErrorWithCause} error with cause
+ */
+function createWebpackErrorWithCause(message, options) {
+	const error = new WebpackError(message, options);
+
+	if (typeof error.cause === "undefined") {
+		/** @type {WebpackErrorWithCause} */
+		(error).cause = options.cause;
+	}
+
+	return error;
+}
+
+/** @typedef {Error & { cause?: unknown, errors: EXPECTED_ANY[] }} AggregateError */
+
+/**
+ * @param {EXPECTED_ANY[]} errors errors
+ * @param {string} message message
+ * @param {{ cause?: unknown }=} options options
+ * @returns {AggregateError} error with errors and cause
+ */
+function createAggregateError(errors, message, options = {}) {
+	// @ts-expect-error for tests
+	if (typeof AggregateError === "undefined") {
+		const error = new Error(message);
+
+		if (options.cause) {
+			/** @type {AggregateError} */
+			(error).cause = options.cause;
+		}
+
+		/** @type {AggregateError} */
+		(error).errors = errors;
+
+		return /** @type {AggregateError} */ (error);
+	}
+
+	// @ts-expect-error for tests
+	return new AggregateError(errors, message, options);
+}
+
 /** @type {import("../../../").Configuration} */
 module.exports = {
 	name: `error cause`,
@@ -8,46 +73,42 @@ module.exports = {
 	plugins: [
 		compiler => {
 			compiler.hooks.compilation.tap("Test", compilation => {
-				// @ts-expect-error for tests
-				const errCauseErr = new Error("error with case", {
+				const errCauseErr = createErrorWithCause("error with case", {
 					cause: new Error("error case")
 				});
 				compilation.errors.push(errCauseErr);
 				compilation.warnings.push(errCauseErr);
-				// @ts-expect-error for tests
-				const errCauseErrCauseErr = new Error("error with nested error case", {
-					// @ts-expect-error for tests
-					cause: new Error("test", { cause: new Error("nested case") })
-				});
+				const errCauseErrCauseErr = createErrorWithCause(
+					"error with nested error case",
+					{
+						cause: createErrorWithCause("test", {
+							cause: new Error("nested case")
+						})
+					}
+				);
 				compilation.errors.push(errCauseErrCauseErr);
 				compilation.warnings.push(errCauseErrCauseErr);
-				// @ts-expect-error for tests
-				const errCauseStr = new Error("error with string case", {
+				const errCauseStr = createErrorWithCause("error with string case", {
 					cause: "string case"
 				});
 				compilation.errors.push(errCauseStr);
 				compilation.warnings.push(errCauseStr);
-				// @ts-expect-error for tests
-				const aggregateError = new AggregateError(
+				const aggregateError = createAggregateError(
 					[
-						// @ts-expect-error for tests
-						new Error("first error", {
-							// @ts-expect-error for tests
-							cause: new Error("cause", {
+						createErrorWithCause("first error", {
+							cause: createErrorWithCause("cause", {
 								cause: new Error("nested cause in errors")
 							})
 						}),
 						"second string error",
-						// @ts-expect-error for tests
-						new AggregateError(
+						createAggregateError(
 							[new Error("nested first"), new Error("nested second")],
 							"third nested aggregate error"
 						)
 					],
 					"aggregate error",
 					{
-						// @ts-expect-error for tests
-						cause: new Error("cause\ncause\ncause", {
+						cause: createErrorWithCause("cause\ncause\ncause", {
 							cause: "nested string cause"
 						})
 					}
@@ -57,9 +118,12 @@ module.exports = {
 				const webpackError = new WebpackError("webpack error");
 				compilation.errors.push(webpackError);
 				compilation.warnings.push(webpackError);
-				const webpackErrorCause = new WebpackError("webpack error with case", {
-					cause: new Error("cause")
-				});
+				const webpackErrorCause = createWebpackErrorWithCause(
+					"webpack error with case",
+					{
+						cause: new Error("cause")
+					}
+				);
 				compilation.errors.push(webpackErrorCause);
 				compilation.warnings.push(webpackErrorCause);
 			});
