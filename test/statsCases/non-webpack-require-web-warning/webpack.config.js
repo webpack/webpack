5be36111dbb03d8361d6f52c7b88e5c8cac5ce54
suguanYang <wangbinqi77@gmail.com>
2025-04-29 22:11:33 +0800

    test: non webpack require warning for es6 module web target

---
+++ test/statsCases/non-webpack-require-web-warning/webpack.config.js
@@ -0,0 +1,12 @@
+/** @type {import("../../../").Configuration} */
+module.exports = {
+	entry: "./index.js",
+	target: ["web"],
+	mode: "production",
+	experiments: {
+		outputModule: true
+	},
+	output: {
+		module: true
+	}
+};
