47a44cb11fe960903053aba2e6c5bcbb62a6b402
xiaoxiaojx <784487301@qq.com>
2025-05-05 15:59:31 +0800

    fix: hmr might fail if there are new initial chunks

---
+++ test/hotCases/chunks/initial-chunks-hmr/initial.js
@@ -0,0 +1,6 @@
+require('lib-js/a')
+export var value = "a";
+---
+require('lib-js/b')
+export var value = "b";
+
---
+++ test/hotCases/chunks/initial-chunks-hmr/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	optimization: {
+		chunkIds: "named",
+		moduleIds: "named",
+		minimize: false,
+		concatenateModules: false,
+		splitChunks: {
+			minSize: 1000,
+			chunks: "all",
+			cacheGroups: {
+				lib: {
+					test: /lib-js/,
+					name: "lib"
+				},
+				default: false,
+				defaultVendors: false
+			}
+		},
+		mangleExports: false
+	},
+	output: {
+		filename: "[name].js",
+		chunkFilename: "async/[name].js"
+	}
+};
