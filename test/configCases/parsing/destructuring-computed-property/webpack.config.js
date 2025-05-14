611d9b7f94f0272eb4ebe2097d4faa5aafd206ef
alexander-akait <sheo13666q@gmail.com>
2025-05-14 19:03:25 +0300

    test: more

--- test/configCases/parsing/destructuring-computed-property/webpack.config.js
+++ test/configCases/parsing/destructuring-computed-property/webpack.config.js
@@ -1,6 +1,13 @@
+const webpack = require("../../../../");
+
 /** @type {import("../../../../").Configuration} */
 module.exports = {
 	optimization: {
 		concatenateModules: true
-	}
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			PROPERTY: JSON.stringify("foo")
+		})
+	]
 };
