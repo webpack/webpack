410651443fc1ae75405136eebd49e9a009a9d41f
alexander-akait <sheo13666q@gmail.com>
2025-05-14 15:34:00 +0300

    test: improve

--- test/configCases/parsing/destructuring-namespace-import/index.js
+++ test/configCases/parsing/destructuring-namespace-import/index.js
@@ -3,40 +3,39 @@ import * as namespace from "./module.js";
 it("should work with destructuring", function() {
 	{
 		const { foo } = namespace;
-		expect(foo).toBe("foo");
+		expect(foo).toBe("bar");
 	}
 
 	{
 		let foo;
 		({ foo } = namespace);
-		expect(foo).toBe("foo");
+		expect(foo).toBe("bar");
 	}
 
 	{
 		let foo;
 		({ foo = 'foo' } = namespace);
-		expect(foo).toBe("foo");
+		expect(foo).toBe("bar");
 	}
 
 	{
 		const { foo = 'foo' } = namespace;
-		expect(foo).toBe("foo");
+		expect(foo).toBe("bar");
 	}
 
 	{
 		const strings = [];
 		({ foo : strings[0] } = namespace);
-		expect(strings[0]).toBe("foo");
-	}
-
-	{
-		const key = "foo";
-		const { [key]: a = 'foo' } = namespace;
-		expect(a).toBe("foo");
+		expect(strings[0]).toBe("bar");
 	}
 
 	{
 		const { foo: otherFoo = 'foo' } = namespace;
-		expect(otherFoo).toBe("foo");
+		expect(otherFoo).toBe("bar");
+	}
+
+	{
+		const { bar = 'foo' } = namespace;
+		expect(bar).toBe("foo");
 	}
 });
