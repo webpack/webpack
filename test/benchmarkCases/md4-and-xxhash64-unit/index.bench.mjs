843f7882050988065b356e1a23998e1d84ca9cfa
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-05-15 18:43:29 +0300

    chore: migrate on tinybench (#19533)

--- test/benchmarkCases/md4-and-xxhash64-unit/index.bench.mjs
+++ test/benchmarkCases/md4-and-xxhash64-unit/index.bench.mjs
@@ -1,39 +1,6 @@
 import crypto from "crypto";
 import createHash from "../../../lib/util/createHash.js";
 
-const NS_PER_MS = 1000000; // 1ms
-const MAX_WARMUP_DURATION = NS_PER_MS; // 1ms
-
-// eslint-disable-next-line no-unused-vars
-let result;
-
-function measure(fn, count) {
-	const start = process.hrtime.bigint();
-	for (let i = 0; i < count; i++) result = fn();
-	return Number(process.hrtime.bigint() - start);
-}
-
-function factoryOnStart(f1, f2) {
-	return function onStart() {
-		const count = 1;
-
-		while (true) {
-			const timings = [f1, f2, f1, f2, f1, f2].map(f => measure(f, count));
-			const t1 = Math.min(timings[0], timings[2], timings[4]);
-			const t2 = Math.min(timings[1], timings[3], timings[5]);
-
-			if (
-				count === 1 &&
-				(t1 > MAX_WARMUP_DURATION || t2 > MAX_WARMUP_DURATION)
-			) {
-				continue;
-			}
-
-			break;
-		}
-	};
-}
-
 function factoryXxhash64(longString) {
 	return function xxhash64String() {
 		const hash = createHash("xxhash64");
@@ -61,18 +28,16 @@ export default function xxhash64AndMd4Benchmarks(suite) {
 		const fn1 = factoryXxhash64(longString);
 		const fn2 = factoryMd4(longString);
 
-		suite.add({
-			name: `xxhash64 ${subName1}`,
-			collectBy: subName1,
-			onStart: factoryOnStart(fn1, fn2),
-			fn: fn1
+		suite.add(`xxhash64 ${subName1}`, fn1, {
+			beforeAll() {
+				this.collectBy = subName1;
+			}
 		});
 
-		suite.add({
-			name: `md4 ${subName1}`,
-			collectBy: subName1,
-			onStart: factoryOnStart(fn1, fn2),
-			fn: fn2
+		suite.add(`md4 ${subName1}`, fn2, {
+			beforeAll() {
+				this.collectBy = subName1;
+			}
 		});
 
 		const bufferSize = size * 2;
@@ -82,18 +47,16 @@ export default function xxhash64AndMd4Benchmarks(suite) {
 		const fn3 = factoryXxhash64(buffer);
 		const fn4 = factoryMd4(buffer);
 
-		suite.add({
-			name: `xxhash64 ${subName2}`,
-			collectBy: subName2,
-			onStart: factoryOnStart(fn3, fn4),
-			fn: fn3
+		suite.add(`xxhash64 ${subName2}`, fn3, {
+			beforeAll() {
+				this.collectBy = subName2;
+			}
 		});
 
-		suite.add({
-			name: `md4 ${subName2}`,
-			collectBy: subName2,
-			onStart: factoryOnStart(fn3, fn4),
-			fn: fn4
+		suite.add(`md4 ${subName2}`, fn4, {
+			beforeAll() {
+				this.collectBy = subName2;
+			}
 		});
 	}
 }
