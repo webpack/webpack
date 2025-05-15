6c75830e9f2340a070f30d070fc7a0cb620b50c6
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-05-15 18:16:26 +0300

    chore: avoid eslint warning after running tests (#19534)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -496,7 +496,8 @@ export default defineConfig([
 	{
 		files: [
 			"test/configCases/{dll-plugin-entry,dll-plugin-side-effects,dll-plugin}/**/webpack.config.js",
-			"examples/**/*.js"
+			"examples/**/*.js",
+			"test/NodeTemplatePlugin.test.js"
 		],
 		rules: {
 			"n/no-missing-require": "off"
