2accb4bc7f6b3443ba041f27c8f01ab30f2f241e
hai-x <haijie0619@gmail.com>
2025-05-16 21:45:41 +0800

    fix: sourceTypes of asset module when lazy compilation

---
+++ test/hotCases/asset-modules-source/lazy-compilation/webpack.config.js
@@ -0,0 +1,25 @@
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	experiments: {
+		lazyCompilation: {
+			entries: false,
+			imports: true
+		}
+	},
+	node: {
+		__dirname: false
+	},
+	module: {
+		generator: {
+			asset: {
+				filename: "assets/[name][ext]"
+			}
+		},
+		rules: [
+			{
+				test: /file\.text$/,
+				type: "asset/resource"
+			}
+		]
+	}
+};
