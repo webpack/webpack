bca3d4097cb3169da28d6b631f95923956563f32
hai-x <98948357+hai-x@users.noreply.github.com>
2025-05-20 19:44:21 +0800

    fix: eliminate unused statements in certain scenarios (#19536)

---
+++ test/configCases/parsing/issue-19514/webpack.config.js
@@ -0,0 +1,12 @@
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	target: "node",
+	output: {
+		library: {
+			type: "commonjs"
+		}
+	},
+	optimization: {
+		minimize: false
+	}
+};
