4be8fcc3dd2fdd30da413988fabf33e2d3ef7c39
Noritaka Kobayashi <arukimedesu1166777@gmail.com>
2025-05-28 23:40:54 +0900

    test: refactor test code (#19564)

--- test/configCases/output-module/iife-multiple-entry-modules/test.js
+++ test/configCases/output-module/iife-multiple-entry-modules/test.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-it("IIFE should present for multiple entires", () => {
+it("IIFE should present for multiple entries", () => {
 	const source = fs.readFileSync(path.join(__dirname, "bundle0.mjs"), "utf-8");
 	expect(source).toContain(`This entry needs to be wrapped in an IIFE because it needs to be isolated against other entry modules.`);
 });
