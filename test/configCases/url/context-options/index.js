9248b563186ca04c8ae4587cdfcc0bc4577ef5d5
hai-x <haijie0619@gmail.com>
2025-06-02 02:45:42 +0800

    feat: support more context options for URL

---
+++ test/configCases/url/context-options/index.js
@@ -0,0 +1,42 @@
+const throwModuleNotFound = fn => expect(fn).toThrow(/Cannot find module/);
+
+it("should work with context options", () => {
+	// webpackInclude
+	let dyn = "/a.json";
+	let url = new URL(
+		/* webpackInclude: /.json$/ */ "./folder1" + dyn,
+		import.meta.url
+	);
+	expect(url).toMatch("folder1/a.json");
+
+	// webpackExclude
+	dyn = "/b.js";
+	throwModuleNotFound(() => {
+		url = new URL(
+			/* webpackInclude: /.json$/ */ "./folder1" + dyn,
+			import.meta.url
+		);
+	});
+
+	dyn = "/c.js";
+	throwModuleNotFound(() => {
+		url = new URL(
+			/* webpackExclude: /.js$/ */ "./folder1" + dyn,
+			import.meta.url
+		);
+	});
+
+	// webpackExclude interfere the context prefix
+	throwModuleNotFound(() => {
+		dyn = "/a.json";
+		url = new URL(
+			/* webpackExclude: /.\/folder2/ */ "./folder2" + dyn,
+			import.meta.url
+		);
+	});
+
+	// webpackIgnore
+	dyn = "bundle0.js";
+	url = new URL(/* webpackIgnore: true */ "./" + dyn, import.meta.url);
+	expect(url.href).toMatch(__webpack_base_uri__.href);
+});
