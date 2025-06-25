18a9a0c0eaf0647c2a85a4474d8a58f8d711281d
alexander-akait <sheo13666q@gmail.com>
2025-06-25 19:17:58 +0300

    test: fix

--- test/cases/parsing/using/test.filter.js
+++ test/cases/parsing/using/test.filter.js
@@ -1,5 +1,10 @@
 const supportsUsing = require("../../../helpers/supportsUsing");
 
-module.exports = function () {
+module.exports = function (config) {
+	// TODO https://github.com/terser/terser/issues/1625
+	if (config.minimize) {
+		return false;
+	}
+
 	return supportsUsing();
 };
