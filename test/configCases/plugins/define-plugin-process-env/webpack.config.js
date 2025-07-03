cdf7ef54ea6b20ff03aebf0559bcde65dcbe684b
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-07-03 16:11:10 +0300

    test: issue-14800

---
+++ test/configCases/plugins/define-plugin-process-env/webpack.config.js
@@ -0,0 +1,12 @@
+const DefinePlugin = require("../../../../").DefinePlugin;
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	target: "node",
+	mode: "production",
+	plugins: [
+		new DefinePlugin({
+			"process.env.ENVIRONMENT": JSON.stringify("node")
+		})
+	]
+};
