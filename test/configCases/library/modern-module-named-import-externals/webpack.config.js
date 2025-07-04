6db082f6075a071af22c317c511a7f63f4d5eb0f
Ryuya <Ryuyxx@gmail.com>
2025-07-03 23:56:25 -0700

    fix: external module async dependencies

--- test/configCases/library/modern-module-named-import-externals/webpack.config.js
+++ test/configCases/library/modern-module-named-import-externals/webpack.config.js
@@ -23,21 +23,23 @@ module.exports = {
 		usedExports: true
 	},
 	plugins: [
-		function plugin() {
-			const handler = (
-				/** @type {import("../../../../types").Compilation} */ compilation
-			) => {
-				compilation.hooks.afterProcessAssets.tap(
-					"testcase",
-					(
-						/** @type {Record<string, import("webpack-sources").Source>} */ assets
-					) => {
-						const source = assets["test.js"].source();
-						expect(source).toMatchSnapshot();
-					}
-				);
-			};
-			this.hooks.compilation.tap("testcase", handler);
+		compiler => {
+			compiler.hooks.compilation.tap(
+				"testcase",
+				(
+					/** @type {import("../../../../types").Compilation} */ compilation
+				) => {
+					compilation.hooks.afterProcessAssets.tap(
+						"testcase",
+						(
+							/** @type {Record<string, import("webpack-sources").Source>} */ assets
+						) => {
+							const source = assets["test.js"].source();
+							expect(source).toMatchSnapshot();
+						}
+					);
+				}
+			);
 		}
 	]
 };
