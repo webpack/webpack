e7b6df37102c6e8a52add02f7e2c69e494464604
Ryuya <Ryuyxx@gmail.com>
2025-07-03 22:50:45 -0700

    fix: yarn fix:special and update types

--- test/configCases/library/modern-module-tree-shaking-simple/webpack.config.js
+++ test/configCases/library/modern-module-tree-shaking-simple/webpack.config.js
@@ -23,12 +23,19 @@ module.exports = {
 		usedExports: true
 	},
 	plugins: [
-		function () {
-			const handler = compilation => {
-				compilation.hooks.afterProcessAssets.tap("testcase", assets => {
-					const source = assets["test.js"].source();
-					expect(source).toMatchSnapshot();
-				});
+		function plugin() {
+			const handler = (
+				/** @type {import("../../../../types").Compilation} */ compilation
+			) => {
+				compilation.hooks.afterProcessAssets.tap(
+					"testcase",
+					(
+						/** @type {Record<string, import("webpack-sources").Source>} */ assets
+					) => {
+						const source = assets["test.js"].source();
+						expect(source).toMatchSnapshot();
+					}
+				);
 			};
 			this.hooks.compilation.tap("testcase", handler);
 		}
