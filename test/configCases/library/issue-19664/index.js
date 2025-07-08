6801022b7bb653b21a8786f060b8110e93613c1c
hai-x <98948357+hai-x@users.noreply.github.com>
2025-07-09 01:23:18 +0800

    fix: lose closing brace when exports are unprovided (#19669)

---
+++ test/configCases/library/issue-19664/index.js
@@ -0,0 +1,10 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = main;
+function main() {}
+
+// commonjs bailout
+if(this){}
+
+it("Should work when exports are unprovided", function() {
+	expect(true).toBe(true);
+});
