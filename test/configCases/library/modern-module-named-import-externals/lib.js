bb7dcdde7c1508bcde674dd0699b411a793edf88
Ryuya <Ryuyxx@gmail.com>
2025-07-08 10:25:59 -0700

    test: add namespace imports and reexports for ESM externals

--- test/configCases/library/modern-module-named-import-externals/lib.js
+++ test/configCases/library/modern-module-named-import-externals/lib.js
@@ -1,2 +1,11 @@
 import { b as a1 } from 'externals0'
+import * as externals2NS from 'externals2'
+
+// Test namespace reexport
+export * as reexportedNS from 'externals2'
+
+// Test mixed reexports
+export { c, d } from 'externals0'
+
 a1;
+externals2NS.default;
--- test/configCases/library/modern-module-named-import-externals/test.js
+++ test/configCases/library/modern-module-named-import-externals/test.js
@@ -1,9 +1,23 @@
+// Original named import/export tests
 export { a as a1 } from 'externals0'
 import { a as a2 } from 'externals1'
 import defaultValue from 'externals2'
 import 'externals3'
 
+// Namespace import tests
+import * as ns1 from 'externals0'
+import * as ns2 from 'externals1'
+
+// Reexport cases
+export * from 'externals0'
+export * as ns3 from 'externals1'
+export { b as b1, c as c1 } from 'externals0'
+
 import './lib'
 
+// Use the imports to ensure they're not tree-shaken
 a2;
 defaultValue;
+ns1.a;
+ns1.b;
+ns2.a;
