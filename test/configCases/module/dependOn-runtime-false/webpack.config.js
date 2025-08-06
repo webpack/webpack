73cd8ec16b5f26e59f491232cc86a93725d0173a
xiaoxiaojx <784487301@qq.com>
2025-08-07 00:09:48 +0800

    fix(esm): fix the import of missing dependency chunks

---
+++ test/configCases/module/dependOn-runtime-false/webpack.config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/** @type {import("../../../../types").Configuration} */
+module.exports = () => ({
+	devtool: false,
+	mode: "development",
+	entry: {
+		main: {
+			import: "./index.js",
+			dependOn: "shared"
+		},
+		shared: "./common.js"
+	},
+	output: {
+		filename: "[name].mjs",
+		library: {
+			type: "module"
+		}
+	},
+	target: ["web", "es2020"],
+	experiments: {
+		outputModule: true
+	},
+	optimization: {
+		minimize: false,
+		runtimeChunk: false,
+		splitChunks: {
+			cacheGroups: {
+				separate: {
+					test: /separate/,
+					chunks: "all",
+					filename: "separate.mjs",
+					enforce: true
+				}
+			}
+		}
+	}
+});
---
+++ test/configCases/module/entries-runtime-single/separate.js
@@ -0,0 +1,2 @@
+import common from "./common";
+export default common;
