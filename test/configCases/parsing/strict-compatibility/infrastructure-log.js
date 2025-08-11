dab3b2c4d2440e2c167be326e33811611d16900b
Ryuya <Ryuyxx@gmail.com>
2025-08-10 18:48:22 -0700

    chore: add strict compatibility log and warnings modules

---
+++ test/configCases/parsing/strict-compatibility/infrastructure-log.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = (options) => {
+	if (options.cache && options.cache.type === "filesystem") {
+		return [/Pack got invalid because of write to/];
+	}
+	return [];
+};
