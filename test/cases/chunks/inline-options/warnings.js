daf4c56e16a7ad516f5069bc798fd2bfe1dbede0
ahabhgk <ahabhgk@gmail.com>
2025-08-18 23:52:00 +0800

    fix

--- test/cases/chunks/inline-options/warnings.js
+++ test/cases/chunks/inline-options/warnings.js
@@ -1,12 +1,17 @@
 "use strict";
 
-module.exports = [
-	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
-	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
-	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
-	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
-	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
-	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
-	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
-	/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/
-];
+module.exports = (options) => {
+	if (options.mode === "development") {
+		return [];
+	}
+	return [
+		/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
+		/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
+		/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
+		/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
+		/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
+		/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
+		/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/,
+		/You don't need `webpackExports` if the usage of dynamic import is statically analyse-able/
+	];
+};
