dc3aa36485681c6924a5ffce2b581a5bee875fc4
ahabhgk <ahabhgk@gmail.com>
2025-09-12 19:51:06 +0800

    add test

--- test/configCases/parsing/statical-dynamic-import-this/index.js
+++ test/configCases/parsing/statical-dynamic-import-this/index.js
@@ -7,3 +7,15 @@ it("should respect strictThisContextOnImports for member call", async () => {
 	expect(m2.b.usedExports).toBe(true);
 	expect(m2.usedExports).toEqual(["b", "usedExports"]);
 })
+
+it("should respect strictThisContextOnImports for member call in then", async () => {
+	await import("./dir4/a").then(m => {
+		expect(m.f()).toBe(1);
+		expect(m.usedExports).toBe(true);
+	});
+	await import("./dir4/lib").then(m2 => {
+		expect(m2.b.f()).toBe(1);
+		expect(m2.b.usedExports).toBe(true);
+		expect(m2.usedExports).toEqual(["b", "usedExports"]);
+	});
+})
