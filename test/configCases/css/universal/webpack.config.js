721d66177da648dd6a744a37464ef1d3110844e3
alexander-akait <sheo13666q@gmail.com>
2025-09-18 23:57:15 +0300

    test: added + refactor

--- test/configCases/css/universal/webpack.config.js
+++ test/configCases/css/universal/webpack.config.js
@@ -1,11 +1,25 @@
 "use strict";
 
-/** @type {import("../../../../").Configuration} */
-module.exports = {
-	target: ["web", "node"],
-	mode: "development",
-	experiments: {
-		css: true,
-		outputModule: true
+/** @type {import("../../../../").Configuration[]} */
+module.exports = [
+	{
+		name: "web",
+		target: ["web", "node"],
+		devtool: false,
+		mode: "development",
+		experiments: {
+			css: true,
+			outputModule: true
+		}
+	},
+	{
+		name: "node",
+		target: ["web", "node"],
+		devtool: false,
+		mode: "development",
+		experiments: {
+			css: true,
+			outputModule: true
+		}
 	}
-};
+];
