8e94386ee466aa232d71399aae89771ddfbf5f5a
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-09-19 03:37:26 +0300

    fix: property handle external modules for CSS (#19917)

--- test/configCases/css/external-in-node/index.js
+++ test/configCases/css/external-in-node/index.js
@@ -1,6 +1,27 @@
-it("should import an external css", done => {
-	import("../external/style.css").then(x => {
-		expect(x).toEqual({});
+import * as style from "./style.module.css";
+import * as style1 from "https://test.cases/external.css";
+
+it("should import an external CSS inside CSS", () => {
+	expect(style).toEqual(
+		nsObj({
+			class: "_external-in-node_style_module_css-class"
+		})
+	);
+});
+
+it("should work with an external URL", () => {
+	const url = new URL("https://test.cases/url-external.css", import.meta.url);
+
+	expect(url.toString().endsWith("url-external.css")).toBe(true);
+});
+
+it("should import an external css dynamically", done => {
+	import("./dynamic.modules.css").then(x => {
+		expect(x).toEqual(
+			nsObj({
+				other: "_external-in-node_dynamic_modules_css-other"
+			})
+		);
 		done();
 	}, done);
 });
