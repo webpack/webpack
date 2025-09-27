11b640488aa3ee37f4e8bb6712c60fc0bedb0995
Hai <haijie0619@gmail.com>
2025-09-27 16:24:31 +0800

    feat: support function matcher in rule options

---
+++ test/configCases/source-map/test-fn-modules-source-map/webpack.config.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const webpack = require("../../../../");
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	node: {
+		__dirname: false,
+		__filename: false
+	},
+	entry: {
+		bundle0: ["./index.js"],
+		bundle1: ["./test1.js", "./test2.js"]
+	},
+	output: {
+		filename: "[name].js"
+	},
+	plugins: [
+		new webpack.EvalSourceMapDevToolPlugin({
+			test: (str) => {
+				if (str.endsWith(".js")) return true;
+			},
+			exclude: /test2\.js/,
+			module: true,
+			columns: false
+		})
+	]
+};
