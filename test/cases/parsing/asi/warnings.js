3c08fd105c03dba0f8a7571c89f407265a0777ad
Kevin Brey <manbearwiz@gmail.com>
2025-09-30 09:32:35 -0500

    fix(warnings): update import.meta warning messages for clarity (#19961)

--- test/cases/parsing/asi/warnings.js
+++ test/cases/parsing/asi/warnings.js
@@ -1,3 +1,7 @@
 "use strict";
 
-module.exports = [[/Critical dependency: Accessing import\.meta/]];
+module.exports = [
+	[
+		/Critical dependency: 'import\.meta' cannot be used as a standalone expression\. For static analysis, its properties must be accessed directly \(e\.g\., 'import\.meta\.url'\) or through destructuring\./
+	]
+];
