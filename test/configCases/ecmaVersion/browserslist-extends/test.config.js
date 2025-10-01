b6c781a0f1ae65cdbc5e6c066a31b5fedb44db38
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-10-01 20:12:27 +0300

    fix: support `extends` with env for `browserslist` (#19964)

---
+++ test/configCases/ecmaVersion/browserslist-extends/test.config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const rootPath = path.resolve(__dirname, "../../../../");
+const rootNodeModules = path.resolve(rootPath, "./node_modules");
+const browserslistPackage = path.resolve(
+	rootNodeModules,
+	"browserslist-config-mycompany2"
+);
+const content = `
+module.exports = {
+  development: [
+    'last 1 version'
+  ],
+  // We are in tests, so 'process.env.NODE_ENV' has the 'test' value (browserslist respects the 'process.env.NODE_ENV' value)
+  test: [
+    'ie 9',
+  ]
+}
+`;
+const browserslistFile = path.resolve(browserslistPackage, "./index.js");
+
+try {
+	fs.mkdirSync(browserslistPackage);
+} catch (_err) {
+	// Nothing
+}
+
+fs.writeFileSync(browserslistFile, content);
+
+module.exports = {
+	afterExecute() {
+		fs.unlinkSync(browserslistFile);
+		fs.rmdirSync(browserslistPackage);
+	}
+};
