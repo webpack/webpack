d4d787a922e1b5321588c711b771625be2ce6bd6
Hai <haijie0619@gmail.com>
2025-10-07 00:45:53 +0800

    refactor

--- test/configCases/plugins/manifest-plugin/infrastructure-log.js
+++ test/configCases/plugins/manifest-plugin/infrastructure-log.js
@@ -3,5 +3,6 @@
 module.exports = [
 	// each time returns different OriginalSource in webpack.config.js:33
 	// this prevents hit in inmemory cache
+	/^Pack got invalid because of write to: RealContentHashPlugin|analyse|third.party.js$/,
 	/^Pack got invalid because of write to: RealContentHashPlugin|analyse|third.party.js$/
 ];
