c9364e183ac706243271b4e642be867814397416
avivkeller <me@aviv.sh>
2025-10-07 11:53:16 -0400

    feat(import.meta): add dirname and filename

--- test/cases/parsing/filename/index.js
+++ test/cases/parsing/filename/index.js
@@ -1,3 +1,5 @@
+const path = require("path/posix");
+
 it("should be a string (__filename)", function() {
 	expect(__filename).toBeTypeOf("string");
 	var f = __filename;
@@ -8,4 +10,21 @@ it("should be a string (__dirname)", function() {
 	expect(__dirname).toBeTypeOf("string");
 	var d = __dirname;
 	expect(d).toBeTypeOf("string");
+});
+
+it("should be a string (import.meta.filename)", function() {
+	expect(import.meta.filename).toBeTypeOf("string");
+	var f = import.meta.filename;
+	expect(f).toBeTypeOf("string");
+});
+
+it("should be a string (import.meta.dirname)", function() {
+	expect(import.meta.dirname).toBeTypeOf("string");
+	var d = import.meta.dirname;
+	expect(d).toBeTypeOf("string");
+});
+
+it("should be able to be used to get this file", function() {
+	const filePath = path.join(import.meta.dirname, "index.js");
+	expect(filePath).toBe(import.meta.filename);
 });
