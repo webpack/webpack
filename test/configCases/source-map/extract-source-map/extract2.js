22c9a0b428214b285508c01d50427912538e592f
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-10-11 23:18:19 +0800

    test: make extract source map tests correct

--- test/configCases/source-map/extract-source-map/extract2.js
+++ test/configCases/source-map/extract-source-map/extract2.js
@@ -1,12 +1,12 @@
 "use strict";
 
 const fs = require("fs");
-const path = require("path");
 
 require("./test2");
 
 it("should extract source map - 2", () => {
-	const fileData = fs.readFileSync(path.resolve(__dirname, "bundle2.js.map")).toString("utf-8");
+	const fileData = fs.readFileSync(__filename + ".map").toString("utf-8");
 	const { sources } = JSON.parse(fileData);
-	expect(sources).toMatchSnapshot();
+	expect(sources).toContain("webpack:///./external-source-map.txt");
+	expect(sources).toContain("webpack:///./extract2.js");
 });
--- test/configCases/source-map/extract-source-map/extract4.js
+++ test/configCases/source-map/extract-source-map/extract4.js
@@ -1,12 +1,11 @@
 "use strict";
 
 const fs = require("fs");
-const path = require("path");
 
 require("./test4");
 
 it("should extract source map - 4", () => {
-	const fileData = fs.readFileSync(path.resolve(__dirname, "bundle3.js.map")).toString("utf-8");
+	const fileData = fs.readFileSync(__filename + ".map").toString("utf-8");
 	const { sources } = JSON.parse(fileData);
 	expect(sources.includes("webpack:///antd/./components/button/index.tsx")).toBe(true);
 });
