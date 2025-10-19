ffd8c60beb5edce5bd2a0b078197805de75990cd
xiaoxiaojx <784487301@qq.com>
2025-10-19 14:28:23 +0800

    feat(css): support exporting raw content

--- test/configCases/css/basic/index.js
+++ test/configCases/css/basic/index.js
@@ -1,9 +1,9 @@
 import * as style from "./style.css";
 
 it("should compile and load style on demand", done => {
-	expect(style).toEqual({});
+	expect(style).toMatchSnapshot();
 	import("./style2.css").then(x => {
-		expect(x).toEqual({});
+		expect(x).toMatchSnapshot();
 		const style = getComputedStyle(document.body);
 		expect(style.getPropertyValue("background")).toBe(" red");
 		expect(style.getPropertyValue("margin")).toBe(" 10px");
