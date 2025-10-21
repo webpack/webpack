543944e1859935d28e2ad10eb2ac6e8dc9e7d7e9
alexander-akait <sheo13666q@gmail.com>
2025-10-21 15:40:37 +0300

    fix: reexport module for default import when no used exports for systemjs library

---
+++ test/configCases/library/1-systemjs-external-commonjs-used-exports-false/webpack.config.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const path = require("path");
+const webpack = require("../../../../");
+
+/** @type {(env: Env, options: TestOptions) => import("../../../../").Configuration[]} */
+module.exports = (env, { testPath }) => [
+	{
+		entry: "./system-external-commonjs.js",
+		output: {
+			library: {
+				type: "system"
+			}
+		},
+		externals: {
+			"library-commonjs": path.resolve(
+				testPath,
+				"../0-create-library/system-commonjs.js"
+			)
+		},
+		optimization: {
+			usedExports: false
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				NAME: JSON.stringify("systemjs with external from commonjs format")
+			})
+		]
+	}
+];
---
+++ test/configCases/library/1-systemjs-external-esm-used-exports-false/system-external-esm.js
@@ -0,0 +1,7 @@
+import MyClass, {a, b} from "library-esm";
+
+it("should get exports from systemjs library (" + NAME + ")", function() {
+	expect(new MyClass().getValue()).toBe("my-class")
+	expect(a).toBe(10);
+	expect(b).toBe(20);
+});
