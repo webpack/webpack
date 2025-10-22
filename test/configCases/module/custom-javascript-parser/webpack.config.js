9ba389b21528368f0c7e087965c77363b59e27c5
alexander-akait <sheo13666q@gmail.com>
2025-10-22 19:29:34 +0300

    test: fix

--- test/configCases/module/custom-javascript-parser/webpack.config.js
+++ test/configCases/module/custom-javascript-parser/webpack.config.js
@@ -4,22 +4,38 @@ const meriyah = require("meriyah");
 
 let counter = 0;
 
+/** @typedef {import("estree").SourceLocation} SourceLocation */
+/** @typedef {import("estree").Program & { start: number, end: number, loc: SourceLocation }} Program */
+/** @typedef {import("estree").Comment & { start: number, end: number, loc: SourceLocation }} Comment */
+
+/**
+ * @param {string} sourceCode source code
+ * @param {{ sourceType: "module", comments: boolean, locations: boolean, semicolons: boolean }} options options
+ * @returns {{ ast: Program, comments: Comment[], semicolons: Set<number>}} parsed source code
+ */
 const parse = (sourceCode, options) => {
+	/** @type {Comment[]} */
 	const comments = [];
 	const semicolons = new Set();
+	/**
+	 * @param {number} pos pos
+	 */
+	const addSemicolons = (pos) => {
+		semicolons.add(pos);
+	};
 	const parseOptions = {
 		...options,
 		module: options.sourceType === "module",
 		loc: options.locations,
 		onComment: options.comments ? comments : undefined,
-		onInsertedSemicolon: options.semicolons
-			? (pos) => semicolons.add(pos)
-			: undefined
+		onInsertedSemicolon: options.semicolons ? addSemicolons : undefined
 	};
+	// @ts-expect-error meriyah types for comments are not align with estree
 	const ast = meriyah.parse(sourceCode, parseOptions);
 
 	counter++;
 
+	// @ts-expect-error meriyah types for ClassExpression is not align with estree
 	return { ast, comments, semicolons };
 };
 
