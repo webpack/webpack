28f917f9560015fb7ffe06ece1f8e6a94e4c3a94
Fy <1114550440@qq.com>
2025-10-23 00:05:55 +0800

    fix: rename HarmonyExportDependencyParserPlugin exported id to CompatibilityPlugin tagged id (#20037)

---
+++ test/configCases/module/consume-webpack-runtime/runtime-export-default.js
@@ -0,0 +1,3 @@
+function __webpack_require__() {return 42}
+__webpack_require__.m = () => {}
+export default __webpack_require__
---
+++ test/configCases/module/consume-webpack-runtime/runtime-export-named.js
@@ -0,0 +1,4 @@
+
+function __webpack_require__() {return 42}
+__webpack_require__.m = () => {}
+export {__webpack_require__}
