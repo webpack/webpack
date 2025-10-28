6d02aea1aaad055aad0e9794ea27719547c1fcd1
alexander-akait <sheo13666q@gmail.com>
2025-10-28 16:30:48 +0300

    fix: logic for eval-only

--- test/configCases/node/filename-and-dirname/webpack.config.js
+++ test/configCases/node/filename-and-dirname/webpack.config.js
@@ -20,7 +20,9 @@ config.push(
 			},
 			plugins: [
 				new webpack.DefinePlugin({
-					NODE_VALUE: typeof value === "boolean" ? value : JSON.stringify(value)
+					NODE_VALUE:
+						typeof value === "boolean" ? value : JSON.stringify(value),
+					FORMAT: JSON.stringify("cjs")
 				})
 			]
 		}))
@@ -28,53 +30,51 @@ config.push(
 
 // ES modules
 config.push(
-	...values
-		.filter((item) => item !== "eval-only")
-		.map((value) => ({
-			target: "node",
-			node: {
-				__filename: value,
-				__dirname: value
-			},
-			output: {
-				module: true
-			},
-			experiments: {
-				outputModule: true
-			},
-			plugins: [
-				new webpack.DefinePlugin({
-					NODE_VALUE: typeof value === "boolean" ? value : JSON.stringify(value)
-				})
-			]
-		}))
+	...values.map((value) => ({
+		target: "node",
+		node: {
+			__filename: value,
+			__dirname: value
+		},
+		output: {
+			module: true
+		},
+		experiments: {
+			outputModule: true
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				NODE_VALUE: typeof value === "boolean" ? value : JSON.stringify(value),
+				FORMAT: JSON.stringify("esm")
+			})
+		]
+	}))
 );
 
 // // ES modules with support `import.meta.dirname` and `import.meta.filename`
 config.push(
-	...values
-		.filter((item) => item !== "eval-only")
-		.map((value) => ({
-			target: "node",
-			node: {
-				__filename: value,
-				__dirname: value
-			},
-			output: {
-				module: true,
-				environment: {
-					importMetaDirnameAndFilename: true
-				}
-			},
-			experiments: {
-				outputModule: true
-			},
-			plugins: [
-				new webpack.DefinePlugin({
-					NODE_VALUE: typeof value === "boolean" ? value : JSON.stringify(value)
-				})
-			]
-		}))
+	...values.map((value) => ({
+		target: "node",
+		node: {
+			__filename: value,
+			__dirname: value
+		},
+		output: {
+			module: true,
+			environment: {
+				importMetaDirnameAndFilename: true
+			}
+		},
+		experiments: {
+			outputModule: true
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				NODE_VALUE: typeof value === "boolean" ? value : JSON.stringify(value),
+				FORMAT: JSON.stringify("esm")
+			})
+		]
+	}))
 );
 
 config.push({
