6c9c3022c095bb7520e6fcfd8e1d756fceccfdc4
alexander-akait <sheo13666q@gmail.com>
2025-11-05 10:09:27 +0300

    feat(css): support `composes`

--- test/configCases/css/css-modules/use-style.js
+++ test/configCases/css/css-modules/use-style.js
@@ -51,5 +51,11 @@ export default {
 	cssModuleWithCustomFileExtension: myCssClass,
 	notAValidCssModuleExtension: isNotACSSModule,
 	UsedClassName,
-	exportLocalVarsShouldCleanup: `${hasOwnProperty(notACssModule, 'local-color')} ${hasOwnProperty(notACssModule, "LOCAL-COLOR")}`
+	exportLocalVarsShouldCleanup: `${hasOwnProperty(notACssModule, 'local-color')} ${hasOwnProperty(notACssModule, "LOCAL-COLOR")}`,
+	exportName: style.exportName,
+	exportNameOtherNoSpaces: style.exportNameOtherNoSpaces,
+	exportNameOtherFromKeyword: style.exportNameOtherFromKeyword,
+	exportNameOtherFromKeywordWithFrom1: style.exportNameOtherFromKeywordWithFrom1,
+	exportNameOtherFromKeywordWithFrom2: style.exportNameOtherFromKeywordWithFrom2,
+	exportNameWeirdCharacters: style.exportNameWeirdCharacters,
 };
