72cd3da3fa2f69f5cdc923c8c5e79e34fb25a9de
alexander-akait <sheo13666q@gmail.com>
2025-11-07 02:36:02 +0300

    test: postcss plugins

--- test/configCases/css/postcss-modules-plugins/index.js
+++ test/configCases/css/postcss-modules-plugins/index.js
@@ -1,4 +1,9 @@
-import * as classes from "./style.modules.css";
+import * as composes from "./postcss-modules-extract-imports.modules.css";
+import * as local from "./postcss-modules-local-by-default.local.modules.css";
+import * as global from "./postcss-modules-local-by-default.global.modules.css";
+import * as pure from "./postcss-modules-local-by-default.pure.modules.css";
+import * as scope from "./postcss-modules-scope.modules.css";
+import * as values from "./postcss-modules-values.modules.css";
 
 it("should work", () => {
 	const links = document.getElementsByTagName("link");
@@ -9,6 +14,11 @@ it("should work", () => {
 		css.push(link.sheet.css);
 	}
 
-	expect(classes).toMatchSnapshot();
+	expect(composes).toMatchSnapshot();
+	expect(local).toMatchSnapshot();
+	expect(global).toMatchSnapshot();
+	expect(pure).toMatchSnapshot();
+	expect(scope).toMatchSnapshot();
+	expect(values).toMatchSnapshot();
 	expect(css).toMatchSnapshot();
 });
