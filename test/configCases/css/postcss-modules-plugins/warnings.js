3ba1e98c07dafffe7bab2103c02aa4e6eb381fda
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-11-07 03:22:57 +0300

    test: postcss plugins (#20100)

--- test/configCases/css/postcss-modules-plugins/warnings.js
+++ test/configCases/css/postcss-modules-plugins/warnings.js
@@ -5,5 +5,14 @@ module.exports = [
 	/Incorrect composition, expected global keyword or string value/,
 	/Incorrect composition, expected class named/,
 	/Referenced class name "className" in composes not found/,
-	/Self-referencing class name "abc-from-def" in composes not found/
+	/Self-referencing class name "abc-from-def" in composes not found/,
+	/Composition is only allowed when selector is single local class name not in "exportName22", "exportName23"/,
+	/Composition is only allowed when selector is single local class name not in "exportName25", "exportName26"/,
+	/Composition is only allowed when selector is single local class name not in "exportName28", "exportName29"/,
+	/Composition is only allowed when selector is single local class name not in "exportName33", "exportName34"/,
+	/Composition is only allowed when selector is single local class name not in "exportName37", "exportName38"/,
+	/Self-referencing class name "exportName40" in composes not found/,
+	/Missing whitespace after ':local' in ':local.b \{'/,
+	/Missing whitespace after ':global' in ':global, .foobar :global \{'/,
+	/Missing whitespace after ':global' in ':global.bar \{'/
 ];
