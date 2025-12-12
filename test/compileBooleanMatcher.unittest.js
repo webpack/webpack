5e52e1ff4af563ab409db40f9d85dfcf0659a775
samarthsinh2660 <rajuvala80@gmail.com>
2025-12-12 12:22:03 +0530

    feat: optimize regex character class with ranges in compileBooleanMatcher

--- test/compileBooleanMatcher.unittest.js
+++ test/compileBooleanMatcher.unittest.js
@@ -29,13 +29,42 @@ describe("itemsToRegexp", () => {
 	);
 
 	expectCompiled("single chars", ["a", "b", "c", "1", "2", "3"], (e) =>
-		e.toMatchInlineSnapshot("[123abc]")
+		e.toMatchInlineSnapshot("[1-3a-c]")
+	);
+
+	expectCompiled("single chars with ranges", ["1", "2", "3", "4", "a"], (e) =>
+		e.toMatchInlineSnapshot("[1-4a]")
+	);
+
+	expectCompiled(
+		"single chars consecutive range",
+		["a", "b", "c", "d", "e"],
+		(e) => e.toMatchInlineSnapshot("[a-e]")
+	);
+
+	expectCompiled(
+		"single chars multiple ranges",
+		["1", "2", "3", "a", "b", "c", "x", "y", "z"],
+		(e) => e.toMatchInlineSnapshot("[1-3a-cx-z]")
+	);
+
+	expectCompiled(
+		"single chars with gaps",
+		["a", "c", "e", "g"],
+		// cspell:ignore aceg
+		(e) => e.toMatchInlineSnapshot("[aceg]")
+	);
+
+	expectCompiled(
+		"single chars mixed ranges and singles",
+		["1", "2", "3", "5", "7", "8", "9"],
+		(e) => e.toMatchInlineSnapshot("[1-357-9]")
 	);
 
 	expectCompiled(
 		"prefixes",
 		["ab1", "ab2", "ab3", "ab4", "de5", "de6", "de7", "ef8", "ef9", "gh0"],
-		(e) => e.toMatchInlineSnapshot("(ab[1234]|de[567]|ef[89]|gh0)")
+		(e) => e.toMatchInlineSnapshot("(ab[1-4]|de[5-7]|ef[89]|gh0)")
 	);
 
 	expectCompiled("short prefixes", "a,ab", (e) =>
@@ -49,7 +78,7 @@ describe("itemsToRegexp", () => {
 	);
 
 	expectCompiled("suffixes", "a1,b1,c1,d1,e1,a2,b2,c2", (e) =>
-		e.toMatchInlineSnapshot("([abcde]1|[abc]2)")
+		e.toMatchInlineSnapshot("([a-e]1|[a-c]2)")
 	);
 
 	expectCompiled(
@@ -57,7 +86,7 @@ describe("itemsToRegexp", () => {
 		"674,542,965,12,942,483,445,943,423,995,434,122,995,248,432,165,436,86,435,221",
 		(e) =>
 			e.toMatchInlineSnapshot(
-				"(1(2|22|65)|4(3[2456]|23|45|83)|9(42|43|65|95)|221|248|542|674|86)"
+				"(1(2|22|65)|4(3[24-6]|23|45|83)|9(42|43|65|95)|221|248|542|674|86)"
 			)
 	);
 
