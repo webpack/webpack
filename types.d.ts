8f9076711c2c10cae3be754719cabe48c6894cb1
samarthsinh2660 <rajuvala80@gmail.com>
2025-12-13 01:48:21 +0530

    fix: add _importAttributes to NormalModuleLoaderContext in types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -12037,6 +12037,7 @@ declare interface NormalModuleLoaderContext<OptionsType> {
 	_module?: NormalModule;
 	_compilation?: Compilation;
 	_compiler?: Compiler;
+	_importAttributes?: Record<string, unknown>;
 }
 declare class NormalModuleReplacementPlugin {
 	/**
