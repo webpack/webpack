2efd21b0b06baa9b1a7f009b336379dcef24c1a5
Fy <1114550440@qq.com>
2025-12-18 20:16:57 +0800

    fix(re-exports): reexports runtime calculation should not accessing `__WEBPACK_IMPORT_KEY__` decl with `var` (#20249)

---
+++ .changeset/clean-mugs-rescue.md
@@ -0,0 +1,5 @@
+---
+"webpack": patch
+---
+
+Reexports runtime calculation should not accessing **WEBPACK_IMPORT_KEY** decl with var.
---
+++ test/configCases/externals/dynamic-reexports/webpack.config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+	target: ["node"],
+	output: {
+		environment: {
+			arrowFunction: true,
+			const: false
+		}
+	}
+};
