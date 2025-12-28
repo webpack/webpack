4575cbe4ddf3341e2478270abc8eaf06553c05ea
samarthsinh2660 <rajuvala80@gmail.com>
2025-12-28 23:20:50 +0530

    test: fix Issue #16878 test case structure and logic

--- test/configCases/issues/issue-16878/index.js
+++ test/configCases/issues/issue-16878/index.js
@@ -1,3 +1,20 @@
+import fs from "fs";
+
+it("should not try to resolve directories in new URL()", () => {
+	const source = fs.readFileSync(__filename, "utf-8");
+	const missingModuleMarker = "webpack" + "MissingModule";
+	try {
+		expect(source).toMatch(/new URL\("\.\/subdir\/",/);
+		expect(source).toMatch(/new URL\("\.\/",/);
+		expect(source).toMatch(/new URL\("\.",/);
+		expect(source).toMatch(/new URL\("\.\.",/);
+		expect(source).not.toContain(missingModuleMarker);
+	} catch (e) {
+		console.log("Generated Source:\n" + source);
+		throw e;
+	}
+});
+
 console.log(new URL("./subdir/", import.meta.url).href);
 console.log(new URL("./", import.meta.url).href);
 console.log(new URL(".", import.meta.url).href);
