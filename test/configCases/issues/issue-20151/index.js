75e3c825b409b6451a003ac25fec0a793fc6ba59
samarthsinh2660 <rajuvala80@gmail.com>
2026-01-06 10:22:03 +0530

    refactor: use variable declaration for inactive harmony imports instead of inline undefined

--- test/configCases/issues/issue-20151/index.js
+++ test/configCases/issues/issue-20151/index.js
@@ -10,5 +10,13 @@ it("should correctly handle unused function with same-named local variable", () 
 	expect(__).toBe("local");
 });
 
+it("should declare inactive harmony imports as variables", () => {
+	const fs = require("fs");
+	const source = fs.readFileSync(__filename, "utf-8");
+	// The identifier should remain in the code, but be declared as undefined
+	expect(source).toContain("var i18n = /* inactive harmony import i18n __ */ undefined;");
+	expect(source).toMatch(/return i18n\("wtf"\)/);
+});
+
 // This ensures unused is not tree-shaken in some configurations
 export { unused };
