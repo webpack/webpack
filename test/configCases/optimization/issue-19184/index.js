96344c00b30822001e171236feed3d74a5b339e2
Hai <haijie0619@gmail.com>
2026-01-10 13:35:05 +0800

    fix

--- test/configCases/optimization/issue-19184/index.js
+++ test/configCases/optimization/issue-19184/index.js
@@ -1,12 +1,14 @@
-it("should have module ids defined in sorted order", function() {
+it("should have module ids defined in sorted order", function () {
 	const ids = [];
 	for (var i = 1; i <= 5; i++) {
 		var moduleId = require("./files/file" + i + ".js");
-		ids[i-1] = moduleId;
+		ids[i - 1] = moduleId;
 	}
 
 	const expectedIds = new Set(ids);
 
-	const moduleIds = Object.keys(__webpack_modules__).filter(id => expectedIds.has(id));
+	const moduleIds = Object.keys(__webpack_modules__).filter((id) =>
+		expectedIds.has(id)
+	);
 	expect(moduleIds).toEqual(ids);
 });
