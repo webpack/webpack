dd63723c71a779f4801b5798f70e0c2216f10581
Hai <haijie0619@gmail.com>
2026-01-21 02:50:10 +0800

    refactor

--- test/configCases/compiletime/exports-presence/warnings.js
+++ test/configCases/compiletime/exports-presence/warnings.js
@@ -12,5 +12,125 @@ module.exports = [
 	{
 		moduleName: /ddd/,
 		message: /NoNo.+not found/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /26:7/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /27:3/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /42:13/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /45:7/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /48:6/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /53:13/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /58:14/
+	},
+	{
+		moduleName: /index/,
+		message: /b.+not found/,
+		loc: /64:14/
+	},
+	{
+		moduleName: /index/,
+		message: /b.+not found/,
+		loc: /68:14/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /71:21/
+	},
+	{
+		moduleName: /index/,
+		message: /a.+not found/,
+		loc: /74:9/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /85:6/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /106:6/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /111:6/
+	},
+	{
+		moduleName: /index/,
+		message: /c.+not found/,
+		loc: /116:6/
+	},
+	{
+		moduleName: /index/,
+		message: /c.+not found/,
+		loc: /116:37/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /126:12/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /127:15/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /131:7/
+	},
+	{
+		moduleName: /index/,
+		message: /c.+not found/,
+		loc: /131:13/
+	},
+	{
+		moduleName: /index/,
+		message: /c.+not found/,
+		loc: /132:15/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /132:18/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /136:3/
+	},
+	{
+		moduleName: /index/,
+		message: /d.+not found/,
+		loc: /136:7/
 	}
 ];
