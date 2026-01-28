ca6115c5658b3cca67d3bbb236f33f7ba1e001fb
xiaoxiaojx <784487301@qq.com>
2026-01-29 00:09:31 +0800

    test: add self-import test case for dynamic import

---
+++ test/configCases/dynamic-import/self-import/test.config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+	findBundle(i) {
+		switch (i) {
+			case 0:
+				return [`bundle${i}.js`];
+			case 1:
+				return [`runtime.bundle${i}.js`, `main.bundle${i}.js`];
+			case 2:
+				return [`bundle${i}.mjs`];
+			case 3:
+				return [`runtime.bundle${i}.mjs`, `main.bundle${i}.mjs`];
+		}
+	}
+};
---
+++ test/configCases/dynamic-import/self-import/warnings.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = [
+	/Critical dependency: the request of a dependency is an expression/,
+	/Critical dependency: the request of a dependency is an expression/,
+	/Critical dependency: the request of a dependency is an expression/,
+	/Critical dependency: the request of a dependency is an expression/
+];
