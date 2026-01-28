312d3105c40d0679c5935d3b83aa951cc502071c
magic-akari <akari.ccino@gmail.com>
2026-01-29 02:18:15 +0800

    docs(test): add JSDoc types to phase-loader.js

--- test/configCases/module/import-phase/phase-loader.js
+++ test/configCases/module/import-phase/phase-loader.js
@@ -1,5 +1,8 @@
 "use strict";
 
+/** @typedef {import("../../../../types").LoaderDefinition<{ phase: string }>} LoaderDefinition */
+
+/** @type {LoaderDefinition} */
 module.exports = function (source) {
 	const options = this.getOptions();
 	return `${source}\nexport default ${JSON.stringify(
