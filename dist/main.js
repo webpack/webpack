19782958c2b9d59600afd4d3f3fee4d013ed01c9
jserfeng <1114550440@qq.com>
2026-02-02 12:06:13 +0800

    fix: tests

---
+++ dist/main.js
@@ -0,0 +1,188 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./node_modules/inline-loader/index.js!./node_modules/nested-package1/index.js"
+/*!*************************************************************************************!*\
+  !*** ./node_modules/inline-loader/index.js!./node_modules/nested-package1/index.js ***!
+  \*************************************************************************************/
+() {
+
+eval("{\n\n//# sourceURL=webpack:///./node_modules/nested-package1/index.js?./node_modules/inline-loader/index.js\n}");
+
+/***/ },
+
+/***/ "./node_modules/inline-loader/index.js!./node_modules/package/index.js"
+/*!*****************************************************************************!*\
+  !*** ./node_modules/inline-loader/index.js!./node_modules/package/index.js ***!
+  \*****************************************************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var nested_package1__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! nested-package1 */ \"./node_modules/nested-package1/index.js\");\n/* harmony import */ var nested_package1__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(nested_package1__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var nested_package2__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! nested-package2 */ \"./node_modules/package/node_modules/nested-package2/index.js\");\n/* harmony import */ var nested_package2__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(nested_package2__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var inline_loader_nested_package1__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! inline-loader!nested-package1 */ \"./node_modules/inline-loader/index.js!./node_modules/nested-package1/index.js\");\n/* harmony import */ var inline_loader_nested_package1__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(inline_loader_nested_package1__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var inline_loader_nested_package2__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! inline-loader!nested-package2 */ \"./node_modules/inline-loader/index.js!./node_modules/package/node_modules/nested-package2/index.js\");\n/* harmony import */ var inline_loader_nested_package2__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(inline_loader_nested_package2__WEBPACK_IMPORTED_MODULE_3__);\n\n\n\n\n\n\n//# sourceURL=webpack:///./node_modules/package/index.js?./node_modules/inline-loader/index.js\n}");
+
+/***/ },
+
+/***/ "./node_modules/inline-loader/index.js!./node_modules/package/node_modules/nested-package2/index.js"
+/*!**********************************************************************************************************!*\
+  !*** ./node_modules/inline-loader/index.js!./node_modules/package/node_modules/nested-package2/index.js ***!
+  \**********************************************************************************************************/
+() {
+
+eval("{\n\n//# sourceURL=webpack:///./node_modules/package/node_modules/nested-package2/index.js?./node_modules/inline-loader/index.js\n}");
+
+/***/ },
+
+/***/ "./node_modules/inline-loader/index.js!./src/local.js"
+/*!************************************************************!*\
+  !*** ./node_modules/inline-loader/index.js!./src/local.js ***!
+  \************************************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var package__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! package */ \"./node_modules/package/index.js\");\n/* harmony import */ var inline_loader_package__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! inline-loader!package */ \"./node_modules/inline-loader/index.js!./node_modules/package/index.js\");\n\n\n\n\n//# sourceURL=webpack:///./src/local.js?./node_modules/inline-loader/index.js\n}");
+
+/***/ },
+
+/***/ "./node_modules/nested-package1/index.js"
+/*!***********************************************!*\
+  !*** ./node_modules/nested-package1/index.js ***!
+  \***********************************************/
+() {
+
+eval("{\n\n//# sourceURL=webpack:///./node_modules/nested-package1/index.js?\n}");
+
+/***/ },
+
+/***/ "./node_modules/package/index.js"
+/*!***************************************!*\
+  !*** ./node_modules/package/index.js ***!
+  \***************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var nested_package1__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! nested-package1 */ \"./node_modules/nested-package1/index.js\");\n/* harmony import */ var nested_package1__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(nested_package1__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var nested_package2__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! nested-package2 */ \"./node_modules/package/node_modules/nested-package2/index.js\");\n/* harmony import */ var nested_package2__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(nested_package2__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var inline_loader_nested_package1__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! inline-loader!nested-package1 */ \"./node_modules/inline-loader/index.js!./node_modules/nested-package1/index.js\");\n/* harmony import */ var inline_loader_nested_package1__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(inline_loader_nested_package1__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var inline_loader_nested_package2__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! inline-loader!nested-package2 */ \"./node_modules/inline-loader/index.js!./node_modules/package/node_modules/nested-package2/index.js\");\n/* harmony import */ var inline_loader_nested_package2__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(inline_loader_nested_package2__WEBPACK_IMPORTED_MODULE_3__);\n\n\n\n\n\n\n//# sourceURL=webpack:///./node_modules/package/index.js?\n}");
+
+/***/ },
+
+/***/ "./node_modules/package/node_modules/nested-package2/index.js"
+/*!********************************************************************!*\
+  !*** ./node_modules/package/node_modules/nested-package2/index.js ***!
+  \********************************************************************/
+() {
+
+eval("{\n\n//# sourceURL=webpack:///./node_modules/package/node_modules/nested-package2/index.js?\n}");
+
+/***/ },
+
+/***/ "./src/index.js"
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var package__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! package */ \"./node_modules/package/index.js\");\n/* harmony import */ var inline_loader_package__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! inline-loader!package */ \"./node_modules/inline-loader/index.js!./node_modules/package/index.js\");\n/* harmony import */ var _local__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./local */ \"./src/local.js\");\n/* harmony import */ var inline_loader_local__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! inline-loader!./local */ \"./node_modules/inline-loader/index.js!./src/local.js\");\n\n\n\n\n\n\n\n//# sourceURL=webpack:///./src/index.js?\n}");
+
+/***/ },
+
+/***/ "./src/local.js"
+/*!**********************!*\
+  !*** ./src/local.js ***!
+  \**********************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var package__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! package */ \"./node_modules/package/index.js\");\n/* harmony import */ var inline_loader_package__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! inline-loader!package */ \"./node_modules/inline-loader/index.js!./node_modules/package/index.js\");\n\n\n\n\n//# sourceURL=webpack:///./src/local.js?\n}");
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Check if module exists (development only)
+/******/ 		if (__webpack_modules__[moduleId] === undefined) {
+/******/ 			var e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
