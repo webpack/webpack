926bab1a0f32acb40b3ed42eea3ac20cffc8d362
xiaoxiaojx <784487301@qq.com>
2026-02-15 22:57:09 +0800

    update

---
+++ test/hotCases/css/css-entry/webpack.config.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const webpack = require("../../../../");
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	mode: "development",
+	devtool: false,
+	entry: {
+		"css-entry": "./entry.css",
+		main: "./index.js"
+	},
+	output: {
+		cssFilename: "[name].css"
+	},
+	experiments: {
+		css: true
+	},
+	node: {
+		__dirname: false
+	},
+	plugins: [
+		{
+			apply(compiler) {
+				compiler.hooks.compilation.tap("Test", (compilation) => {
+					compilation.hooks.additionalTreeRuntimeRequirements.tap(
+						"Test",
+						(module, set, _context) => {
+							// To prevent the runtime error `ReferenceError: __webpack_exports__ is not defined`,
+							// which occurs because the default `output.library` setting is `commonjs2`,
+							// resulting in adding `module.exports = __webpack_exports__;`.
+							set.add(webpack.RuntimeGlobals.startup);
+							set.add(webpack.RuntimeGlobals.exports);
+						}
+					);
+				});
+			}
+		}
+	]
+};
