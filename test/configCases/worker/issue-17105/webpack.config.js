2a0c16eca4ee466e11b6fd86e00c8de2fc09e4cf
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2026-02-16 18:41:22 +0300

    fix: do not crash when a referenced chunk is not a runtime chunk (#20461)

---
+++ test/configCases/worker/issue-17105/webpack.config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/** @typedef {import("../../../../").Module} Module */
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	target: "node",
+	entry: {
+		app: "./app.js",
+		"my-worker": "./worker.js"
+	},
+	output: {
+		filename: "[name].js",
+		environment: {
+			nodePrefixForCoreModules: false
+		}
+	},
+	optimization: {
+		runtimeChunk: { name: "runtime" },
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					/**
+					 * @param {Module} mod module
+					 * @returns {string | boolean | null} result
+					 */
+					test(mod) {
+						return mod.context && mod.context.includes("node_modules");
+					},
+					chunks: "all",
+					name: "vendor",
+					priority: 10,
+					enforce: true
+				}
+			}
+		}
+	}
+};
