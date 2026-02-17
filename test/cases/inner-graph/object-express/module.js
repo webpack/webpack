b892223acd3759c5217cf646d2a3db3f747c6876
Hai <haijie0619@gmail.com>
2026-02-18 04:35:30 +0800

    fix: handle more expression for `isPure` detection

---
+++ test/cases/inner-graph/object-express/module.js
@@ -0,0 +1,17 @@
+import { EXPORT, EXPORT2, EXPORT3, EXPORT4, EXPORT5, EXPORT6 } from "./inner";
+
+const o1 = {
+	EXPORT2: EXPORT2,
+	EXPORT3: EXPORT3
+};
+
+const o2 = {
+	EXPORT4: EXPORT4
+};
+
+const o3 = {
+	EXPORT5: EXPORT5,
+	EXPORT6: EXPORT6
+};
+
+export { o1, o2, o3 };
