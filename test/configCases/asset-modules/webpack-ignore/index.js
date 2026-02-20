c52ada748153b77029c55f60bbc4e5f2d4c2ca5e
alexander-akait <sheo13666q@gmail.com>
2026-02-20 15:29:20 +0300

    test: ignore with `bytes` type and refactor tests

---
+++ test/configCases/asset-modules/webpack-ignore/index.js
@@ -0,0 +1,28 @@
+it("should work", async () => {
+	const decoder = new TextDecoder("utf-8");
+
+	const textFile = (await import(/* webpackIgnore: true */ "./file.text", { with: { type: "bytes" } })).default;
+	const textFileContent = decoder.decode(textFile);
+
+	expect(textFileContent).toBe("a Ā 𐀀 文 🦄 Text");
+
+	const jsonFile = (await import(/* webpackIgnore: true */ "./file.json", { with: { type: "bytes" } })).default;
+	const jsonFileContent = decoder.decode(jsonFile);
+
+	expect(JSON.parse(jsonFileContent)).toStrictEqual({ foo: "bar" });
+
+	const jsFile = (await import(/* webpackIgnore: true */ "./file.js", { with: { type: "bytes" } })).default;
+	const jsFileContent = decoder.decode(jsFile);
+
+	expect(jsFileContent).toBe("export default \"test\";\n");
+
+	const cssFile = (await import(/* webpackIgnore: true */ "./file.css", { with: { type: "bytes" } })).default;
+	const cssFileContent = decoder.decode(cssFile);
+
+	expect(cssFileContent).toBe("body { color: red; }\n");
+
+	const htmlFile = (await import(/* webpackIgnore: true */ "./file.html", { with: { type: "bytes" } })).default;
+	const htmlFileContent = decoder.decode(htmlFile);
+
+	expect(htmlFileContent).toBe("<div>test</div>\n");
+});
