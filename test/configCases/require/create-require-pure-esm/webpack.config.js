4d37f5ad3eabb5e4d5338b22eae918fedd508588
stefan binoj <stefan.binoj.007@gmail.com>
2026-02-22 11:19:58 +0530

    feat: add createRequire support for ESM modules with new parser

--- test/configCases/require/create-require-pure-esm/webpack.config.js
+++ test/configCases/require/create-require-pure-esm/webpack.config.js
@@ -3,12 +3,17 @@
 /** @type {import("../../../../").Configuration} */
 module.exports = {
 	target: "node",
+	entry: "./index.mjs",
 	optimization: {
 		moduleIds: "named"
 	},
 	module: {
 		parser: {
 			javascript: {
+				createRequire: true
+			},
+			"javascript/esm": {
+				commonjs: false,
 				createRequire: true
 			}
 		}
