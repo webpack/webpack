4b61e3c9ca7dc883ad85ed3bc4eed9e2a135d4ea
Hai <haijie0619@gmail.com>
2026-02-22 22:17:57 +0800

    remove test

--- test/configCases/chunk-graph/deterministic-roots/index.js
+++
@@ -1,6 +0,0 @@
-it("should produce deterministic output with cyclic dependencies", async () => {
-	import("./a").then(({ getCombined }) => {
-		const result = getCombined();
-		expect(result).toBe("abcaca")
-	});
-});
