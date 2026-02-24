c44db7547873c7fb97351f46dbf280010cfdcb03
stefan binoj <stefan.binoj.007@gmail.com>
2026-02-24 12:57:48 +0530

    add tests

---
+++ test/configCases/target/universal-fetch-priority/webpack.config.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const base = {
+	target: ["web", "node"],
+	entry: "./index.mjs",
+	experiments: {
+		outputModule: true
+	},
+	output: {
+		publicPath: "",
+		module: true,
+		filename: "bundle0.mjs",
+		chunkFilename: "[name].mjs",
+		crossOriginLoading: "anonymous"
+	},
+	optimization: {
+		minimize: false,
+		chunkIds: "named"
+	}
+};
+
+/** @type {import("../../../../").Configuration[]} */
+module.exports = [
+	{ name: "web", ...base },
+	{ name: "node", ...base }
+];
