f1ec3d2d6a98ac5785d83389dc6f19c30f518f00
aryanraj45 <rajaryan445567@gmail.com>
2026-02-24 17:03:44 +0530

    test: add coverage for CSS modules with target webworker

---
+++ test/configCases/css/webworker/webpack.config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	target: "webworker",
+	mode: "development",
+	devtool: false,
+	experiments: {
+		css: true
+	}
+};
