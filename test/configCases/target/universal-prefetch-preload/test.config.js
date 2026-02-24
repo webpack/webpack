67c5aae55b1f50d9609138aea1b6ddb566e782c9
Raj Aryan <143009186+aryanraj45@users.noreply.github.com>
2026-02-24 19:19:30 +0530

    test: add configCase for ESM prefetch/preload under neutral target (#20524)

---
+++ test/configCases/target/universal-prefetch-preload/test.config.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+	moduleScope(scope, options) {
+		if (options.name.includes("node")) {
+			delete scope.window;
+			delete scope.document;
+			delete scope.self;
+		}
+	},
+	findBundle() {
+		return "./bundle0.mjs";
+	}
+};
