510abf0cdde20c9e7f9e8386010896565ae09953
aryanraj45 <rajaryan445567@gmail.com>
2026-02-24 12:45:14 +0530

    test: add coverage for external script in EnvironmentNotSupportAsyncWarning

--- test/configCases/async-module/environment-not-support-async-warning/test.filter.js
+++ test/configCases/async-module/environment-not-support-async-warning/test.filter.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const supportsGlobalThis = require("../../../helpers/supportsGlobalThis");
 const supportsWebAssembly = require("../../../helpers/supportsWebAssembly");
 
-module.exports = () => supportsWebAssembly();
+module.exports = () => supportsWebAssembly() && supportsGlobalThis();
