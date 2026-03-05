webpack/webpack

commit 39933d33e47e7e1fdaf9f2bb4aee63dd82491a0d

feat: cjs require destructuring assignment tree shaking (#20548)

test/cases/cjs-tree-shaking/require-destructuring/index.js
@@ -1,0 +1,12 @@
+it("should static analyze require destructuring assignment", () => {
+	const { a, usedExports } = require("./module");
+	expect(a).toBe("a");
+	expect(usedExports).toEqual(["a", "usedExports"]);
+});
+
+it("should support require context destructuring assignment", () => {
+	const file = "a";
+	const { a, usedExports } = require(`./dir/${file}.js`);
+	expect(a).toBe("a/a");
+	expect(usedExports).toEqual(["a", "usedExports"]);
+});

lib/dependencies/CommonJsImportsParserPlugin.js
@@ -14,6 +14,7 @@ const {
 	expressionIsUnsupported,
 	toConstantDependency
 } = require("../javascript/JavascriptParserHelpers");
+const traverseDestructuringAssignmentProperties = require("../util/traverseDestructuringAssignmentProperties");
 const CommonJsFullRequireDependency = require("./CommonJsFullRequireDependency");
 const CommonJsRequireContextDependency = require("./CommonJsRequireContextDependency");
 const CommonJsRequireDependency = require("./CommonJsRequireDependency");
@@ -31,6 +32,7 @@ const RequireResolveHeaderDependency = require("./RequireResolveHeaderDependency
 /** @typedef {import("estree").NewExpression} NewExpression */
 /** @typedef {import("../../declarations/WebpackOptions").JavascriptParserOptions} JavascriptParserOptions */
 /** @typedef {import("../Dependency").DependencyLocation} DependencyLocation */
+/** @typedef {import("../Dependency").RawReferencedExports} RawReferencedExports */
 /** @typedef {import("../javascript/JavascriptParser")} JavascriptParser */
 /** @typedef {import("../javascript/BasicEvaluatedExpression")} BasicEvaluatedExpression */
 /** @typedef {import("../javascript/JavascriptParser").ImportSource} ImportSource */
@@ -46,6 +48,48 @@ const RequireResolveHeaderDependency = require("./RequireResolveHeaderDependency
  */
 
 const PLUGIN_NAME = "CommonJsImportsParserPlugin";
+
+/**
+ * @param {Expression} expression expression
+ * @returns {boolean} true, when expression is `require(...)` or `module.require(...)`
+ */
+const isRequireCallExpression = (expression) => {
+	if (expression.type !== "CallExpression") return false;
+	const { callee } = expression;
+	if (callee.type === "Identifier") {
+		return callee.name === "require";
+	}
+	if (callee.type === "MemberExpression" && !callee.computed) {
+		const object = callee.object;
+		const property = callee.property;
+		return (
+			object.type === "Identifier" &&
+			object.name === "module" &&
+			property.type === "Identifier" &&
+			property.name === "require"
+		);
+	}
+	return false;
+};
+
+/**
+ * @param {JavascriptParser} parser parser
+ * @param {CallExpression | NewExpression} expr expression
+ * @returns {RawReferencedExports | null} referenced exports from destructuring
+ */
+const getRequireReferencedExportsFromDestructuring = (parser, expr) => {
+	const referencedPropertiesInDestructuring =
+		parser.destructuringAssignmentPropertiesFor(expr);
+	if (!referencedPropertiesInDestructuring) return null;
+
+	/** @type {RawReferencedExports} */
+	const referencedExports = [];
+	traverseDestructuringAssignmentProperties(
+		referencedPropertiesInDestructuring,
+		(stack) => referencedExports.push(stack.map((p) => p.id))
+	);
+	return referencedExports;
+};
 
 /**
  * @param {JavascriptParser} parser parser
@@ -101,10 +145,15 @@ const createRequireCallHandler = (parser, options, getContext) => {
 	 */
 	const processRequireItem = (expr, param) => {
 		if (param.isString()) {
+			const referencedExports = getRequireReferencedExportsFromDestructuring(
+				parser,
+				expr
+			);
 			const dep = new CommonJsRequireDependency(
 				/** @type {string} */ (param.string),
 				/** @type {Range} */ (param.range),
-				getContext()
+				getContext(),
+				referencedExports
 			);
 			dep.loc = /** @type {DependencyLocation} */ (expr.loc);
 			dep.optional = Boolean(parser.scope.inTry);
@@ -118,6 +167,10 @@ const createRequireCallHandler = (parser, options, getContext) => {
 	 * @returns {boolean | void} true when handled
 	 */
 	const processRequireContext = (expr, param) => {
+		const referencedExports = getRequireReferencedExportsFromDestructuring(
+			parser,
+			expr
+		);
 		const dep = ContextDependencyHelpers.create(
 			CommonJsRequireContextDependency,
 			/** @type {Range} */ (expr.range),
@@ -125,7 +178,8 @@ const createRequireCallHandler = (parser, options, getContext) => {
 			expr,
 			options,
 			{
-				category: "commonjs"
+				category: "commonjs",
+				referencedExports
 			},
 			parser,
 			undefined,
@@ -353,6 +407,13 @@ class CommonJsImportsParserPlugin {
 	 */
 	apply(parser) {
 		const options = this.options;
+		parser.hooks.collectDestructuringAssignmentProperties.tap(
+			PLUGIN_NAME,
+			(expr) => {
+				if (isRequireCallExpression(expr)) return true;
+			}
+		);
+
 		const getContext = () => {
 			if (parser.currentTagData) {
 				const { context } =

lib/dependencies/CommonJsRequireContextDependency.js
@@ -5,6 +5,7 @@
 
 "use strict";
 
+const Dependency = require("../Dependency");
 const makeSerializable = require("../util/makeSerializable");
 const ContextDependency = require("./ContextDependency");
 const ContextDependencyTemplateAsRequireCall = require("./ContextDependencyTemplateAsRequireCall");
@@ -13,6 +14,10 @@ const ContextDependencyTemplateAsRequireCall = require("./ContextDependencyTempl
 /** @typedef {import("../serialization/ObjectMiddleware").ObjectDeserializerContext} ObjectDeserializerContext */
 /** @typedef {import("../serialization/ObjectMiddleware").ObjectSerializerContext} ObjectSerializerContext */
 /** @typedef {import("./ContextDependency").ContextDependencyOptions} ContextDependencyOptions */
+/** @typedef {import("../Dependency").RawReferencedExports} RawReferencedExports */
+/** @typedef {import("../Dependency").ReferencedExports} ReferencedExports */
+/** @typedef {import("../ModuleGraph")} ModuleGraph */
+/** @typedef {import("../util/runtime").RuntimeSpec} RuntimeSpec */
 
 class CommonJsRequireContextDependency extends ContextDependency {
 	/**
@@ -33,6 +38,22 @@ class CommonJsRequireContextDependency extends ContextDependency {
 
 	get type() {
 		return "cjs require context";
+	}
+
+	/**
+	 * Returns list of exports referenced by this dependency
+	 * @param {ModuleGraph} moduleGraph module graph
+	 * @param {RuntimeSpec} runtime the runtime for which the module is analysed
+	 * @returns {ReferencedExports} referenced exports
+	 */
+	getReferencedExports(moduleGraph, runtime) {
+		if (!this.options.referencedExports) {
+			return Dependency.EXPORTS_OBJECT_REFERENCED;
+		}
+		return this.options.referencedExports.map((name) => ({
+			name,
+			canMangle: false
+		}));
 	}
 
 	/**

lib/dependencies/CommonJsRequireDependency.js
@@ -5,22 +5,31 @@
 
 "use strict";
 
+const Dependency = require("../Dependency");
 const makeSerializable = require("../util/makeSerializable");
 const ModuleDependency = require("./ModuleDependency");
 const ModuleDependencyTemplateAsId = require("./ModuleDependencyTemplateAsId");
 
+/** @typedef {import("../Dependency").RawReferencedExports} RawReferencedExports */
+/** @typedef {import("../Dependency").ReferencedExports} ReferencedExports */
+/** @typedef {import("../ModuleGraph")} ModuleGraph */
 /** @typedef {import("../javascript/JavascriptParser").Range} Range */
+/** @typedef {import("../serialization/ObjectMiddleware").ObjectDeserializerContext} ObjectDeserializerContext */
+/** @typedef {import("../serialization/ObjectMiddleware").ObjectSerializerContext} ObjectSerializerContext */
+/** @typedef {import("../util/runtime").RuntimeSpec} RuntimeSpec */
 
 class CommonJsRequireDependency extends ModuleDependency {
 	/**
 	 * @param {string} request request
 	 * @param {Range=} range location in source code
 	 * @param {string=} context request context
+	 * @param {RawReferencedExports | null=} referencedExports list of referenced exports
 	 */
-	constructor(request, range, context) {
+	constructor(request, range, context, referencedExports = null) {
 		super(request);
 		this.range = range;
 		this._context = context;
+		this.referencedExports = referencedExports;
 	}
 
 	get type() {
@@ -29,6 +38,38 @@ class CommonJsRequireDependency extends ModuleDependency {
 
 	get category() {
 		return "commonjs";
+	}
+
+	/**
+	 * Returns list of exports referenced by this dependency
+	 * @param {ModuleGraph} moduleGraph module graph
+	 * @param {RuntimeSpec} runtime the runtime for which the module is analysed
+	 * @returns {ReferencedExports} referenced exports
+	 */
+	getReferencedExports(moduleGraph, runtime) {
+		if (!this.referencedExports) return Dependency.EXPORTS_OBJECT_REFERENCED;
+		return this.referencedExports.map((name) => ({
+			name,
+			canMangle: false
+		}));
+	}
+
+	/**
+	 * @param {ObjectSerializerContext} context context
+	 */
+	serialize(context) {
+		const { write } = context;
+		write(this.referencedExports);
+		super.serialize(context);
+	}
+
+	/**
+	 * @param {ObjectDeserializerContext} context context
+	 */
+	deserialize(context) {
+		const { read } = context;
+		this.referencedExports = read();
+		super.deserialize(context);
 	}
 }
 

test/cases/cjs-tree-shaking/require-destructuring/dir/a.js
@@ -1,0 +1,3 @@
+export const a = "a/a";
+export const b = "a/b";
+export const usedExports = __webpack_exports_info__.usedExports;

test/cases/cjs-tree-shaking/require-destructuring/dir/b.js
@@ -1,0 +1,3 @@
+export const a = "b/a";
+export const b = "b/b";
+export const usedExports = __webpack_exports_info__.usedExports;

test/cases/cjs-tree-shaking/require-destructuring/module.js
@@ -1,0 +1,3 @@
+exports.a = "a";
+exports.b = "b";
+exports.usedExports = __webpack_exports_info__.usedExports;

test/cases/cjs-tree-shaking/require-destructuring/test.filter.js
@@ -1,0 +1,6 @@
+"use strict";
+
+module.exports = function filter(config) {
+	// This test can't run in development mode
+	return config.mode !== "development";
+};

commit 21eb8c2a5340573e1c5665dcc8725b6a9073d21c

test: add edge case coverage for cjs require destructuring tree shaking
Adds three missing test assertions for the feature introduced in #20548:

- aliased destructuring ({ a: renamedA }) marks the original key as used
- aliased context require (template literal) same behavior
- default value destructuring ({ a = 'fallback' }) still marks the key as used
- rest element ({ a, ...rest }) bails the analysis, all exports kept

test/cases/cjs-tree-shaking/require-destructuring/index.js
@@ -10,3 +10,28 @@ it("should support require context destructuring assignment", () => {
 	expect(a).toBe("a/a");
 	expect(usedExports).toEqual(["a", "usedExports"]);
 });
+
+it("should static analyze aliased require destructuring", () => {
+	const { a: renamedA, usedExports } = require("./module");
+	expect(renamedA).toBe("a");
+	expect(usedExports).toEqual(["a", "usedExports"]);
+});
+
+it("should support require context aliased destructuring assignment", () => {
+	const file = "a";
+	const { a: renamedA, usedExports } = require(`./dir/${file}.js`);
+	expect(renamedA).toBe("a/a");
+	expect(usedExports).toEqual(["a", "usedExports"]);
+});
+
+it("should static analyze require destructuring with default values", () => {
+	const { a = "fallback", usedExports } = require("./module");
+	expect(a).toBe("a");
+	expect(usedExports).toEqual(["a", "usedExports"]);
+});
+
+it("should bail on rest element in require destructuring", () => {
+	const { usedExports, ...rest } = require("./module-rest");
+	expect(usedExports).toBe(true);
+	expect(rest).toEqual({ a: "a", b: "b" });
+});

test/cases/cjs-tree-shaking/require-destructuring/module-rest.js
@@ -1,0 +1,3 @@
+exports.a = "a";
+exports.b = "b";
+exports.usedExports = __webpack_exports_info__.usedExports;