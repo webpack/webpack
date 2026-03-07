57c77ffeacf3648f3a438f0fd82c3de4179055ed
aryanraj45 <rajaryan445567@gmail.com>
2026-03-07 22:11:17 +0530

    fix: handle computed member access with string concat on import.meta

--- test/configCases/parsing/import-meta-computed/index.js
+++ test/configCases/parsing/import-meta-computed/index.js
@@ -8,6 +8,11 @@ it("should support computed property access with template string on import.meta"
 	expect(import.meta[`url`]).toBe(url);
 });
 
+it("should support computed property access with string concatenation on import.meta", () => {
+	const url = import.meta.url;
+	expect(import.meta["u" + "rl"]).toBe(url);
+});
+
 it("should support computed property access on import.meta.webpackHot", () => {
 	if (import.meta.webpackHot) {
 		expect(import.meta["webpackHot"]).toBe(import.meta.webpackHot);
