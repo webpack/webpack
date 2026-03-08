6c2282746dcf8c8bf241c2373520d799e8758781
xiaoxiaojx <784487301@qq.com>
2026-03-08 23:47:41 +0800

    update

--- test/configCases/css/export-type-style/webpack.config.js
+++ test/configCases/css/export-type-style/webpack.config.js
@@ -1,25 +1,42 @@
 "use strict";
 
 /** @type {import("../../../../").Configuration} */
-module.exports = {
-	devtool: false,
-	target: "web",
-	mode: "development",
-	optimization: {
-		chunkIds: "named"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				type: "css/module",
-				parser: {
-					exportType: "style"
+/**
+ * Create a webpack configuration for a given target.
+ * @param {"node" | "web"} target target environment
+ * @param {boolean} concatenateModules whether to concatenate modules
+ * @returns {import("../../../../").Configuration} webpack configuration
+ */
+function createConfig(target, concatenateModules) {
+	return {
+		name: `${target}-${concatenateModules ? "concat" : "no-concat"}`,
+		devtool: false,
+		target,
+		mode: "development",
+		optimization: {
+			chunkIds: "named",
+			concatenateModules
+		},
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					type: "css/module",
+					parser: {
+						exportType: "style"
+					}
 				}
-			}
-		]
-	},
-	experiments: {
-		css: true
-	}
-};
+			]
+		},
+		experiments: {
+			css: true
+		}
+	};
+}
+
+module.exports = [
+	createConfig("node", false),
+	createConfig("node", true),
+	createConfig("web", false),
+	createConfig("web", true)
+];
