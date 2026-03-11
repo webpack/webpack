cca82296929a352ec21dd68ec6341503de813e41
junaiddshaukat <junaiddshaukat@gmail.com>
2026-03-11 21:00:40 +0500

    test(target): add config case coverage for electron-main and electron-preload

---
+++ test/configCases/target/electron-preload/index.js
@@ -0,0 +1,5 @@
+const foo = require("foo");
+
+it("should use main field", () => {
+	expect(foo).toBe("main");
+});
---
+++ test/configCases/target/electron-preload/webpack.config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	target: "electron-preload",
+	optimization: {
+		minimize: false
+	}
+};
