3fb3499a1dfa51824295c7f917a192135e2bd3db
snehasipani <sipanisneha616@gmail.com>
2026-03-12 23:08:33 +0530

    Improve readability of chat worker example and add documentation comments

--- examples/worker/chat-worker.js
+++ examples/worker/chat-worker.js
@@ -4,16 +4,20 @@ onconnect = function (e) {
 	for (const port of e.ports) {
 		port.onmessage = event => {
 			const msg = event.data;
-			switch (msg.type) {
-				case "message":
-					add(msg.content, msg.from);
-				// fallthrough
-				case "history":
-					port.postMessage({
-						type: "history",
-						history
-					});
-					break;
+				// When a new chat message arrives, store it in history
+			if (msg.type === "message") {
+				add(msg.content, msg.from);
+			}
+
+			/**
+			 * For both "message" and "history" requests,
+			 * return the current chat history to the client.
+			 */
+			if (msg.type === "message" || msg.type === "history") {
+				port.postMessage({
+					type: "history",
+					history
+				});
 			}
 		};
 	}
