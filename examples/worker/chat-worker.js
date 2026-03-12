07849c95e1fab33008940603b7c6ed13b056ba09
snehasipani <sipanisneha616@gmail.com>
2026-03-12 22:24:24 +0530

    Refactor chat worker to remove switch fallthrough and improve readability

--- examples/worker/chat-worker.js
+++ examples/worker/chat-worker.js
@@ -4,16 +4,14 @@ onconnect = function (e) {
 	for (const port of e.ports) {
 		port.onmessage = event => {
 			const msg = event.data;
-			switch (msg.type) {
-				case "message":
-					add(msg.content, msg.from);
-				// fallthrough
-				case "history":
-					port.postMessage({
-						type: "history",
-						history
-					});
-					break;
+			if (msg.type === "message") {
+				add(msg.content, msg.from);
+			}
+			if (msg.type === "message" || msg.type === "history") {
+				port.postMessage({
+					type: "history",
+					history
+				});
 			}
 		};
 	}
