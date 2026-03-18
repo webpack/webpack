0b9890f635bd6d4a4ba48c8b135a5fa5f8607f9e
aryanraj45 <rajaryan445567@gmail.com>
2026-03-18 18:50:19 +0530

    fix(css): trace conflicting modules back to their direct issuers

--- test/configCases/css/conflicting-order/warnings.js
+++ test/configCases/css/conflicting-order/warnings.js
@@ -2,6 +2,6 @@
 
 module.exports = [
 	[
-		/Conflicting order between css \.\/b\.css and css \.\/c\.css\ncaused by chunk group css/
+		/Conflicting order between css \.\/b\.css and css \.\/c\.css\nCSS modules are imported in:\n {2}- css \.\/lazy1\.css\n {2}- css \.\/lazy2\.css/
 	]
 ];
