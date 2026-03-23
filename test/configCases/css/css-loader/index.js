a60867a515f9139b5f70392377d0459303051aa2
alexander-akait <sheo13666q@gmail.com>
2026-03-24 02:51:53 +0300

    test: added

--- test/configCases/css/css-loader/index.js
+++ test/configCases/css/css-loader/index.js
@@ -14,6 +14,7 @@ import * as styles13 from "./local-Ident-name.module.css?local-ident-name-3";
 import * as styles14 from "./local-Ident-name.module.css?local-ident-name-4";
 import * as styles15 from "./local-Ident-name.module.css?local-ident-name-5";
 import * as styles16 from "./order.module.css";
+import * as styles17 from "./dedup.module.css";
 
 it("should work", () => {
 	const links = document.getElementsByTagName("link");
@@ -40,5 +41,6 @@ it("should work", () => {
 	expect(styles14).toMatchSnapshot();
 	expect(styles15).toMatchSnapshot();
 	expect(styles16).toMatchSnapshot();
+	expect(styles17).toMatchSnapshot();
 	expect(css).toMatchSnapshot();
 });
--- test/configCases/css/css-loader/webpack.config.js
+++ test/configCases/css/css-loader/webpack.config.js
@@ -47,7 +47,6 @@ module.exports = {
 			}
 		]
 	},
-
 	experiments: {
 		css: true
 	}
