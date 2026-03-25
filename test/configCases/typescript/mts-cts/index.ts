17c0464ece7de4b5547f04924066fd11a7237724
alexander-akait <sheo13666q@gmail.com>
2026-03-25 20:09:19 +0300

    test: mts + cts with package.json

--- test/configCases/typescript/mts-cts/index.ts
+++ test/configCases/typescript/mts-cts/index.ts
@@ -1,12 +1,18 @@
 import mod1 from "./module.mts";
 import mod2 from "./module.cts";
 
+import mod3 from "./packages/mts/module.js";
+import mod4 from "./packages/cts/module.js";
+
 it("should work", () => {
 	expect(mod1).toBe("mts");
 	expect(mod2).toBe("cts");
+	expect(mod3).toBe("mts");
+	expect(mod4).toBe("cts");
 
-	// There are two modules:
-	// - index.js + module.ts (concatenated)
+	// There are three modules:
+	// - index.js + module.ts + packages/mts/module.js (concatenated)
 	// - module.cts
-	expect(Object.keys(__webpack_modules__).length).toBe(2);
+	// - packages/cts/module.js
+	expect(Object.keys(__webpack_modules__).length).toBe(3);
 });
