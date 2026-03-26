31d936efbe4af3b86c6da1d88a77fcf6724d4faa
alexander-akait <sheo13666q@gmail.com>
2026-03-26 16:46:02 +0300

    test: added

---
+++ test/configCases/css/mini-css-extract-plugin/index.js
@@ -0,0 +1,24 @@
+it("should work with asset modules", async () => {
+	await import("./asset-modules.css");
+
+	const links = [...document.getElementsByTagName("link")];
+
+	expect(links.find((item) => /asset-modules/.test(item.href)).sheet.css).toMatchSnapshot();
+});
+
+// TODO fix me
+it("should work with multiple @charset at-rules", async () => {
+	await import("./multiple-at-charset.js");
+
+	const links = [...document.getElementsByTagName("link")];
+
+	expect(links.find((item) => /multiple-at-charset/.test(item.href)).sheet.css).toMatchSnapshot();
+});
+
+it("should work with different media at-rules", async () => {
+	await import("./media-at-rule.css");
+
+	const links = [...document.getElementsByTagName("link")];
+
+	expect(links.find((item) => /media-at-rule/.test(item.href)).sheet.css).toMatchSnapshot();
+});
---
+++ test/configCases/css/mini-css-extract-plugin/test.config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+	findBundle() {
+		return [
+			"asset-modules_css.chunk.js",
+			"multiple-at-charset_js-multiple-at-charset-a_css-multiple-at-charset-b_css.chunk.js",
+			"media-at-rule_css.chunk.js",
+			"bundle0.js"
+		];
+	}
+};
