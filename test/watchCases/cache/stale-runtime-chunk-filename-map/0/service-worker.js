de13ec9b45d2989e4ec0a3f681593a0b11ac7091
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2026-03-26 20:50:03 -0400

    add comment clarifying why the assertion  is needed

--- test/watchCases/cache/stale-runtime-chunk-filename-map/0/service-worker.js
+++ test/watchCases/cache/stale-runtime-chunk-filename-map/0/service-worker.js
@@ -14,5 +14,8 @@ it("should update the runtime chunk filename map on rebuild", () => {
 		expect(currentChunkFile).not.toBe(STATE.previousChunkFile);
 	}
 
+	// We need this emitted-source assertion because an incorrect hash order may
+	// still produce a new build, but a wrong one. Check the emitted source to
+	// ensure the new chunk filename is actually referenced by service-worker.js.
 	expect(serviceWorkerSource).toContain(currentChunkFile);
 });
