a3f2035df7255da83c7fa212c482ae2091761c7d
aryanraj45 <rajaryan445567@gmail.com>
2026-04-22 21:42:40 +0530

    test: fix CleanPlugin test to use thisCompilation+processAssets pattern

--- test/configCases/clean/ignore-string/webpack.config.js
+++ test/configCases/clean/ignore-string/webpack.config.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const fs = require("fs");
 const path = require("path");
 const readDir = require("../enabled/readdir");
 
@@ -15,27 +16,28 @@ module.exports = {
 	},
 	plugins: [
 		(compiler) => {
-			compiler.hooks.emit.tap("Test", (compilation) => {
-				const outputPath = compilation.getPath(compiler.outputPath, {});
-				const fs = compiler.outputFileSystem;
-				const customDirPath = path.join(outputPath, CUSTOM_DIR);
-				const ignoredDirPath = path.join(
-					outputPath,
-					IGNORED_DIR,
-					"that/should/not/be/removed"
-				);
-
-				fs.mkdirSync(customDirPath, { recursive: true });
-				fs.writeFileSync(path.join(customDirPath, "file.ext"), "");
-				fs.mkdirSync(ignoredDirPath, { recursive: true });
-				fs.writeFileSync(path.join(ignoredDirPath, "file.ext"), "");
+			let once = true;
+			compiler.hooks.thisCompilation.tap("Test", (compilation) => {
+				compilation.hooks.processAssets.tap("Test", () => {
+					if (once) {
+						const outputPath = compilation.getPath(compiler.outputPath, {});
+						const customDir = path.join(outputPath, CUSTOM_DIR);
+						const ignoredDir = path.join(
+							outputPath,
+							IGNORED_DIR,
+							"that/should/not/be/removed"
+						);
+						fs.mkdirSync(customDir, { recursive: true });
+						fs.writeFileSync(path.join(customDir, "file.ext"), "");
+						fs.mkdirSync(ignoredDir, { recursive: true });
+						fs.writeFileSync(path.join(ignoredDir, "file.ext"), "");
+						once = false;
+					}
+				});
 			});
 			compiler.hooks.afterEmit.tap("Test", (compilation) => {
 				const outputPath = compilation.getPath(compiler.outputPath, {});
-				const result = readDir(outputPath);
-				result.directories.sort();
-				result.files.sort();
-				expect(result).toMatchInlineSnapshot(`
+				expect(readDir(outputPath)).toMatchInlineSnapshot(`
 			Object {
 			  "directories": Array [
 			    "ignored",
@@ -47,8 +49,8 @@ module.exports = {
 			    "ignored/dir/that/should/not/be/removed",
 			  ],
 			  "files": Array [
-			    "bundle0.js",
 			    "ignored/dir/that/should/not/be/removed/file.ext",
+			    "bundle0.js",
 			  ],
 			}
 		`);
