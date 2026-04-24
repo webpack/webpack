b82500f3d3cc31a9f8d44b8a5f8fbfad6fe60946
alexander-akait <sheo13666q@gmail.com>
2026-04-24 17:40:26 +0300

    refactor: more

--- schemas/plugins/dll/DllPlugin.check.d.ts
+++ schemas/plugins/dll/DllPlugin.check.d.ts
@@ -3,5 +3,5 @@
  * DO NOT MODIFY BY HAND.
  * Run `yarn fix:special` to update
  */
-declare const check: (options: import("../../declarations/plugins/DllPlugin").DllPluginOptions) => boolean;
+declare const check: (options: import("../../../declarations/plugins/dll/DllPlugin").DllPluginOptions) => boolean;
 export = check;
