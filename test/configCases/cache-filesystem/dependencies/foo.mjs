d4c42b1086eb56d793c7273961c14697a59d9eaa
alexander-akait <sheo13666q@gmail.com>
2026-04-29 15:37:47 +0300

    refactor: more

--- test/configCases/cache-filesystem/dependencies/foo.mjs
+++ test/configCases/cache-filesystem/dependencies/foo.mjs
@@ -16,3 +16,28 @@ import(`./bar10\\'.mjs`);
 import(`./bar11".mjs`);
 import(`./bar12\".mjs`);
 import(`./bar13\\".mjs`);
+import(test);
+
+import { name } from './bar14.js';
+import json from './json.json' with { type: 'json' }
+
+export var p = 5;
+export function q () {
+
+}
+
+export { x } from './bar15.js';
+
+// Comments provided to demonstrate edge cases
+import /*comment!*/ (  './bar16.js', { with: { type: 'json' }});
+import /*comment!*/.meta.asdf;
+
+// Defer phase imports:
+import defer mod from './bar17.js';
+import.defer('./bar18.js');
+
+// Source phase imports:
+import source mod from './bar19.wasm';
+import.source('./bar20.wasm');
+
+import("./" + "bar21.js")
