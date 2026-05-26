2198313f41c3c23c94e39aa35945d5e896169708
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2026-05-26 14:06:43 +0300

    refactor: move Module.hot to NormalModule (#21028)

--- types.d.ts
+++ types.d.ts
@@ -14223,7 +14223,6 @@ declare class Module extends DependenciesBlock {
 	factoryMeta?: FactoryMeta;
 	useSourceMap: boolean;
 	useSimpleSourceMap: boolean;
-	hot: boolean;
 	buildMeta?: BuildMeta;
 	buildInfo?: BuildInfo;
 	presentationalDependencies?: Dependency[];
@@ -16170,6 +16169,7 @@ declare class NormalModule extends Module {
 	matchResource?: string;
 	loaders: LoaderItem[];
 	extractSourceMap: boolean;
+	hot: boolean;
 	error: null | Error;
 	getResource(): null | string;
 
