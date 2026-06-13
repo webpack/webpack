62c9cbf97fdfb241d215cf600da58c1b632c130b
sheo13666q <sheo13666q@gmail.com>
2026-06-13 12:01:07 +0000

    test: cover dynamic import() of node builtins in universal targets
    Verifies the other safe universal external path: a dynamic `import()` of a
    node built-in stays lazy (no hoisted static import that would crash a browser
    at load) and resolves correctly when run in node.

---
+++ test/configCases/externals/universal-dynamic-import/webpack.config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	target: ["node", "web"],
+	output: { module: true },
+	experiments: { outputModule: true }
+};
