9ab27a1c6ca4d45600e30677bd1fccd11cb5746d
sheo13666q <sheo13666q@gmail.com>
2026-06-19 13:34:10 +0000

    test: skip Deno for new ESM-worker config cases (node-global-worker, universal-all-module-types)

--- test/configCases/target/universal-all-module-types/test.filter.js
+++ test/configCases/target/universal-all-module-types/test.filter.js
@@ -7,3 +7,11 @@ const supportsWorker = require("../../../helpers/supportsWorker");
 // (Node >= 22.3).
 module.exports = () =>
 	supportsWorker() && typeof process.getBuiltinModule === "function";
+
+const _denoOrigFilter = module.exports;
+
+// Deno 2.8.3 hard-panics ("Module not found", bindings.rs) instead of throwing
+// when executing this case's ESM worker output; the panic aborts the process and
+// cannot be caught, so skip the case under Deno.
+module.exports = (...args) =>
+	!process.versions.deno && _denoOrigFilter(...args);
