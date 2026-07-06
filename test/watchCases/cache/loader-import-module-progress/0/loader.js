3b826379e99eebeddce4541f443682ea75d4a4b3
alexander-akait <4567934+alexander-akait@users.noreply.github.com>
2026-07-06 21:54:42 +0000

    test: remove watchCases for the removed ProgressPlugin counts cache
    The loader-import-module-progress and loader-load-module-progress cases
    asserted the persisted `ProgressPlugin|counts` value, which no longer
    exists after dropping the pack-invalidating counts cache.

--- test/watchCases/cache/loader-import-module-progress/0/loader.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("../../../../../").PitchLoaderDefinitionFunction} */
-exports.pitch = async function (remaining) {
-	const result = await this.importModule(
-		`${this.resourcePath}.webpack[javascript/auto]!=!${remaining}`
-	);
-	return JSON.stringify(result, null, 2);
-};
--- test/watchCases/cache/loader-load-module-progress/0/mod.js
+++
@@ -1,4 +0,0 @@
-export const value = 42;
-export * from "./imported.js";
-export const random = Math.random();
-export default 24;
