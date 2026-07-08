78e59b9bbed58cd2a463780c9c944390f64068ac
alexander-akait <sheo13666q@gmail.com>
2026-07-08 20:38:54 +0000

    test(html): format integrity no-cors warnings.js for prettier

--- test/configCases/html/output-html-integrity-no-cors/warnings.js
+++ test/configCases/html/output-html-integrity-no-cors/warnings.js
@@ -1,7 +1,5 @@
 "use strict";
 
 module.exports = [
-	[
-		/output\.html\.integrity is set but output\.crossOriginLoading is not/
-	]
+	[/output\.html\.integrity is set but output\.crossOriginLoading is not/]
 ];
