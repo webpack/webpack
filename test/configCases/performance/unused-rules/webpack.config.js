f1bdb152dc8c5169b6642f6abe561b3590a634f7
alexander-akait <4567934+alexander-akait@users.noreply.github.com>
2026-08-15 19:40:44 +0000

    test(performance): cover performance.unusedRules

---
+++ test/configCases/performance/unused-rules/webpack.config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	mode: "development",
+	performance: {
+		hints: "warning",
+		unusedRules: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: []
+			},
+			{
+				test: /\.never-matches$/,
+				loader: "./loader"
+			}
+		]
+	}
+};
